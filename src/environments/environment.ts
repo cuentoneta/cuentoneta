5f4b07f0f28c74eefdb5927a5630029c9aee8b61
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-01-24 02:47:39 -0300

    Agregados certificados para servidor HTTPS local. Cambios en build de Angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
     production: false,
-    apiUrl: '',
-    // apiUrl: "https://localhost:443",
+    apiUrl: 'http://localhost:8100',
 };
 
 /*
