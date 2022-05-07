07ae8457a9239a8e74ed5d057188648065f06b91
Ramiro Olivencia <ramiro@olivencia.com.ar>
2022-05-07 16:22:10 -0300

    Removed console.error statement

--- api/story/[id].js
+++ api/story/[id].js
@@ -3,7 +3,6 @@ import { mapAuthor, mapBodyToParagraphs, mapPrologues } from '../functions';
 const sanityConnector = require('../_helpers/sanity-connector');
 
 export default async function getById(req, res) {
-    console.error(req.query);
     const { id } = req.query;
     const query = `*[_type == 'story' && day == ${id}]{title, day, originalLink, forewords, categories, publishedAt, body, review, forewords, author->}`;
     const result = await sanityConnector.client.fetch(query, {});
