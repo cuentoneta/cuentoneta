c7d77c9e1784f1be1f66cb741a0f1e9ffa89150a
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-12-11 23:42:23 -0300

    [#103] - Agrega modelos desde v1

---
+++ src/app/models/author.model.ts
@@ -0,0 +1,8 @@
+export interface Author {
+    id: number;
+    name: string;
+    imageUrl: string;
+    nationality: string;
+    biography: string;
+    fullBioUrl: string;
+}
---
+++ src/app/models/story.model.ts
@@ -0,0 +1,12 @@
+import { Author } from './author.model';
+import { Prologue } from './prologue.model';
+
+export interface StoryModel {
+    id: number;
+    author: Author;
+    title: string;
+    day: number;
+    prologues: Prologue[];
+    summary: string;
+    paragraphs: string[];
+}
