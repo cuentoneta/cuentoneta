d4adcb1aef075c962b548409a204871422adf6f9
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-12-26 21:40:53 -0300

    [#103] - Agrega StoryService como provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
+// Components
+import { AppComponent } from './app.component';
+
+// Core
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+// Modules
+import { HttpClientModule } from '@angular/common/http';
+
+// Providers
+import { StoryService } from './providers/story.service';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, HttpClientModule],
+    providers: [StoryService],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
