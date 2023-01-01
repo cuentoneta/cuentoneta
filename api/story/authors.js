660173e907e79932dcbd78cdb1ba214eb8eb3af5
Ramiro Olivencia <ramiro@olivencia.com.ar>
2023-01-01 19:39:24 -0300

    [#107] - Agrega edition a los parameters de authors

--- api/story/authors.js
+++ api/story/authors.js
@@ -2,7 +2,8 @@ const sanityConnector = require('../_helpers/sanity-connector');
 const { mapAuthor } = require('../functions');
 
 export default async function get(req, res) {
-    const query = `*[_type == 'story'] | order(day desc) {title, day, categories, publishedAt, author->}`;
+    const { edition } = req.query;
+    const query = `*[_type == 'story' && edition == '${edition}'] | order(day desc) {title, day, categories, publishedAt, author->}`;
     const authors = await sanityConnector.client.fetch(query, {});
     res.json(authors.map((x) => ({ ...x, author: mapAuthor(x.author) })));
 }
