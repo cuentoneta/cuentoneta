06a58173c93fdfe983603d33361c3e46f035bd61
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-06 12:32:38 -0300

    [#110] - Agregada optimización de imagen

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { StoryCardModule } from '../../components/story-card/story-card.module';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, HomeRoutingModule, NgxSkeletonLoaderModule, StoryCardModule],
+    imports: [CommonModule, HomeRoutingModule, StoryCardModule, NgOptimizedImage],
 })
 export class HomeModule {}
