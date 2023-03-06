51393b1ba7899935248464e2156807c1b8ea54f1
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-06 12:04:44 -0300

    [#110] - Versión inicial del componente home de landing page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { StoryService } from '../../providers/story.service';
+import { Observable, of } from 'rxjs';
+import { Story } from '../../models/story.model';
 
 @Component({
     selector: 'cuentoneta-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+
+    topStoriesAmount = 5;
+    storiesAmount = 6;
+    $latestStories: Observable<Story[]> = of([]);
+    $oldStories: Observable<Story[]> = of([]);
+
+    constructor(private storyService: StoryService) {}
+
+    ngOnInit(): void {
+        this.$latestStories = this.storyService.getLatest('2022', this.topStoriesAmount)
+        this.$oldStories = this.storyService.getLatest('2021', this.storiesAmount)
+    }
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { StoryCardModule } from '../../components/story-card/story-card.module';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, HomeRoutingModule],
+    imports: [CommonModule, HomeRoutingModule, NgxSkeletonLoaderModule, StoryCardModule],
 })
 export class HomeModule {}
