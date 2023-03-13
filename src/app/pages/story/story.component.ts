a557689ed25f5a7caebf9b25cdc69d8522fbb5c6
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-13 11:46:47 -0300

    [#127] - Fetch de lista en sidebar desde parámetros en URL

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { combineLatest, map, Observable, of, switchMap } from 'rxjs';
 import { StoryService } from '../../providers/story.service';
-import { Story } from '../../models/story.model';
+import { Story, StoryList } from '../../models/story.model';
 
 @Component({
     selector: 'cuentoneta-story',
@@ -11,10 +11,12 @@ import { Story } from '../../models/story.model';
 })
 export class StoryComponent {
     story$: Observable<Story> | undefined;
-    storyList$: Observable<Story[]> | undefined;
+    storyList$: Observable<StoryList> | undefined;
     constructor(private activatedRoute: ActivatedRoute, private storyService: StoryService) {
         this.story$ = activatedRoute.queryParams.pipe(switchMap(({ id }) => this.storyService.getById(id)));
-        // ToDo: Parametrizar StoryList
-        this.storyList$ = activatedRoute.queryParams.pipe(switchMap(({ id }) => this.storyService.getLatest('2021', 10)));
+        this.storyList$ = activatedRoute.queryParams.pipe(
+            switchMap(({ list }) => combineLatest([of(list), this.storyService.getLatest(list, 10)])),
+            map(([name, stories]) => ({ title: name, stories: stories }))
+        );
     }
 }
