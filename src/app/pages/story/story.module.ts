0dde30ecd18bbef133591ac080be236b410dd7e9
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-13 00:41:04 -0300

    [#127] - Extrae tarjeta de bio a nuevo componente

--- src/app/pages/story/story.module.ts
+++ src/app/pages/story/story.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 import { StoryRoutingModule } from './story-routing.module';
 import { StoryComponent } from './story.component';
+import { BioSummaryCardModule } from '../../components/bio-summary-card/bio-summary-card.module';
 
 @NgModule({
     declarations: [StoryComponent],
-    imports: [CommonModule, StoryRoutingModule, NgOptimizedImage],
+    imports: [CommonModule, StoryRoutingModule, NgOptimizedImage, BioSummaryCardModule],
 })
 export class StoryModule {}
