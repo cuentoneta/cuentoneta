715d44da6744bf47b041e8524110e4164fa6811a
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-13 11:04:34 -0300

    [#127] - Extrae label a nuevo componente

--- src/app/components/story-card/story-card.module.ts
+++ src/app/components/story-card/story-card.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule, NgOptimizedImage } from '@angular/common'
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { StoryCardComponent } from './story-card.component';
-import {NgxSkeletonLoaderModule} from "ngx-skeleton-loader";
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { StoryEditionDateLabelModule } from '../story-edition-date-label/story-edition-date-label.module';
 
 @NgModule({
     declarations: [StoryCardComponent],
     exports: [StoryCardComponent],
-    imports: [CommonModule, NgOptimizedImage, NgxSkeletonLoaderModule],
+    imports: [CommonModule, NgOptimizedImage, NgxSkeletonLoaderModule, StoryEditionDateLabelModule],
 })
 export class StoryCardModule {}
