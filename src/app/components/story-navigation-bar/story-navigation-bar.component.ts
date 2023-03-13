bc26f9568a3642690685f642d23d07e39c476839
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-13 01:31:07 -0300

    [#127] - Agrega componente StoryNavigationBar

--- src/app/components/story-navigation-bar/story-navigation-bar.component.ts
+++ src/app/components/story-navigation-bar/story-navigation-bar.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Story } from '../../models/story.model';
 
 @Component({
     selector: 'cuentoneta-story-navigation-bar',
     templateUrl: './story-navigation-bar.component.html',
     styleUrls: ['./story-navigation-bar.component.scss'],
 })
-export class StoryNavigationBarComponent {}
+export class StoryNavigationBarComponent {
+    @Input() storyList: Story[] = [];
+}
--- src/app/components/story-navigation-bar/story-navigation-bar.module.ts
+++ src/app/components/story-navigation-bar/story-navigation-bar.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoryNavigationBarComponent } from './story-navigation-bar.component';
+import {RouterLink} from "@angular/router";
 
 @NgModule({
     declarations: [StoryNavigationBarComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
+    exports: [
+        StoryNavigationBarComponent
+    ]
 })
 export class StoryNavigationBarModule {}
