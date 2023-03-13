e57c83548daa2afb8059c20dd8ee008449669aac
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-13 00:50:01 -0300

    [#127] - Agrega componente de navegación de historias

---
+++ src/app/components/story-navigation-bar/story-navigation-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cuentoneta-story-navigation-bar',
+    templateUrl: './story-navigation-bar.component.html',
+    styleUrls: ['./story-navigation-bar.component.scss'],
+})
+export class StoryNavigationBarComponent {}
---
+++ src/app/components/story-navigation-bar/story-navigation-bar.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoryNavigationBarComponent } from './story-navigation-bar.component';
+
+@NgModule({
+    declarations: [StoryNavigationBarComponent],
+    imports: [CommonModule],
+})
+export class StoryNavigationBarModule {}
