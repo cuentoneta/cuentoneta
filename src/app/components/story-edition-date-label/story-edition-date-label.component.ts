e8d4d98d37994850703948d59de02f8cb24a432e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-14 18:37:46 -0300

    [#130] - Simplifica componente StoryEditionDateLabel

--- src/app/components/story-edition-date-label/story-edition-date-label.component.ts
+++ src/app/components/story-edition-date-label/story-edition-date-label.component.ts
@@ -2,11 +2,11 @@ import { Component, Input } from '@angular/core';
 import { Story } from '../../models/story.model';
 
 @Component({
-    selector: 'cuentoneta-story-edition-date-label[story]',
+    selector: 'cuentoneta-story-edition-date-label[label]',
     templateUrl: './story-edition-date-label.component.html',
     styleUrls: ['./story-edition-date-label.component.scss'],
 })
 export class StoryEditionDateLabelComponent {
-    @Input() story: Story | undefined;
+    @Input() label: string | undefined;
     @Input() markAsNew: boolean = false;
 }
