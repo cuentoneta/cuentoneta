277273af080127fa08460ecd970e7382275e2650
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-14 17:04:33 -0300

    [#130] - Simplifica fetch de Storylist

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, map, Observable, of, switchMap } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { StoryService } from '../../providers/story.service';
-import { Story, StoryList } from '../../models/story.model';
+import { Story } from '../../models/story.model';
+import { StoryList } from '../../models/storylist.model';
 
 @Component({
     selector: 'cuentoneta-story',
@@ -11,12 +12,11 @@ import { Story, StoryList } from '../../models/story.model';
 })
 export class StoryComponent {
     story$: Observable<Story> | undefined;
-    storyList$: Observable<StoryList> | undefined;
+    storylist$: Observable<StoryList> | undefined;
     constructor(private activatedRoute: ActivatedRoute, private storyService: StoryService) {
         this.story$ = activatedRoute.queryParams.pipe(switchMap(({ id }) => this.storyService.getById(id)));
-        this.storyList$ = activatedRoute.queryParams.pipe(
-            switchMap(({ list }) => combineLatest([of(list), this.storyService.getLatest(list, 10)])),
-            map(([name, stories]) => ({ title: name, stories: stories }))
+        this.storylist$ = activatedRoute.queryParams.pipe(
+            switchMap(({ list }) => this.storyService.getLatest(list, 10))
         );
     }
 }
