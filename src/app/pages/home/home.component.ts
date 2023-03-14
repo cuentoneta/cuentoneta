70410ec87cda7bd46935a3fbafd537b46eff38bf
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-14 18:17:13 -0300

    [#130] - Actualización de Hero. Uso del componente StoryListCardDeckComponent para mostrar lista de Stories.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StoryService } from '../../providers/story.service';
-import { combineLatest, Observable, of } from 'rxjs';
-import { Story } from '../../models/story.model';
+import { combineLatest } from 'rxjs';
+import { StoryList } from '../../models/storylist.model';
 
 @Component({
     selector: 'cuentoneta-home',
@@ -9,16 +9,15 @@ import { Story } from '../../models/story.model';
     styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-    latestStories: Story[] = Array(5);
-    oldStories: Story[] = Array(6);
+    latestStories: StoryList | undefined;
+    oldStories: StoryList | undefined;
 
     constructor(private storyService: StoryService) {}
 
     ngOnInit(): void {
         combineLatest([
-            this.storyService.getLatest('2022', this.latestStories.length),
-            this.storyService.getLatest('2021', this.oldStories.length),
+            this.storyService.getLatest('fec-english-sessions', 5),
+            this.storyService.getLatest('verano-2022', 6),
         ]).subscribe(([topStories, oldStories]) => {
             this.latestStories = topStories;
             this.oldStories = oldStories;
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { StoryCardModule } from '../../components/story-card/story-card.module';
+import { StoryListCardDeckComponent } from '../../components/story-list-card-deck/story-list-card-deck.component';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, HomeRoutingModule, StoryCardModule, NgOptimizedImage],
+    imports: [CommonModule, HomeRoutingModule, StoryCardModule, NgOptimizedImage, StoryListCardDeckComponent],
 })
 export class HomeModule {}
