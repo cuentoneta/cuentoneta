3aeb69d3daae26a4b50313a8aa73f0daa59962f6
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-14 17:21:00 -0300

    [#130] - Arregla caps en nombre de modelo

--- src/app/models/storylist.model.ts
+++ src/app/models/storylist.model.ts
@@ -1,15 +1,15 @@
 import { Story, StoryDTO } from './story.model';
 
-interface StoryListBase {
+interface StorylistBase {
     title: string;
     slug: string;
     description?: string[];
     language?: string;
     imageUrl?: string;
 }
-export interface StoryList extends StoryListBase {
+export interface StoryList extends StorylistBase {
     stories: Story[];
 }
-export interface StoryListDTO extends StoryListBase {
+export interface StoryListDTO extends StorylistBase {
     stories: StoryDTO[];
 }
