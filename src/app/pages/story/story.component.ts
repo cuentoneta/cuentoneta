626d8046ce67baeedf8c2f92041c6ed0711cd099
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-14 19:51:36 -0300

    [#130] - Agrega desuscripción manual en OnDestroy

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, first, switchMap } from 'rxjs';
+import { combineLatest, first, Subscription, switchMap } from 'rxjs';
 import { StoryService } from '../../providers/story.service';
 import { Story } from '../../models/story.model';
 import { StoryList } from '../../models/storylist.model';
@@ -10,20 +10,24 @@ import { StoryList } from '../../models/storylist.model';
     templateUrl: './story.component.html',
     styleUrls: ['./story.component.scss'],
 })
-export class StoryComponent {
+export class StoryComponent implements OnDestroy {
     story: Story | undefined;
     storylist: StoryList | undefined;
 
-    dummyList = Array(10)
+    dummyList = Array(10);
+    private subscription: Subscription;
+
     constructor(private activatedRoute: ActivatedRoute, private storyService: StoryService) {
-        combineLatest([
+        this.subscription = combineLatest([
             activatedRoute.queryParams.pipe(switchMap(({ id }) => this.storyService.getById(id))),
             activatedRoute.queryParams.pipe(switchMap(({ list }) => this.storyService.getLatest(list, 10))),
-        ])
-            .pipe(first())
-            .subscribe(([story, storylist]) => {
-                this.story = story;
-                this.storylist = storylist;
-            });
+        ]).subscribe(([story, storylist]) => {
+            this.story = story;
+            this.storylist = storylist;
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
     }
 }
