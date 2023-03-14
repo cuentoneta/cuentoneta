1e41661a120413351498bf77513223d84d86af2e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-14 18:37:15 -0300

    [#130] - Adapta uso de skeletons para card decks

--- src/app/components/story-list-card-deck/story-list-card-deck.component.ts
+++ src/app/components/story-list-card-deck/story-list-card-deck.component.ts
@@ -1,18 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoryCardModule } from '../story-card/story-card.module';
 import { RouterLink } from '@angular/router';
 import { StoryList } from '../../models/storylist.model';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
     selector: 'cuentoneta-story-list-card-deck',
     standalone: true,
-    imports: [CommonModule, StoryCardModule, RouterLink],
+    imports: [CommonModule, StoryCardModule, RouterLink, NgxSkeletonLoaderModule],
     templateUrl: './story-list-card-deck.component.html',
     styleUrls: ['./story-list-card-deck.component.scss'],
 })
-export class StoryListCardDeckComponent {
+export class StoryListCardDeckComponent implements OnInit {
+    @Input() number: number = 6;
     @Input() storylist: StoryList | undefined;
     @Input() highlightFirstRow: boolean = false;
     @Input() displayTitle: boolean = true;
+
+    dummyList: any[] = [];
+
+    ngOnInit() {
+        this.dummyList = Array(this.number);
+    }
 }
