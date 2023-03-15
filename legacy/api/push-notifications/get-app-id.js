7235ddfaaa0c3d8d0671043971fc86a1fdda4a2d
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-15 01:58:03 -0300

    [#134] - Elimina carpeta legacy de rama activa de desarrollo

--- legacy/api/push-notifications/get-app-id.js
+++
@@ -1,5 +0,0 @@
-const environment = require('../_helpers/environment');
-
-export default function getAppId(req, res) {
-    res.json(environment.oneSignalAppId);
-}
--- legacy/copy-build-assets.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-// File destination.txt will be created or overwritten by default.
-fs.copyFile('OneSignalSDKWorker.js', 'www/OneSignalSDKWorker.js', (err) => {
-    if (err) throw err;
-    console.log('OneSignalSDKWorker.js was copied to www/OneSignalSDKWorker.js');
-});
