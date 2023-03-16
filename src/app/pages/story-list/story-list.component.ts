5e2643804925993fc7f7f9b53e2fda0f094607d6
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-16 11:00:26 -0300

    [#139] - Agrega fetch de lista completa para page StoryList

--- src/app/pages/story-list/story-list.component.ts
+++ src/app/pages/story-list/story-list.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StoryService } from '../../providers/story.service';
+import { Observable, switchMap } from 'rxjs';
+import { StoryList } from '../../models/storylist.model';
 
 @Component({
     selector: 'cuentoneta-story-list',
     templateUrl: './story-list.component.html',
     styleUrls: ['./story-list.component.scss'],
 })
-export class StoryListComponent {}
+export class StoryListComponent {
+    storyList$: Observable<StoryList> | undefined;
+    constructor(private activatedRoute: ActivatedRoute, private storyService: StoryService) {
+        this.storyList$ = activatedRoute.queryParams.pipe(
+            // ToDo: Rediseñar signature del método para poder traer todas las historias y luego hacer fetch vía scroll/ver más
+            switchMap(({ slug }) => this.storyService.getLatest(slug, 60))
+        );
+    }
+}
--- src/app/pages/story-list/story-list.module.ts
+++ src/app/pages/story-list/story-list.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { StoryListRoutingModule } from './story-list-routing.module';
 import { StoryListComponent } from './story-list.component';
+import {StoryListCardDeckComponent} from "../../components/story-list-card-deck/story-list-card-deck.component";
 
 @NgModule({
     declarations: [StoryListComponent],
-    imports: [CommonModule, StoryListRoutingModule],
+    imports: [CommonModule, StoryListRoutingModule, StoryListCardDeckComponent],
 })
 export class StoryListModule {}
