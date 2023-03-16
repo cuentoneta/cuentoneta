784759731d656fc3488c1dbf31ee79e2153ac27f
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-16 12:09:13 -0300

    [#139] - Agrega desuscripción en OnDestroy. Muestra nuevamente skeletons al cambiar de StoryList bajo la misma vista

--- src/app/pages/story-list/story-list.component.ts
+++ src/app/pages/story-list/story-list.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StoryService } from '../../providers/story.service';
-import { switchMap, take } from 'rxjs';
+import { Subscription, switchMap, take, tap } from 'rxjs';
 import { StoryList } from '../../models/storylist.model';
 
 @Component({
@@ -9,17 +9,24 @@ import { StoryList } from '../../models/storylist.model';
     templateUrl: './story-list.component.html',
     styleUrls: ['./story-list.component.scss'],
 })
-export class StoryListComponent {
+export class StoryListComponent implements OnDestroy {
     storyList: StoryList | undefined;
+    private subscription: Subscription;
     constructor(private activatedRoute: ActivatedRoute, private storyService: StoryService) {
-        activatedRoute.queryParams
+        this.subscription = activatedRoute.queryParams
             .pipe(
                 // ToDo: Rediseñar signature del método para poder traer todas las historias y luego hacer fetch vía scroll/ver más
-                switchMap(({ slug }) => this.storyService.getLatest(slug, 60)),
-                take(1)
+                switchMap(({ slug }) => {
+                    this.storyList = undefined;
+                    return this.storyService.getLatest(slug, 60);
+                })
             )
             .subscribe((storyList) => {
                 this.storyList = storyList;
             });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 }
