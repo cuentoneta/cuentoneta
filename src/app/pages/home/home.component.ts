eb9d37ff4e78556b60528599c153cd6a807028f3
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-18 00:01:37 -0300

    [#144] - Implementa uso de destroyedDirective para desuscripciones

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { StoryService } from '../../providers/story.service';
-import { combineLatest, first } from 'rxjs';
+import { combineLatest, takeUntil } from 'rxjs';
 import { StoryList } from '../../models/storylist.model';
+import { DestroyedDirective } from '../../directives/destroyed.directive';
 
 @Component({
     selector: 'cuentoneta-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
+    hostDirectives: [DestroyedDirective],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
     latestStories: StoryList | undefined;
     oldStories: StoryList | undefined;
 
-    constructor(private storyService: StoryService) {}
+    constructor() {
+        const destroyedDirective = inject(DestroyedDirective);
+        const storyService = inject(StoryService);
 
-    ngOnInit(): void {
-        combineLatest([
-            this.storyService.getLatest('fec-english-sessions', 5),
-            this.storyService.getLatest('verano-2022', 6),
-        ])
-            .pipe(first())
+        combineLatest([storyService.getLatest('fec-english-sessions', 5), storyService.getLatest('verano-2022', 6)])
+            .pipe(takeUntil(destroyedDirective.destroyed$))
             .subscribe(([topStories, oldStories]) => {
                 this.latestStories = topStories;
                 this.oldStories = oldStories;
--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -1,38 +1,41 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, Subscription, switchMap } from 'rxjs';
+import { combineLatest, Subscription, switchMap, takeUntil } from 'rxjs';
 import { StoryService } from '../../providers/story.service';
 import { Story } from '../../models/story.model';
 import { StoryList } from '../../models/storylist.model';
+import { DestroyedDirective } from '../../directives/destroyed.directive';
 
 @Component({
     selector: 'cuentoneta-story',
     templateUrl: './story.component.html',
     styleUrls: ['./story.component.scss'],
+    hostDirectives: [DestroyedDirective],
 })
-export class StoryComponent implements OnDestroy {
+export class StoryComponent {
     story: Story | undefined;
     storylist: StoryList | undefined;
 
     dummyList = Array(10);
-    private subscription: Subscription;
 
-    constructor(private activatedRoute: ActivatedRoute, private storyService: StoryService) {
-        this.subscription = combineLatest([
+    constructor() {
+        const activatedRoute = inject(ActivatedRoute);
+        const destroyedDirective = inject(DestroyedDirective);
+        const storyService = inject(StoryService);
+
+        combineLatest([
             activatedRoute.queryParams.pipe(
                 switchMap(({ slug }) => {
                     this.story = undefined;
-                    return this.storyService.getBySlug(slug);
+                    return storyService.getBySlug(slug);
                 })
             ),
-            activatedRoute.queryParams.pipe(switchMap(({ list }) => this.storyService.getLatest(list, 10))),
-        ]).subscribe(([story, storylist]) => {
-            this.story = story;
-            this.storylist = storylist;
-        });
-    }
-
-    ngOnDestroy(): void {
-        this.subscription.unsubscribe();
+            activatedRoute.queryParams.pipe(switchMap(({ list }) => storyService.getLatest(list, 10))),
+        ])
+            .pipe(takeUntil(destroyedDirective.destroyed$))
+            .subscribe(([story, storylist]) => {
+                this.story = story;
+                this.storylist = storylist;
+            });
     }
 }
