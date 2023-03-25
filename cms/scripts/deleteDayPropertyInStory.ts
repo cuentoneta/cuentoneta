2d45ba6f9cb0f2b91c3622d097787b9e7c0bf3ad
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-25 16:32:51 -0300

    [#164] - Agrega migración para unset de campo day en documentos de tipo Story

---
+++ cms/scripts/deleteDayPropertyInStory.ts
@@ -0,0 +1,44 @@
+// RO - 2023-25-03
+// Este script fue utilizado el 25/03/2023 para eliminar el campo day de los documentos de tipo Story en Sanity CMS.
+// Queda aquí a modo de ejemplo para saber cómo proceder a la hora de escribir otro script de migración a futuro.
+
+import { client } from '../../api/_helpers/sanity-connector';
+
+const fetchStories = () =>
+    client.fetch(`
+  *[_type == 'story']
+`);
+
+const buildPatches = (stories) =>
+    stories.map((story) => ({
+        id: story._id,
+        patch: {
+            unset: ['day'],
+            ifRevisionID: story._rev,
+        },
+    }));
+
+const createTransaction = (patches) =>
+    patches.reduce((tx, patch) => tx.patch(patch.id, patch.patch), client.transaction());
+
+const commitTransaction = (tx) => tx.commit();
+
+const migrateBatch = async () => {
+    const stories = await fetchStories();
+    const patches = buildPatches(stories);
+    if (patches.length === 0) {
+        console.log('No hay documentos para migrar!');
+        return null;
+    }
+    console.log(
+        `Migrando batch:\n %s`,
+        patches.map((patch) => `${patch.id} => ${JSON.stringify(patch.patch)}`).join('\n')
+    );
+    const transaction = createTransaction(patches);
+    await commitTransaction(transaction);
+};
+
+migrateBatch().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
