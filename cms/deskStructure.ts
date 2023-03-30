440dbc5640fec567fe288e97c0f4fcfada7c7064
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-30 10:49:48 -0300

    [#181] - Remueve settings de vista de desk

--- cms/deskStructure.ts
+++ cms/deskStructure.ts
@@ -1,12 +1,13 @@
 // deskStructure.js
 export default (S) => S.list().title('Content').items([
-    S.listItem()
-        .title('Settings')
-        .child(
-            S.editor()
-                .id('settings')
-                .schemaType('settings')
-                .documentId('settings')
-        ),
+    // ToDo: Configurar settings
+    // S.listItem()
+    //     .title('Settings')
+    //     .child(
+    //         S.editor()
+    //             .id('settings')
+    //             .schemaType('settings')
+    //             .documentId('settings')
+    //     ),
     ...S.documentTypeListItems().filter(listItem => !['settings'].includes(listItem.getId()))
 ])
