32ca04c894414dfcdd9e4e9754d740698c8c3dd9
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-04-06 11:17:43 -0300

    [#178] - Agrega métodos a FetchContentDirective. Agrega documentación y tipos.

--- src/app/directives/fetch-content.directive.ts
+++ src/app/directives/fetch-content.directive.ts
@@ -2,6 +2,17 @@ import { Directive } from '@angular/core';
 import { Observable, OperatorFunction, tap } from 'rxjs';
 import { Params } from '@angular/router';
 
+/**
+ * @description
+ * Esta directiva se usa para traer contenido de una fuente y mostrar un indicador
+ * de carga, mediante la propiedad isLoading, mientras se está realizando
+ * la llamada async a la fuente.
+ * @example
+ * // Path: src\app\pages\home\home.component.ts
+ * // Path: src\app\pages\story-list\story-list.component.ts
+ *
+ */
+
 @Directive({
     selector: '[cuentonetaFetchContent]',
     standalone: true,
@@ -9,11 +20,33 @@ import { Params } from '@angular/router';
 export class FetchContentDirective<T> {
     isLoading: boolean = false;
 
-    public fetchContent(params$: Observable<Params>, operatorFunction$: OperatorFunction<Params, T>): Observable<T> {
+    /**
+     * @description
+     * Desde un observable con parámetros, se utiliza una función operadora de tipo
+     * OperatorFunction<Params, T> para traer contenido de una fuente y mostrar un
+     * indicador de carga a lo largo de la llamada.
+     * @param params$
+     * @param operatorFunction$
+     */
+    public fetchContentWithSourceParams$(
+        params$: Observable<Params>,
+        operatorFunction$: OperatorFunction<Params, T>
+    ): Observable<T> {
         return params$.pipe(
             tap(() => (this.isLoading = true)),
             operatorFunction$,
             tap(() => (this.isLoading = false))
         );
+    }
+
+    /**
+     * @description
+     * Dado un observable de tipo T, se actualiza la propiedad isLoading
+     * al realizar la llamada async a la fuente y luego de terminada ésta.
+     * @param source$
+     */
+    public fetchContent$(source$: Observable<T>): Observable<T> {
+        this.isLoading = true;
+        return source$.pipe(tap(() => (this.isLoading = false)));
     }
 }
