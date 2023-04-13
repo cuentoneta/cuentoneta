5e65ebc2840573d80a250e15425963a326f732cd
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-04-13 02:30:30 -0300

    [#178] - Agrega visualización de fechas en locale es

--- src/app/components/story-card/story-card.component.ts
+++ src/app/components/story-card/story-card.component.ts
@@ -2,24 +2,31 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Story } from '../../models/story.model';
 import { Publication } from '../../models/storylist.model';
 
+// dayjs
+require('dayjs/locale/es');
+import dayjs from 'dayjs';
+dayjs.locale('es');
+
 @Component({
-    selector: 'cuentoneta-story-card',
-    templateUrl: './story-card.component.html',
-    styleUrls: ['./story-card.component.scss'],
+  selector: 'cuentoneta-story-card',
+  templateUrl: './story-card.component.html',
+  styleUrls: ['./story-card.component.scss'],
 })
 export class StoryCardComponent implements OnInit {
-    @Input() editionPrefix: string | undefined;
-    @Input() editionSuffix: string | undefined;
-    @Input() displayDate: boolean = false;
-    @Input() publication: Publication<Story> | undefined;
-    @Input() editionIndex: number = 0;
+  @Input() editionPrefix: string | undefined;
+  @Input() editionSuffix: string | undefined;
+  @Input() displayDate: boolean = false;
+  @Input() publication: Publication<Story> | undefined;
+  @Input() editionIndex: number = 0;
 
-    editionLabel: string = '';
+  editionLabel: string = '';
 
-    ngOnInit() {
-        this.editionLabel = this.editionPrefix + ' ' + this.editionIndex + ' - ' + this.publication?.publishingDate;
-        this.editionLabel = `${this.editionPrefix} ${this.editionIndex} ${
-            this.displayDate ? ' - ' + this.publication?.publishingDate : ''
-        }${this.editionSuffix ? ' | ' + this.editionSuffix : ''}`;
-    }
+  ngOnInit() {
+    this.editionLabel = `${this.editionPrefix} ${this.editionIndex} ${
+      this.displayDate
+        ? ' - ' +
+          dayjs(this.publication?.publishingDate).format('DD [de] MMMM, YYYY')
+        : ''
+    }${this.editionSuffix ? ' | ' + this.editionSuffix : ''}`;
+  }
 }
