9c3e485c23c672db2b3596e46459b014b53e19ed
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-04-13 12:19:19 -0300

    [#178] - Agrega componente skeleton para story cards

---
+++ src/app/components/story-card-skeleton/story-card-skeleton.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { StoryEditionDateLabelModule } from '../story-edition-date-label/story-edition-date-label.module';
+
+@Component({
+  selector: 'cuentoneta-story-card-skeleton',
+  standalone: true,
+  imports: [CommonModule, NgxSkeletonLoaderModule, StoryEditionDateLabelModule],
+  templateUrl: './story-card-skeleton.component.html',
+  styleUrls: ['./story-card-skeleton.component.scss'],
+})
+export class StoryCardSkeletonComponent {
+  @Input() animation: 'progress' | 'progress-dark' | 'pulse' | 'false' | false =
+    false;
+  @Input() displayDate: boolean = false;
+  @Input() editionLabel: string = '';
+}
