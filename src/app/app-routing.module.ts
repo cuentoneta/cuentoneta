4fbfd5d86d234dbdb79222e31c37d9e552b51834
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-04-14 08:57:53 -0300

    [#178] - Implementa objeto de definición de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,52 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+export const APP_ROUTE_TREE = {
+  HOME: 'home',
+  STORY: 'story',
+  STORYLIST: 'story-list',
+};
+
 const routes: Routes = [
-    {
-        path: 'home',
-        loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule),
-    },
-    {
-        path: 'story',
-        loadChildren: () => import('./pages/story/story.module').then((m) => m.StoryModule),
-    },
-    {
-      path: 'story-list',
-      loadChildren: () => import('./pages/story-list/story-list.module').then((m) => m.StoryListModule),
-    },
-    // {
-    //   path: 'about',
-    //   loadChildren: () => import('./pages/about/about.module').then((m) => m.AboutModule),
-    // },
-    // {
-    //   path: 'dmca',
-    //   loadChildren: () => import('./pages/dmca/dmca.module').then((m) => m.DmcaModule),
-    // },
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full',
-    },
+  {
+    path: APP_ROUTE_TREE.HOME,
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: APP_ROUTE_TREE.STORY,
+    loadChildren: () =>
+      import('./pages/story/story.module').then((m) => m.StoryModule),
+  },
+  {
+    path: APP_ROUTE_TREE.STORYLIST,
+    loadChildren: () =>
+      import('./pages/story-list/story-list.module').then(
+        (m) => m.StoryListModule
+      ),
+  },
+  // {
+  //   path: 'about',
+  //   loadChildren: () => import('./pages/about/about.module').then((m) => m.AboutModule),
+  // },
+  // {
+  //   path: 'dmca',
+  //   loadChildren: () => import('./pages/dmca/dmca.module').then((m) => m.DmcaModule),
+  // },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload', scrollPositionRestoration: 'enabled' })],
-    exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: 'reload',
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
