724d2cfb29bebe0a3502f64d9aafb55936c2de62
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-04-18 02:08:50 -0300

    [FE-#196] - Actualiza metadata OG y título al navegar a story

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -2,6 +2,7 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { combineLatest, switchMap, takeUntil } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 // Models
 import { Story } from '../../models/story.model';
@@ -15,35 +16,59 @@ import { DestroyedDirective } from '../../directives/destroyed.directive';
 import { FetchContentDirective } from '../../directives/fetch-content.directive';
 
 @Component({
-    selector: 'cuentoneta-story',
-    templateUrl: './story.component.html',
-    styleUrls: ['./story.component.scss'],
-    hostDirectives: [DestroyedDirective, FetchContentDirective],
+  selector: 'cuentoneta-story',
+  templateUrl: './story.component.html',
+  styleUrls: ['./story.component.scss'],
+  hostDirectives: [DestroyedDirective, FetchContentDirective],
 })
 export class StoryComponent {
-    fetchContentDirective = inject(FetchContentDirective<[Story, StoryList]>);
+  fetchContentDirective = inject(FetchContentDirective<[Story, StoryList]>);
 
-    story!: Story;
-    storylist!: StoryList;
+  story!: Story;
+  storylist!: StoryList;
 
-    dummyList = Array(10);
+  dummyList = Array(10);
 
-    constructor() {
-        const activatedRoute = inject(ActivatedRoute);
-        const destroyedDirective = inject(DestroyedDirective);
-        const storyService = inject(StoryService);
+  constructor() {
+    const activatedRoute = inject(ActivatedRoute);
+    const destroyedDirective = inject(DestroyedDirective);
+    const storyService = inject(StoryService);
+    const metaTagService = inject(Meta);
+    const titleService = inject(Title);
 
-        this.fetchContentDirective
-            .fetchContentWithSourceParams$(
-                activatedRoute.queryParams,
-                switchMap(({ slug, list }) =>
-                    combineLatest([storyService.getBySlug(slug), storyService.getLatest(list, 10)])
-                )
-            )
-            .pipe(takeUntil(destroyedDirective.destroyed$))
-            .subscribe(([story, storylist]) => {
-                this.story = story;
-                this.storylist = storylist;
-            });
-    }
+    this.fetchContentDirective
+      .fetchContentWithSourceParams$(
+        activatedRoute.queryParams,
+        switchMap(({ slug, list }) =>
+          combineLatest([
+            storyService.getBySlug(slug),
+            storyService.getLatest(list, 10),
+          ])
+        )
+      )
+      .pipe(takeUntil(destroyedDirective.destroyed$))
+      .subscribe(([story, storylist]) => {
+        this.story = story;
+        this.storylist = storylist;
+        titleService.setTitle(
+          `${story.title}, de ${story.author.name} - La Cuentoneta`
+        );
+        metaTagService.updateTag({
+          name: 'twitter:title',
+          content: `"${story.title}", de ${story.author.name} - La Cuentoneta`,
+        });
+        metaTagService.updateTag({
+          name: 'twitter:description',
+          content: `"${story.title}", de ${story.author.name}. Parte de la colección "${storylist.title}" en La Cuentoneta`,
+        });
+        metaTagService.updateTag({
+          property: 'og:title',
+          content: `"${story.title}", de ${story.author.name} - La Cuentoneta`,
+        });
+        metaTagService.updateTag({
+          property: 'og:description',
+          content: `"${story.title}", de ${story.author.name}. parte de la colección "${storylist.title}" en La Cuentoneta`,
+        });
+      });
+  }
 }
