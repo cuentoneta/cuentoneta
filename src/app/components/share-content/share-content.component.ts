4633da01b5b62d7f00f0bb3f5c5018deb968aa4b
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-04-18 01:53:53 -0300

    [FE-#160] - Genera adapters para compartir el contenido en las diversas plataformas de RRSS

--- src/app/components/share-content/share-content.component.ts
+++ src/app/components/share-content/share-content.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { StoryList } from '../../models/storylist.model';
-import { Story } from '../../models/story.model';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'cuentoneta-share-content',
@@ -11,58 +10,125 @@ import { Story } from '../../models/story.model';
   styleUrls: ['./share-content.component.scss'],
 })
 export class ShareContentComponent {
-  @Input() content!: Story | StoryList;
+  @Input() route: string = '';
+  @Input() params: { [p: string]: string } = {};
+  @Input() message: string = '';
+
   platforms: SharingPlatform[] = [
-    {
-      name: 'Facebook',
-      logo: 'facebook',
-      url: `https://www.facebook.com/share.php?u=${encodeURIComponent(
-        `https://www.cuentoneta.ar`
-      )}`,
-      content: 'abc',
-      target: 'facebook-share-dialog',
-      features: 'width=626,height=436',
-    },
-    {
-      name: 'Whatsapp',
-      logo: 'whatsapp',
-      target: '_blank',
-      content: 'Sumate a La Cuentoneta',
-      url: `https://wa.me?text=${'Sumate a La Cuentoneta: https://www.cuentoneta.ar'}`,
-    },
-    {
-      name: 'Twitter',
-      logo: 'twitter',
-      content: 'Sumate a La Cuentoneta',
-      url: `https://twitter.com/intent/tweet?&text=${'Sumate a La Cuentoneta: https://www.cuentoneta.ar'}`,
-    },
-    {
-      name: 'Instagram',
-      logo: 'instagram',
-      url: 'https://instagram.com',
-      content: 'abc',
-    },
-    {
-      name: 'Copiar Hipervínculo al Portapapeles',
-      logo: 'copy-link',
-      url: 'copy',
-      content: 'abc',
-    },
+    new FacebookPlatform(),
+    new WhatsappPlatform(),
+    new TwitterPlatform(),
+    // {
+    //   name: 'Instagram',
+    //   logo: 'instagram',
+    //   url: 'https://instagram.com',
+    // },
+    // {
+    //   name: 'Copiar Hipervínculo al Portapapeles',
+    //   logo: 'copy-link',
+    //   url: 'copy',
+    // },
   ];
 
   onShareToPlatformClicked(
     event: MouseEvent | KeyboardEvent,
     platform: SharingPlatform
   ) {
-    window.open(platform.url, platform.target, platform.features);
+    const urlParams = Object.keys(this.params)
+      .map((key) => `${key}=${this.params[key]}`)
+      .join('&');
+    window.open(
+      platform.generateSharingUrl(this.route, urlParams, this.message),
+      platform.target,
+      platform.features
+    );
   }
 }
 
 interface SharingPlatform {
   name: string;
   logo: string;
-  url: string;
+  platformApiUrl: string;
   target?: string;
   features?: string;
-  content: string;
+  generateSharingUrl(
+    appRoute: string,
+    urlParams: string,
+    message: string
+  ): string;
+}
+
+class FacebookPlatform implements SharingPlatform {
+  name = 'Facebook';
+  logo = 'facebook';
+  platformApiUrl = `https://www.facebook.com/share.php`;
+  target = 'facebook-share-dialog';
+  features = 'width=626,height=436';
+
+  generateSharingUrl(
+    appRoute: string,
+    urlParams: string,
+    message: string
+  ): string {
+    const url = encodeURIComponent(
+      `${environment.website}/${appRoute}?${urlParams}`
+    );
+    return `${this.platformApiUrl}?u=${url}`;
+  }
+}
+
+class WhatsappPlatform implements SharingPlatform {
+  name = 'Whatsapp';
+  logo = 'whatsapp';
+  platformApiUrl = `whatsapp://send/`;
+  target = '_blank';
+  features = '';
+
+  generateSharingUrl(
+    appRoute: string,
+    urlParams: string,
+    message: string
+  ): string {
+    const sharedUrl = `${environment.website}/${appRoute}?${urlParams}`;
+    const queryParams: { [key: string]: string } = {
+      text: `${message}:%20${sharedUrl}`,
+    };
+
+    const serializedApiQueryParams = Object.keys(queryParams).map(
+      (key) => `${key}=${queryParams[key]}`
+    );
+    const navigationUrl = `${
+      this.platformApiUrl
+    }?${serializedApiQueryParams.join('&')}`;
+    return navigationUrl;
+  }
+}
+
+class TwitterPlatform implements SharingPlatform {
+  name = 'Twitter';
+  logo = 'twitter';
+  platformApiUrl = `https://twitter.com/intent/tweet`;
+  target = '_blank';
+  features = '';
+
+  generateSharingUrl(
+    appRoute: string,
+    urlParams: string,
+    message: string
+  ): string {
+    const queryParams: { [key: string]: string } = {
+      url: encodeURIComponent(
+        `${environment.website}/${appRoute}?${urlParams}`
+      ),
+      text: message,
+    };
+
+    const serializedApiQueryParams = Object.keys(queryParams).map(
+      (key) => `${key}=${queryParams[key]}`
+    );
+    const navigationUrl = `${
+      this.platformApiUrl
+    }?${serializedApiQueryParams.join('&')}`;
+    return navigationUrl;
+  }
 }
--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -13,37 +13,44 @@ import { StoryService } from '../../providers/story.service';
 // Directives
 import { DestroyedDirective } from '../../directives/destroyed.directive';
 import { FetchContentDirective } from '../../directives/fetch-content.directive';
+import { APP_ROUTE_TREE } from '../../app-routing.module';
 
 @Component({
-    selector: 'cuentoneta-story',
-    templateUrl: './story.component.html',
-    styleUrls: ['./story.component.scss'],
-    hostDirectives: [DestroyedDirective, FetchContentDirective],
+  selector: 'cuentoneta-story',
+  templateUrl: './story.component.html',
+  styleUrls: ['./story.component.scss'],
+  hostDirectives: [DestroyedDirective, FetchContentDirective],
 })
 export class StoryComponent {
-    fetchContentDirective = inject(FetchContentDirective<[Story, StoryList]>);
+  readonly appRouteTree = APP_ROUTE_TREE;
+  fetchContentDirective = inject(FetchContentDirective<[Story, StoryList]>);
 
-    story!: Story;
-    storylist!: StoryList;
+  story!: Story;
+  storylist!: StoryList;
 
-    dummyList = Array(10);
+  dummyList = Array(10);
+  shareMessage: string = '';
 
-    constructor() {
-        const activatedRoute = inject(ActivatedRoute);
-        const destroyedDirective = inject(DestroyedDirective);
-        const storyService = inject(StoryService);
+  constructor() {
+    const activatedRoute = inject(ActivatedRoute);
+    const destroyedDirective = inject(DestroyedDirective);
+    const storyService = inject(StoryService);
 
-        this.fetchContentDirective
-            .fetchContentWithSourceParams$(
-                activatedRoute.queryParams,
-                switchMap(({ slug, list }) =>
-                    combineLatest([storyService.getBySlug(slug), storyService.getLatest(list, 10)])
-                )
-            )
-            .pipe(takeUntil(destroyedDirective.destroyed$))
-            .subscribe(([story, storylist]) => {
-                this.story = story;
-                this.storylist = storylist;
-            });
-    }
+    this.fetchContentDirective
+      .fetchContentWithSourceParams$(
+        activatedRoute.queryParams,
+        switchMap(({ slug, list }) =>
+          combineLatest([
+            storyService.getBySlug(slug),
+            storyService.getLatest(list, 10),
+          ])
+        )
+      )
+      .pipe(takeUntil(destroyedDirective.destroyed$))
+      .subscribe(([story, storylist]) => {
+        this.story = story;
+        this.storylist = storylist;
+        this.shareMessage = `Leí "${story.title}" de ${story.author.name} en La Cuentoneta y te lo comparto. Sumate a leer este y otros cuentos de la colección "${storylist.title}" en este link:`;
+      });
+  }
 }
