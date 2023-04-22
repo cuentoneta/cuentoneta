5de2ac2cc32f5b2ee23482b75a2dfa706055b321
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-04-22 13:38:29 -0300

    [TL] - Agrega Analytics de Vercel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { inject } from '@vercel/analytics';
+import { environment } from './environments/environment';
 @Component({
   selector: 'cuentoneta-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cuentoneta';
+  constructor() {
+    // Importa y configura el paquete de analytics de Vercel
+    inject({
+      mode: environment.production ? 'production' : 'development',
+    });
+  }
 }
