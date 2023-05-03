65c03f65ff02bfd01077fc9e1543fa7f1d545736
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-05-03 19:56:11 -0300

    [#93] - Implementa Angular Universal con Express

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
+function bootstrap() {
+     platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
+   };
+
+
+ if (document.readyState === 'complete') {
+   bootstrap();
+ } else {
+   document.addEventListener('DOMContentLoaded', bootstrap);
+ }
+ 
