8f0a20d040002bacecb8faa0a8478d115cf2723e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-05-04 01:49:55 -0300

    [#93] - Corrige nombre de index

--- src/ssr.server.ts
+++ src/ssr.server.ts
@@ -12,7 +12,7 @@ import { existsSync } from 'fs';
 export function app(): express.Express {
     const server = express();
     const distFolder = join(process.cwd(), 'dist/cuentoneta/browser');
-    const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+    const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index.html';
 
     // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
     server.engine('html', ngExpressEngine({
