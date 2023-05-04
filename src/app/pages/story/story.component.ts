4baa14d31cb8b9c159132e18df36d14c2e694a1d
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-05-04 02:55:28 -0300

    TEST - Agrego espera para actualizar metatags

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -1,7 +1,7 @@
 // Core
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, switchMap, takeUntil } from 'rxjs';
+import { combineLatest, Observable, switchMap, takeUntil } from 'rxjs';
 
 // Models
 import { Story } from '../../models/story.model';
@@ -15,6 +15,7 @@ import { DestroyedDirective } from '../../directives/destroyed.directive';
 import { FetchContentDirective } from '../../directives/fetch-content.directive';
 import { MetaTagsDirective } from '../../directives/meta-tags.directive';
 import { APP_ROUTE_TREE } from '../../app-routing.module';
+import { waitFor } from '../../functions/ssr.functions';
 
 @Component({
   selector: 'cuentoneta-story',
@@ -26,7 +27,7 @@ import { APP_ROUTE_TREE } from '../../app-routing.module';
     MetaTagsDirective,
   ],
 })
-export class StoryComponent {
+export class StoryComponent implements OnInit {
   readonly appRouteTree = APP_ROUTE_TREE;
   fetchContentDirective = inject(FetchContentDirective<[Story, StoryList]>);
 
@@ -34,37 +35,40 @@ export class StoryComponent {
   storylist!: StoryList;
 
   dummyList = Array(10);
-  shareContentParams: { [key: string]: string } = {}
+  shareContentParams: { [key: string]: string } = {};
   shareMessage: string = '';
 
-  constructor() {
+  constructor() {}
+
+  async ngOnInit(): Promise<void> {
     const activatedRoute = inject(ActivatedRoute);
     const destroyedDirective = inject(DestroyedDirective);
     const metaTagsDirective = inject(MetaTagsDirective);
     const storyService = inject(StoryService);
 
-    this.fetchContentDirective
-      .fetchContentWithSourceParams$(
-        activatedRoute.queryParams,
-        switchMap(({ slug, list }) =>
-          combineLatest([
-            storyService.getBySlug(slug),
-            storyService.getLatest(list, 10),
-          ])
+    const fetchData$: Observable<[Story, StoryList]> =
+      this.fetchContentDirective
+        .fetchContentWithSourceParams$(
+          activatedRoute.queryParams,
+          switchMap(({ slug, list }) =>
+            combineLatest([
+              storyService.getBySlug(slug),
+              storyService.getLatest(list, 10),
+            ])
+          )
         )
-      )
-      .pipe(takeUntil(destroyedDirective.destroyed$))
-      .subscribe(([story, storylist]) => {
-        this.story = story;
-        this.storylist = storylist;
-        metaTagsDirective.setTitle(
-          `${story.title}, de ${story.author.name} en La Cuentoneta`
-        );
-        metaTagsDirective.setDescription(
-          `"${story.title}", de ${story.author.name}. Parte de la colección "${storylist.title}" en La Cuentoneta: Una iniciativa que busca fomentar y hacer accesible la lectura digital.`
-        );
-        this.shareContentParams = { slug: story.slug, list: storylist.slug };
-        this.shareMessage = `Leí "${story.title}" de ${story.author.name} en La Cuentoneta y te lo comparto. Sumate a leer este y otros cuentos de la colección "${storylist.title}" en este link:`;
-      });
+        .pipe(takeUntil(destroyedDirective.destroyed$));
+
+    const [story, storylist] = await waitFor(fetchData$);
+    this.story = story;
+    this.storylist = storylist;
+    metaTagsDirective.setTitle(
+      `${story.title}, de ${story.author.name} en La Cuentoneta`
+    );
+    metaTagsDirective.setDescription(
+      `"${story.title}", de ${story.author.name}. Parte de la colección "${storylist.title}" en La Cuentoneta: Una iniciativa que busca fomentar y hacer accesible la lectura digital.`
+    );
+    this.shareContentParams = { slug: story.slug, list: storylist.slug };
+    this.shareMessage = `Leí "${story.title}" de ${story.author.name} en La Cuentoneta y te lo comparto. Sumate a leer este y otros cuentos de ${storylist.title} en La Cuentoneta: Una iniciativa que busca fomentar y hacer accesible la lectura digital.`;
   }
 }
