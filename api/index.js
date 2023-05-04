5ee898730064717eb85598af452b5246f34e39ec
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-05-03 23:51:29 -0300

    [#93] - Configuración de redirección

--- api/index.js
+++
@@ -1,3 +0,0 @@
-const server = require('../dist/cuentoneta/server/main');
-
-module.exports = server.app();
---
+++ api/index.ts
@@ -0,0 +1,2 @@
+import server from '../dist/cuentoneta/server/main';
+module.exports = server.app();
