6c9e121bb2fd787049a0153f88e3fdc76c64590d
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-05-05 02:23:56 -0300

    TEST - Uso de MacroTaskWrapperService

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -1,5 +1,5 @@
 // Core
-import { Component, inject, PLATFORM_ID } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { combineLatest, switchMap, takeUntil } from 'rxjs';
 
@@ -15,7 +15,8 @@ import { DestroyedDirective } from '../../directives/destroyed.directive';
 import { FetchContentDirective } from '../../directives/fetch-content.directive';
 import { MetaTagsDirective } from '../../directives/meta-tags.directive';
 import { APP_ROUTE_TREE } from '../../app-routing.module';
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { MacroTaskWrapperService } from '../../providers/macro-task-wrapper.service';
 
 @Component({
   selector: 'cuentoneta-story',
@@ -27,7 +28,7 @@ import { isPlatformBrowser } from '@angular/common';
     MetaTagsDirective,
   ],
 })
-export class StoryComponent {
+export class StoryComponent implements OnInit {
   readonly appRouteTree = APP_ROUTE_TREE;
   fetchContentDirective = inject(FetchContentDirective<[Story, StoryList]>);
 
@@ -35,43 +36,52 @@ export class StoryComponent {
   storylist!: StoryList;
 
   dummyList = Array(10);
-  shareContentParams: { [key: string]: string } = {}
+  shareContentParams: { [key: string]: string } = {};
   shareMessage: string = '';
 
-  constructor() {
-    const platformId = inject(PLATFORM_ID);
+  private platformId = inject(PLATFORM_ID);
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyedDirective = inject(DestroyedDirective);
+  private metaTagsDirective = inject(MetaTagsDirective);
+  private storyService = inject(StoryService);
+  private macroTaskWrapperService = inject(MacroTaskWrapperService);
 
-    if(!isPlatformBrowser(platformId)) {
-      return;
+  ngOnInit() {
+
+    if (isPlatformServer(this.platformId)) {
+      this.macroTaskWrapperService.wrapMacroTaskObservable(
+        'MyComponent.ngOnInit',
+        this.fetchData$()
+      );
+      // If any tasks have started outside of the component use this:
+      this.macroTaskWrapperService.awaitMacroTasks('StoryComponent.ngOnInit');
     }
 
-    const activatedRoute = inject(ActivatedRoute);
-    const destroyedDirective = inject(DestroyedDirective);
-    const metaTagsDirective = inject(MetaTagsDirective);
-    const storyService = inject(StoryService);
+    this.fetchData$().subscribe(([story, storylist]) => {
+      this.story = story;
+      this.storylist = storylist;
+      this.metaTagsDirective.setTitle(
+        `${story.title}, de ${story.author.name} en La Cuentoneta`
+      );
+      this.metaTagsDirective.setDescription(
+        `"${story.title}", de ${story.author.name}. Parte de la colección "${storylist.title}" en La Cuentoneta: Una iniciativa que busca fomentar y hacer accesible la lectura digital.`
+      );
+      this.shareContentParams = { slug: story.slug, list: storylist.slug };
+      this.shareMessage = `Leí "${story.title}" de ${story.author.name} en La Cuentoneta y te lo comparto. Sumate a leer este y otros cuentos de la colección "${storylist.title}" en este link:`;
+    });
+  }
 
-    this.fetchContentDirective
-        .fetchContentWithSourceParams$(
-            activatedRoute.queryParams,
-            switchMap(({ slug, list }) =>
-                combineLatest([
-                  storyService.getBySlug(slug),
-                  storyService.getLatest(list, 10),
-                ])
-            )
+  private fetchData$() {
+    return this.fetchContentDirective
+      .fetchContentWithSourceParams$<[Story, StoryList]>(
+        this.activatedRoute.queryParams,
+        switchMap(({ slug, list }) =>
+          combineLatest([
+            this.storyService.getBySlug(slug),
+            this.storyService.getLatest(list, 10),
+          ])
         )
-        .pipe(takeUntil(destroyedDirective.destroyed$))
-        .subscribe(([story, storylist]) => {
-          this.story = story;
-          this.storylist = storylist;
-          metaTagsDirective.setTitle(
-              `${story.title}, de ${story.author.name} en La Cuentoneta`
-          );
-          metaTagsDirective.setDescription(
-              `"${story.title}", de ${story.author.name}. Parte de la colección "${storylist.title}" en La Cuentoneta: Una iniciativa que busca fomentar y hacer accesible la lectura digital.`
-          );
-          this.shareContentParams = { slug: story.slug, list: storylist.slug };
-          this.shareMessage = `Leí "${story.title}" de ${story.author.name} en La Cuentoneta y te lo comparto. Sumate a leer este y otros cuentos de la colección "${storylist.title}" en este link:`;
-        });
+      )
+      .pipe(takeUntil(this.destroyedDirective.destroyed$));
   }
 }
