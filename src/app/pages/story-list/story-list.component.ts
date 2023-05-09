43c117e3ff495a737ab566ee97eaf481b8d9817f
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-05-09 10:32:03 -0300

    [#10] - Agrega discriminación para fetch SSR y CSR

--- src/app/pages/story-list/story-list.component.ts
+++ src/app/pages/story-list/story-list.component.ts
@@ -1,12 +1,14 @@
 // Core
-import { Component, inject } from '@angular/core';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap, takeUntil } from 'rxjs';
+import { Observable, switchMap, takeUntil } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 // Models
 import { StoryList } from '../../models/storylist.model';
 
 // Services
+import { MacroTaskWrapperService } from '../../providers/macro-task-wrapper.service';
 import { StoryService } from '../../providers/story.service';
 
 // Directives
@@ -29,26 +31,39 @@ export class StoryListComponent {
   storyList!: StoryList | undefined;
 
   constructor() {
+    const platformId = inject(PLATFORM_ID);
     const activatedRoute = inject(ActivatedRoute);
     const destroyedDirective = inject(DestroyedDirective);
     const metaTagsDirective = inject(MetaTagsDirective);
-
     const storyService = inject(StoryService);
+    const macroTaskWrapperService = inject(MacroTaskWrapperService);
 
-    this.fetchContentDirective
-      .fetchContentWithSourceParams$(
+    const fetchObservable$: Observable<StoryList> = this.fetchContentDirective
+      .fetchContentWithSourceParams$<StoryList>(
         activatedRoute.queryParams,
         switchMap(({ slug }) => {
           return storyService.getLatest(slug, 60);
         })
       )
-      .pipe(takeUntil(destroyedDirective.destroyed$))
-      .subscribe((storylist) => {
-        this.storyList = storylist;
-        metaTagsDirective.setTitle(`"${storylist.title}" en La Cuentoneta`);
-        metaTagsDirective.setDescription(
-          `Colección "${storylist.title}", una storylist en La Cuentoneta: Una iniciativa que busca fomentar y hacer accesible la lectura digital.`
+      .pipe(takeUntil(destroyedDirective.destroyed$));
+
+    // TODO: Mover discriminación entre client-side y server-side a directiva
+    // En base a si la plataforma es browser o server, utiliza el wrapper de macro tasks en el segundo caso
+    const storyList$ = isPlatformBrowser(platformId)
+      ? fetchObservable$
+      : macroTaskWrapperService.wrapMacroTaskObservable<StoryList>(
+          'StoryListComponent.fetchData',
+          fetchObservable$,
+          null,
+          'first-emit'
         );
-      });
+
+    storyList$.subscribe((storylist) => {
+      this.storyList = storylist;
+      metaTagsDirective.setTitle(`"${storylist.title}" en La Cuentoneta`);
+      metaTagsDirective.setDescription(
+        `Colección "${storylist.title}", una storylist en La Cuentoneta: Una iniciativa que busca fomentar y hacer accesible la lectura digital.`
+      );
+    });
   }
 }
