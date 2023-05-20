e7789faa9e24bae6a8c67a63af7f2194f5bd2da0
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2023-05-20 18:30:59 +0000

    Refactor: ngSrc and sizes added

---
+++ src/app/components/footer/footer.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { FooterComponent } from './footer.component';
+
+@NgModule({
+  declarations: [FooterComponent],
+  imports: [NgOptimizedImage],
+})
+export class FooterModule {}
