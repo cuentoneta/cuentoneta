d39390a9bc7c387e538e4669fb9391ecb2d5ef2f
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-05-28 22:26:34 -0300

    [#250] - Elimina interface redundante

--- src/app/models/storylist.model.ts
+++ src/app/models/storylist.model.ts
@@ -2,32 +2,30 @@ import { Story, StoryBase, StoryDTO } from './story.model';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 interface StorylistBase {
-    title: string;
+  title: string;
+  slug: string;
+  displayDates: boolean;
+  editionPrefix: string;
+  count: number;
+  comingNextLabel: string;
+  description?: string[];
+  language?: string;
+  featuredImage?: SanityImageSource;
+  images?: {
     slug: string;
-    displayDates: boolean;
-    editionPrefix: string;
-    count: number;
-    comingNextLabel: string;
-    description?: string[];
-    language?: string;
-    featuredImage?: SanityImageSource;
-    images?: StorylistImage[]
+    url: SanityImageSource;
+  }[];
 }
 export interface StoryList extends StorylistBase {
-    publications: Publication<Story>[];
+  publications: Publication<Story>[];
 }
 export interface StoryListDTO extends StorylistBase {
-    publications: Publication<StoryDTO>[];
+  publications: Publication<StoryDTO>[];
 }
 
 export interface Publication<T extends StoryBase> {
-    order: number;
-    published: boolean;
-    publishingDate?: string;
-    story: T;
-}
-
-export interface StorylistImage {
-    slug: string;
-    url: SanityImageSource;
+  order: number;
+  published: boolean;
+  publishingDate?: string;
+  story: T;
 }
