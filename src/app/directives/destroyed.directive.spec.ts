da26f7297dda9603ada0457e0a601f201dfccf1c
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-06-04 13:15:08 -0300

    [#272] - Remueve destroyedDirective

--- src/app/directives/destroyed.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DestroyedDirective } from './destroyed.directive';
-
-describe('DestroyedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DestroyedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
