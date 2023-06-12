b215614adefee2c08834c68700c4e91dd005b142
ErikGIovani <kingsunzu@outlook.com>
2023-06-12 13:27:29 +0000

    [#192] - Endpoint de og agregado

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,8 +1,13 @@
 import storyController from './story.controller';
+import ogController from './og.controller';
 
 export default [
   {
     path: '/story',
     controller: storyController,
+  },
+  {
+    path: '/og',
+    controller: ogController,
   },
 ];
