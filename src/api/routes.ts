b331edef954c130756cf347c61ca63bd9f8b500f
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-06-13 10:20:21 -0300

    [#285] - Registra ruta /storylist en APIs

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,8 +1,13 @@
 import storyController from './story.controller';
+import storylistController from './storylist.controller';
 
 export default [
   {
     path: '/story',
     controller: storyController,
+  },
+  {
+    path: '/storylist',
+    controller: storylistController,
   },
 ];
