46b48ade468a580b74d0153cb014d4f135f6aac3
Soledad Sasia <soledadsasia@gmail.com>
2023-06-29 12:01:04 -0300

    [#346] - Remueve la seccion Stories List en DmcaComponent

--- src/app/pages/dmca/dmca.component.ts
+++ src/app/pages/dmca/dmca.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Author } from 'src/app/models/author.model';
 
 @Component({
   selector: 'cuentoneta-dmca',
@@ -9,16 +8,4 @@ import { Author } from 'src/app/models/author.model';
   templateUrl: './dmca.component.html',
   styleUrls: ['./dmca.component.scss'],
 })
-export class DmcaComponent {
-  public linksList: OriginalLink[] = [];
-
-  
-  
-}
-
-export interface OriginalLink {
-  author: Author;
-  day: number;
-  originalLink: string;
-  title: string;
-}
+export class DmcaComponent {}
