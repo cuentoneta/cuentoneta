af2a349bfbe5738d13dae56fbf687eaafc429b01
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-06-30 10:05:51 -0300

    [#288] - Destaca story actual en vista en navbar de navegación de storylist

--- src/app/components/story-navigation-bar/story-navigation-bar.component.ts
+++ src/app/components/story-navigation-bar/story-navigation-bar.component.ts
@@ -1,36 +1,49 @@
-import { Component, Input } from '@angular/core';
-import { Publication, Storylist } from '../../models/storylist.model';
-import { Story } from '../../models/story.model';
+import { Component, inject, Input } from '@angular/core';
+import { Publication, Storylist } from '@models/storylist.model';
+import { Story } from '@models/story.model';
 import { APP_ROUTE_TREE } from '../../app-routing.module';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { StoryEditionDateLabelComponent } from '../story-edition-date-label/story-edition-date-label.component';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { NgIf, NgFor, CommonModule } from '@angular/common';
+import { combineLatest, of, switchMap } from 'rxjs';
 
 @Component({
-    selector: 'cuentoneta-story-navigation-bar',
-    templateUrl: './story-navigation-bar.component.html',
-    styleUrls: ['./story-navigation-bar.component.scss'],
-    standalone: true,
-    imports: [
-        CommonModule,
-        NgxSkeletonLoaderModule,
-        NgIf,
-        NgFor,
-        RouterLink,
-        StoryEditionDateLabelComponent,
-        StoryNavigationBarComponent
-    ],
+  selector: 'cuentoneta-story-navigation-bar',
+  templateUrl: './story-navigation-bar.component.html',
+  styleUrls: ['./story-navigation-bar.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgxSkeletonLoaderModule,
+    NgIf,
+    NgFor,
+    RouterLink,
+    StoryEditionDateLabelComponent,
+    StoryNavigationBarComponent,
+  ],
 })
 export class StoryNavigationBarComponent {
-    @Input() storylist!: Storylist;
+  @Input() storylist!: Storylist;
 
-    readonly appRouteTree = APP_ROUTE_TREE;
-    dummyList: null[] = Array(10);
+  readonly appRouteTree = APP_ROUTE_TREE;
+  dummyList: null[] = Array(10);
+  selectedStorySlug: string = '';
 
-    // ToDo: Separar card de cada cuento de la lista en su propio componente, para evitar usar un método en el template
-    getEditionLabel(publication: Publication<Story>, editionIndex: number = 0): string {
-        return `${this.storylist?.editionPrefix} ${editionIndex} ${this.storylist.displayDates ? ' - ' + publication.publishingDate : ''
-            }`;
-    }
+  constructor() {
+    const activatedRoute = inject(ActivatedRoute);
+    activatedRoute.queryParams.subscribe(
+      ({ slug, list }) => (this.selectedStorySlug = slug)
+    );
+  }
+
+  // ToDo: Separar card de cada cuento de la lista en su propio componente, para evitar usar un método en el template
+  getEditionLabel(
+    publication: Publication<Story>,
+    editionIndex: number = 0
+  ): string {
+    return `${this.storylist?.editionPrefix} ${editionIndex} ${
+      this.storylist.displayDates ? ' - ' + publication.publishingDate : ''
+    }`;
+  }
 }
