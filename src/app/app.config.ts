ca00fe4d621b39d28e5a19da6bc8faf9b2f77607
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-07-08 18:57:25 -0300

    [#360] - Transforma AppComponent a componente standalone

---
+++ src/app/app.config.ts
@@ -0,0 +1,31 @@
+import {
+  APP_ID,
+  APP_INITIALIZER,
+  ApplicationConfig,
+  importProvidersFrom,
+} from '@angular/core';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+} from '@angular/router';
+import { appRoutes } from './app.routes';
+import { ContentService } from './providers/content.service';
+import { StoryService } from './providers/story.service';
+import { HttpClientModule } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    ContentService,
+    StoryService,
+    { provide: APP_ID, useValue: 'serverApp' },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (contentService: ContentService) => () =>
+        contentService.fetchContentConfig(),
+      deps: [ContentService],
+      multi: true,
+    },
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(HttpClientModule),
+  ],
+};
