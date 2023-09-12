ffa96ae20d29c9e7172c97beadfb597d4a5e336d
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-09-12 11:43:54 -0300

    [#357] - Reestructura código de componente en nuevos métodos

--- src/app/components/story-navigation-bar/story-navigation-bar.component.ts
+++ src/app/components/story-navigation-bar/story-navigation-bar.component.ts
@@ -57,10 +57,132 @@ export class StoryNavigationBarComponent implements OnChanges, AfterViewInit {
   indexLength!: number;
   totalHeightElementsInView!: number;
 
+  ngOnChanges(changes: SimpleChanges) {
+    const storylist: Storylist = changes['storylist'].currentValue;
+    if (!!storylist) {
+      this.sliceDisplayedPublications(storylist.publications);
+    }
+  }
+
   ngAfterViewInit() {
-    this.childrenHeight =
-      this.renderer.selectRootElement(this.sliderInner.nativeElement)
-        .children[0].offsetHeight + 2;
+    this.loadCarousel();
+  }
+
+  // Anteriores elementos
+  prev() {
+    if (this.index <= this.totalHeightElementsInView) {
+      this.index = 0;
+      this.setButtonDisplay(this.prevButton, 'none');
+      this.setButtonDisplay(this.nextButton, 'block');
+    } else {
+      this.index -= this.childrenHeight * this.elementsInView;
+      this.setButtonDisplay(this.nextButton, 'block');
+    }
+    this.renderer.setStyle(
+      this.sliderInner.nativeElement,
+      'transform',
+      `translateY(-${this.index}px)`
+    );
+  }
+
+  // Siguientes elementos
+  next() {
+    this.index = this.getSelectedStoryIndex(this.storylist.publications);
+
+    if (
+      this.index + this.childrenHeight * this.elementsInView >=
+      this.totalHeight - this.totalHeightElementsInView
+    ) {
+      this.index = this.indexLength;
+      this.setButtonDisplay(this.nextButton, 'none');
+      this.setButtonDisplay(this.prevButton, 'block');
+    } else {
+      this.index += this.childrenHeight * this.elementsInView;
+      this.setButtonDisplay(this.prevButton, 'block');
+    }
+    this.renderer.setStyle(
+      this.sliderInner.nativeElement,
+      'transform',
+      `translateY(-${this.index}px)`
+    );
+  }
+
+  /**
+   * Este método se encarga de mostrar la lista de publicaciones de la navbar en base a la story actualmente en vista.
+   * Si la storylist tiene más de 10 publicaciones, se muestran las 10 publicaciones más cercanas a la story actualmente
+   * en vista tomando las 5 publicaciones anteriores y las 5 siguientes en el caso por defecto y ajustando los límites en
+   * caso de que la story actualmente en vista sea una de las primeras o de las últimas.
+   * @author Ramiro Olivencia <ramiro@olivencia.com.ar>
+   */
+  sliceDisplayedPublications(publications: Publication<Story>[]): void {
+    const numberOfDisplayedPublications = 10;
+
+    if (this.storylist.publications.length <= numberOfDisplayedPublications) {
+      this.displayedPublications = this.storylist.publications;
+      return;
+    }
+
+    const selectedStoryIndex = this.getSelectedStoryIndex(publications);
+
+    const lowerIndex =
+      selectedStoryIndex - numberOfDisplayedPublications / 2 < 0
+        ? 0
+        : selectedStoryIndex - numberOfDisplayedPublications / 2;
+    const upperIndex =
+      selectedStoryIndex + numberOfDisplayedPublications / 2 >
+      publications.length
+        ? publications.length
+        : selectedStoryIndex + numberOfDisplayedPublications / 2;
+
+    this.displayedPublications = this.storylist.publications.slice(
+      upperIndex === publications.length
+        ? publications.length - numberOfDisplayedPublications
+        : lowerIndex,
+      lowerIndex === 0 ? numberOfDisplayedPublications : upperIndex
+    );
+
+  }
+
+  // ToDo: Separar card de cada cuento de la lista en su propio componente, para evitar usar un método en el template
+  getEditionLabel(
+    publication: Publication<Story>,
+    editionIndex: number = 0
+  ): string {
+    return `${this.storylist?.editionPrefix} ${editionIndex} ${
+      this.storylist.displayDates ? ' - ' + publication.publishingDate : ''
+    }`;
+  }
+
+  /**
+   * Obtiene el índice de la historia actualimente visualizada en la lista.
+   * @param publications
+   * @private
+   */
+  private getSelectedStoryIndex(publications: Publication<Story>[]): number {
+    return publications.findIndex(
+      (publication) => publication.story.slug === this.selectedStorySlug
+    );
+  }
+
+  /**
+   * Utilizado para cambiar el valor del atributo display en los botones anteriores y siguientes de la navbar.
+   * @param element
+   * @param displayValue
+   * @private
+   */
+  private setButtonDisplay(element: ElementRef, displayValue: string) {
+    this.renderer.setStyle(element.nativeElement, 'display', displayValue);
+  }
+
+  /**
+   * Inicializa el carrusel y se encarga de renderizar, si corresponde, los botones para visualizar las historias anteriores y siguientes.
+   * @private
+   */
+  private loadCarousel() {
+    const sliderRootElement = this.renderer.selectRootElement(
+      this.sliderInner.nativeElement
+    );
+    this.childrenHeight = sliderRootElement.children[0].offsetHeight + 2;
     this.childrensLenght = this.renderer.selectRootElement(
       this.sliderInner.nativeElement
     ).childElementCount;
@@ -82,101 +204,10 @@ export class StoryNavigationBarComponent implements OnChanges, AfterViewInit {
 
     // Si hay menos elementos que en elementsInView no aparecen los botones.
     if (this.childrensLenght <= this.elementsInView) {
-      this.renderer.setStyle(this.prevButton.nativeElement, 'display', 'none');
-      this.renderer.setStyle(this.nextButton.nativeElement, 'display', 'none');
+      this.setButtonDisplay(this.prevButton, 'none');
+      this.setButtonDisplay(this.nextButton, 'none');
     } else {
-      this.renderer.setStyle(this.prevButton.nativeElement, 'display', 'none');
+      this.setButtonDisplay(this.prevButton, 'none');
     }
-  }
-
-  // Anteriores elemetos
-  prev() {
-    if (this.index <= this.totalHeightElementsInView) {
-      this.index = 0;
-      this.renderer.setStyle(this.prevButton.nativeElement, 'display', 'none');
-      this.renderer.setStyle(this.nextButton.nativeElement, 'display', 'block');
-    } else {
-      this.index -= this.childrenHeight * this.elementsInView;
-      this.renderer.setStyle(this.nextButton.nativeElement, 'display', 'block');
-    }
-    this.renderer.setStyle(
-      this.sliderInner.nativeElement,
-      'transform',
-      `translateY(-${this.index}px)`
-    );
-  }
-
-  // Siguientes elementos
-  next() {
-    if (
-      this.index + this.childrenHeight * this.elementsInView >=
-      this.totalHeight - this.totalHeightElementsInView
-    ) {
-      this.index = this.indexLength;
-      this.renderer.setStyle(this.nextButton.nativeElement, 'display', 'none');
-      this.renderer.setStyle(this.prevButton.nativeElement, 'display', 'block');
-    } else {
-      this.index += this.childrenHeight * this.elementsInView;
-      this.renderer.setStyle(this.prevButton.nativeElement, 'display', 'block');
-    }
-    this.renderer.setStyle(
-      this.sliderInner.nativeElement,
-      'transform',
-      `translateY(-${this.index}px)`
-    );
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    const storylist: Storylist = changes['storylist'].currentValue;
-    if (!!storylist) {
-      this.sliceDisplayedPublications(storylist.publications);
-    }
-  }
-
-  /**
-   * Este método se encarga de mostrar la lista de publicaciones de la navbar en base a la story actualmente en vista.
-   * Si la storylist tiene más de 10 publicaciones, se muestran las 10 publicaciones más cercanas a la story actualmente
-   * en vista tomando las 5 publicaciones anteriores y las 5 siguientes en el caso por defecto y ajustando los límites en
-   * caso de que la story actualmente en vista sea una de las primeras o de las últimas.
-   * @author Ramiro Olivencia <ramiro@olivencia.com.ar>
-   */
-  sliceDisplayedPublications(publications: Publication<Story>[]): void {
-    const numberOfDisplayedPublications = 10;
-
-    if (this.storylist.publications.length <= numberOfDisplayedPublications) {
-      this.displayedPublications = this.storylist.publications;
-      return;
-    }
-
-    const selectedStoryIndex = publications.findIndex(
-      (publication) => publication.story.slug === this.selectedStorySlug
-    );
-
-    const lowerIndex =
-      selectedStoryIndex - numberOfDisplayedPublications / 2 < 0
-        ? 0
-        : selectedStoryIndex - numberOfDisplayedPublications / 2;
-    const upperIndex =
-      selectedStoryIndex + numberOfDisplayedPublications / 2 >
-      publications.length
-        ? publications.length
-        : selectedStoryIndex + numberOfDisplayedPublications / 2;
-
-    this.displayedPublications = this.storylist.publications.slice(
-      upperIndex === publications.length
-        ? publications.length - numberOfDisplayedPublications
-        : lowerIndex,
-      lowerIndex === 0 ? numberOfDisplayedPublications : upperIndex
-    );
-  }
-
-  // ToDo: Separar card de cada cuento de la lista en su propio componente, para evitar usar un método en el template
-  getEditionLabel(
-    publication: Publication<Story>,
-    editionIndex: number = 0
-  ): string {
-    return `${this.storylist?.editionPrefix} ${editionIndex} ${
-      this.storylist.displayDates ? ' - ' + publication.publishingDate : ''
-    }`;
   }
 }
