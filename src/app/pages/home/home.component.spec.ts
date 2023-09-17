b19d22aca6e466ad2174d5a76889d02534662d1b
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-09-17 16:11:03 -0300

    [#358] - Corrige tests de HomeComponentSpec

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,22 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { render, RenderResult } from '@testing-library/angular';
+import { CommonModule, NgForOf, NgIf, NgOptimizedImage } from '@angular/common';
+import { StoryCardComponent } from '../../components/story-card/story-card.component';
+import { StorylistCardDeckComponent } from '../../components/storylist-card-deck/storylist-card-deck.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideMock } from '@testing-library/angular/jest-utils';
+import { ContentService } from '../../providers/content.service';
+import { FetchContentDirective } from '../../directives/fetch-content.directive';
 
 describe('HomeComponent', () => {
-    let component: HomeComponent;
-    let fixture: ComponentFixture<HomeComponent>;
+  let componentRender: RenderResult<HomeComponent, HomeComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [HomeComponent],
-        }).compileComponents();
-
-        fixture = TestBed.createComponent(HomeComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+  beforeEach(async () => {
+    componentRender = await render(HomeComponent, {
+      componentImports: [
+        CommonModule,
+        HttpClientTestingModule,
+        NgForOf,
+        NgIf,
+        NgOptimizedImage,
+        StoryCardComponent,
+        StorylistCardDeckComponent,
+        RouterTestingModule,
+      ],
+      componentProviders: [
+        { provide: ContentService, useClass: provideMock(ContentService) },
+        { provide: FetchContentDirective, useClass: provideMock(FetchContentDirective) },
+      ],
     });
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(componentRender).toBeTruthy();
+  });
 });
