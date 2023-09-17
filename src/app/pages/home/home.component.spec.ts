c0f7fb980f29666875774d7c347d544f8653dcd5
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-09-17 16:34:51 -0300

    [#358] - Corrige imports en testbed de HomeComponentSpec

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,13 +1,15 @@
 import { HomeComponent } from './home.component';
 import { render, RenderResult } from '@testing-library/angular';
 import { CommonModule, NgForOf, NgIf, NgOptimizedImage } from '@angular/common';
-import { StoryCardComponent } from '../../components/story-card/story-card.component';
-import { StorylistCardDeckComponent } from '../../components/storylist-card-deck/storylist-card-deck.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { provideMock } from '@testing-library/angular/jest-utils';
 import { ContentService } from '../../providers/content.service';
 import { FetchContentDirective } from '../../directives/fetch-content.directive';
+import { Component, Input } from '@angular/core';
+import { Publication, Storylist } from '@models/storylist.model';
+import { StorylistGridSkeletonConfig } from '@models/content.model';
+import { Story } from '@models/story.model';
 
 describe('HomeComponent', () => {
   let componentRender: RenderResult<HomeComponent, HomeComponent>;
@@ -20,8 +22,8 @@ describe('HomeComponent', () => {
         NgForOf,
         NgIf,
         NgOptimizedImage,
-        StoryCardComponent,
-        StorylistCardDeckComponent,
+        MockStoryCardComponent,
+        MockStorylistCardDeckComponent,
         RouterTestingModule,
       ],
       componentProviders: [
@@ -35,3 +37,35 @@ describe('HomeComponent', () => {
     expect(componentRender).toBeTruthy();
   });
 });
+
+
+
+@Component({
+  standalone: true,
+  selector: 'cuentoneta-story-card:not(p):not(a)',
+  template: '<div></div>',
+})
+class MockStoryCardComponent {
+  @Input() editionPrefix: string | undefined;
+  @Input() editionSuffix: string | undefined;
+  @Input() comingNextLabel: string = '';
+  @Input() displayDate: boolean = false;
+  @Input() publication: Publication<Story> | undefined;
+  @Input() editionIndex: number = 0;
+}
+
+@Component({
+  standalone: true,
+  selector: 'cuentoneta-storylist-card-deck:not(p)',
+  imports: [MockStoryCardComponent],
+  template: '',
+})
+class MockStorylistCardDeckComponent {
+  @Input() number: number = 6;
+  @Input() storylist: Storylist | undefined;
+  @Input() isLoading: boolean = false;
+  @Input() canNavigateToStorylist: boolean = false;
+  @Input() displayTitle: boolean = true;
+  @Input() displayFeaturedImage: boolean = false;
+  @Input() skeletonConfig: StorylistGridSkeletonConfig | undefined;
+}
