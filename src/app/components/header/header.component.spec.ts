9aa36a805ef28eb6eb7a462174d05bda292e87d1
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-09-17 00:33:25 -0300

    [#358] - Corrige tests de HeaderComponentSpec

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -2,13 +2,23 @@ import { render, screen } from '@testing-library/angular';
 import { HeaderComponent } from './header.component';
 import { ContentService } from 'src/app/providers/content.service';
 import { provideMock } from '@testing-library/angular/jest-utils';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
-describe('HeadercComponent', () => {
+describe('HeaderComponent', () => {
   test('should render Header component', async () => {
-    await render(HeaderComponent, {
-      componentProviders: [provideMock(ContentService)]
+    const component = await render(HeaderComponent, {
+      componentImports: [
+        CommonModule,
+        NgOptimizedImage,
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ],
+      componentProviders: [provideMock(ContentService)],
     });
-    expect(screen).toBeTruthy();
+
+    expect(component).toBeTruthy();
     expect(screen.getByAltText(/Cuentoneta/)).toBeTruthy();
     expect(screen.getByRole('link', { name: 'Inicio' })).toHaveProperty('href', expect.stringMatching(/home/))
     expect(screen.getByRole('link', { name: 'Nosotros' })).toHaveProperty('href', expect.stringMatching(/about/));
