49a42067a9baf9fc1360c4653333c571d744451d
Ramiro Olivencia <ramiro@olivencia.com.ar>
2023-11-25 13:25:15 -0300

    [#114] - Actualiza función mapAuthor para retornar objeto AuthorDAO

--- src/api/_utils/functions.ts
+++ src/api/_utils/functions.ts
@@ -3,31 +3,30 @@ import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { AuthorDAO } from '../_models/author-dao.model';
 import { ForewordDAO } from '../_models/story-dao.model';
 import imageUrlBuilder from '@sanity/image-url';
+import { baseLanguage } from '../../../cms/utils/localization';
 
-export function mapAuthor(authorDAO: AuthorDAO) {
-    const result = {
-        id: authorDAO._id,
-        biography: authorDAO.bio,
-        nationality: authorDAO.nationality,
-        fullBioUrl: authorDAO.fullBioUrl,
-        imageUrl: urlFor(authorDAO.image).url(),
-        name: authorDAO.name,
-    };
-
-    if (authorDAO.nationality?.country && authorDAO.nationality?.flag) {
-        result.nationality = {
-            country: authorDAO.nationality?.country,
-            flag: urlFor(authorDAO.nationality?.flag)?.url(),
-        };
-    }
-
-    return result;
+export function mapAuthor(rawAuthorData: any, language?: string): AuthorDAO {
+  return {
+    id: rawAuthorData._id,
+    nationality: {
+      country: rawAuthorData.nationality?.country,
+      flag: urlFor(rawAuthorData.nationality?.flag)?.url(),
+    },
+    fullBioUrl: rawAuthorData.fullBioUrl,
+    imageUrl: urlFor(rawAuthorData.image).url(),
+    name: rawAuthorData.name,
+    biography: rawAuthorData.biography
+      ? rawAuthorData.biography[language || baseLanguage!.id]
+      : undefined,
+  };
 }
 
 export function mapPrologues(prologuesDTO: ForewordDAO[]) {
-    return prologuesDTO ? prologuesDTO.map((x) => ({ reference: x.fwAuthor, text: x.fwText })) : [];
+  return prologuesDTO
+    ? prologuesDTO.map((x) => ({ reference: x.fwAuthor, text: x.fwText }))
+    : [];
 }
 
 export function urlFor(source: SanityImageSource) {
-    return imageUrlBuilder(client).image(source);
+  return imageUrlBuilder(client).image(source);
 }
