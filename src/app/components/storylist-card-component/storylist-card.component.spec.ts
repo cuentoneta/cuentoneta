59a673bf2821bdb449f30fe79b2172c10fb6549e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-12-15 00:35:38 -0300

    [#489] - Agrega componente StorylistCard

---
+++ src/app/components/storylist-card-component/storylist-card.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { StorylistCardComponent } from './storylist-card.component';
+
+describe('StorylistCardComponent', () => {
+  let component: StorylistCardComponent;
+  let fixture: ComponentFixture<StorylistCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [StorylistCardComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(StorylistCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/components/storylist-card-component/storylist-card.component.ts
+++ src/app/components/storylist-card-component/storylist-card.component.ts
@@ -2,11 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'cuentoneta-storylist-card-component',
+  selector: 'cuentoneta-storylist-card',
   standalone: true,
   imports: [CommonModule],
   template: `<p>storylist-card-component works!</p>`,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StorylistCardComponentComponent {}
+export class StorylistCardComponent{}
