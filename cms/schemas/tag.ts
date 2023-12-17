d54efae65b3aee1ca457d242e02eb4de891bfb5f
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-12-17 19:25:02 -0300

    [#491] - Define campo icon usando plugin sanity-plugin-icon-picker

--- cms/schemas/tag.ts
+++ cms/schemas/tag.ts
@@ -1,25 +1,54 @@
 import { TagIcon } from '@sanity/icons';
+import { preview } from 'sanity-plugin-icon-picker';
 
 export default {
   name: 'tag',
   title: 'Etiquetas',
   type: 'document',
   icon: TagIcon,
+  preview: {
+    select: {
+      title: 'title',
+      description: 'description',
+      provider: 'icon.provider',
+      name: 'icon.name',
+    },
+    prepare(selection) {
+      return {
+        title: selection.title,
+        subtitle: selection.description,
+        media: preview(selection),
+      };
+    },
+  },
   fields: [
     {
-      name: 'name',
-      title: 'Nombre',
+      name: 'title',
+      title: 'Título',
       type: 'string',
     },
     {
       name: 'slug',
       title: 'Slug',
       type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'description',
+      title: 'Descripción',
+      type: 'string',
     },
     {
       name: 'icon',
       title: 'Icono',
-      type: 'image',
+      type: 'iconPicker',
+      options: {
+        storeSvg: true
+      }
     },
   ],
 };
