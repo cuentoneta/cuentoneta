35047f6e41a08de9f30b1341a64d265bf13fa2c6
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-12-17 18:55:10 -0300

    [#489] - Agrega template y estilos básicos de componente

--- src/app/components/storylist-card-component/storylist-card.component.ts
+++ src/app/components/storylist-card-component/storylist-card.component.ts
@@ -1,12 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { StorylistCard } from '@models/storylist.model';
+import { BadgeComponent } from '../badge/badge.component';
 
 @Component({
   selector: 'cuentoneta-storylist-card',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>storylist-card-component works!</p>`,
-  styles: [],
+  imports: [CommonModule, NgOptimizedImage, BadgeComponent],
+  template: `
+    <header>
+      <img
+        [ngSrc]="storylist?.images?.[0]?.url ?? ''"
+        width="602"
+        height="240"
+      />
+    </header>
+    <article class="p-5">
+      <section class="flex flex-col gap-4">
+        <h1 class="card-title">{{ storylist?.title }}</h1>
+        <p class="description">{{ storylist?.description }}</p>
+        <hr />
+        <footer class="flex justify-end">
+          <cuentoneta-badge [label]="'Staff Picks'" />
+        </footer>
+      </section>
+    </article>
+  `,
+  styleUrl: './storylist-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StorylistCardComponent{}
+export class StorylistCardComponent {
+  @Input() storylist: Partial<StorylistCard> | null = null;
+}
