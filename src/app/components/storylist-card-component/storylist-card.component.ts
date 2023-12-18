72661f766f167c49c4afabde7ab72362d7595388
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-12-18 08:56:09 -0300

    [#491] - Agrega estilos y funcionalidad para navegación

--- src/app/components/storylist-card-component/storylist-card.component.ts
+++ src/app/components/storylist-card-component/storylist-card.component.ts
@@ -2,28 +2,39 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { StorylistCard } from '@models/storylist.model';
 import { BadgeComponent } from '../badge/badge.component';
+import { RouterLink } from '@angular/router';
+import { APP_ROUTE_TREE } from '../../app.routes';
 
 @Component({
   selector: 'cuentoneta-storylist-card',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, BadgeComponent],
+  imports: [CommonModule, NgOptimizedImage, BadgeComponent, RouterLink],
   template: `
-    <header>
-      <img
-        [ngSrc]="storylist?.images?.[0]?.url ?? ''"
-        width="602"
-        height="240"
-      />
-    </header>
-    <article class="p-5">
-      <section class="flex flex-col gap-4">
-        <h1 class="card-title">{{ storylist?.title }}</h1>
-        <p class="description">{{ storylist?.description }}</p>
-        <hr />
-        <footer class="flex justify-end">
-          <cuentoneta-badge [label]="'Staff Picks'" />
-        </footer>
-      </section>
+    <article>
+      <div
+          class="navigation-link"
+        [routerLink]="['/' + appRouteTree['STORYLIST']]"
+        [queryParams]="{ slug: storylist?.slug }"
+      >
+        <header>
+          <img
+            [ngSrc]="storylist?.images?.[0]?.url ?? ''"
+            width="602"
+            height="240"
+          />
+        </header>
+        <section class="flex flex-col gap-4 pt-5 px-4">
+          <h1 class="card-title">{{ storylist?.title }}</h1>
+          <p class="description">{{ storylist?.description }}</p>
+          <hr />
+        </section>
+      </div>
+      <footer class="flex justify-end pt-4 px-5 pb-5">
+        @if (storylist?.tags?.length > 0) { @for (tag of storylist?.tags;track
+        tag.slug) {
+        <cuentoneta-badge [tag]="tag" [showIcon]="true" class="ml-3"/>
+        } }
+      </footer>
     </article>
   `,
   styleUrl: './storylist-card.component.scss',
@@ -31,4 +42,6 @@ import { BadgeComponent } from '../badge/badge.component';
 })
 export class StorylistCardComponent {
   @Input() storylist: Partial<StorylistCard> | null = null;
+
+  protected readonly appRouteTree = APP_ROUTE_TREE;
 }
