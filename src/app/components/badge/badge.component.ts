25789c85b43ad930337e3ed2d692f0bc2b71eac7
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-12-18 07:46:31 -0300

    [#491] - Agrega estilos y tooltip a componente badge

--- src/app/components/badge/badge.component.ts
+++ src/app/components/badge/badge.component.ts
@@ -1,40 +1,37 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Tag } from '@models/tag.model';
+import { BypassHtmlSanitizerPipe } from '../../pipes/bypass-html-sanitizer.pipe';
+import { TooltipDirective } from '../../directives/tooltip.directive';
 
 @Component({
   selector: 'cuentoneta-badge',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
-  template: `<span class="flex items-center gap-1"
-    ><svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="14"
-      height="14"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      data-id="28"
-    >
-      <polygon
-        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-      ></polygon>
-    </svg>
-    {{ label }}
+  hostDirectives: [TooltipDirective],
+  imports: [BypassHtmlSanitizerPipe, CommonModule, NgOptimizedImage],
+  template: `<span class="flex items-center gap-1">
+    @if (showIcon && !!tag?.icon?.svg) {
+    <div [outerHTML]="tag?.icon?.svg ?? '' | bypassHtmlSanitizer"></div>
+    }
+    {{ tag.title }}
   </span> `,
   styleUrls: ['./badge.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class BadgeComponent {
-  @Input() label: string = '';
-  @Input() tag: Tag | null = null;
-  @Input() includeIcon: boolean = false;
+export class BadgeComponent implements OnInit {
+  @Input({ required: true }) tag!: Tag;
+  @Input() showIcon: boolean = false;
 
-  icon = '⭐';
-}
+  private tooltipDirective = inject(TooltipDirective);
 
-interface Tag {
-  label: string;
-  iconUrl: string;
+  ngOnInit() {
+    this.tooltipDirective.text = this.tag.description;
+    this.tooltipDirective.position = 'top';
+  }
 }
