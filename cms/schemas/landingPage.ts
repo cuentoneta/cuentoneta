bac5c6c6241cbd625a5a2e4eb10bd662c0c363d8
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-12-19 02:51:19 -0300

    [#496] - Agrega preview y validaciones al schema LandingPage

--- cms/schemas/landingPage.ts
+++ cms/schemas/landingPage.ts
@@ -5,16 +5,31 @@ export default {
   title: 'Página de Inicio',
   type: 'document',
   icon: CodeBlockIcon,
+  preview: {
+    select: {
+      config: 'config',
+      active: 'active',
+    },
+    prepare(selection) {
+      const { config, active } = selection;
+      return {
+        title: `${config}`,
+        subtitle: active ? 'Activa' : 'Inactiva',
+      };
+    },
+  },
   fields: [
     {
       name: 'config',
       title: 'Configuración',
       type: 'string',
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'active',
       title: 'Activa',
       type: 'boolean',
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'previews',
