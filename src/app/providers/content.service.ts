226eb727717ae3f5db27fbd7802e2c40c2760ddd
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-12-20 19:57:34 -0300

    [#498] - Agrega método fetchStorylistCards

--- src/app/providers/content.service.ts
+++ src/app/providers/content.service.ts
@@ -3,31 +3,34 @@ import { inject, Injectable } from '@angular/core';
 import { combineLatest, map, Observable, of, tap } from 'rxjs';
 
 // Interfaces
-import {
-  StorylistCardDeck,
-  StorylistDeckConfig,
-} from '@models/content.model';
+import { StorylistCardDeck, StorylistDeckConfig } from '@models/content.model';
 import { Storylist } from '@models/storylist.model';
 
 // Providers
 import { environment } from '../environments/environment';
 import { StorylistService } from './storylist.service';
 
+interface LandingPageContent {
+  cards: StorylistDeckConfig[];
+  previews: StorylistDeckConfig[];
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ContentService {
-  private _contentConfig: StorylistDeckConfig[] = [];
+  private readonly prefix = `${environment.apiUrl}api/content`;
+  private _contentConfig: LandingPageContent = { cards: [], previews: [] };
 
   // Services
   private storylistService = inject(StorylistService);
 
-  get contentConfig(): StorylistDeckConfig[] {
+  get contentConfig(): LandingPageContent {
     return this._contentConfig;
   }
 
-  public fetchContentConfig(): Observable<StorylistDeckConfig[]> {
-    return of(environment.contentConfig as StorylistDeckConfig[]).pipe(
+  public fetchContentConfig(): Observable<LandingPageContent> {
+    return of(environment.contentConfig as LandingPageContent).pipe(
       tap((contentConfig) => {
         this._contentConfig = contentConfig;
       })
@@ -51,14 +54,41 @@ export class ContentService {
    * cards de cada storylist.
    */
   public fetchStorylistDecks(): Observable<StorylistCardDeck[]> {
-    const configs = this.contentConfig;
+    const previewConfigs = this.contentConfig.previews;
+
     return combineLatest(
-      [...configs].map((storylistDeckConfig) =>
-        this.storylistService.getPreview(storylistDeckConfig.slug)
+      [...previewConfigs].map((config) =>
+        this.storylistService.getPreview(config.slug)
       )
     ).pipe(
       map((storylists) =>
-        configs.map(
+        previewConfigs.map(
+          (contentConfig): StorylistCardDeck => ({
+            ...contentConfig,
+            storylist: storylists
+              .filter((storylist) => storylist.slug === contentConfig.slug)
+              .pop() as Storylist,
+          })
+        )
+      )
+    );
+  }
+
+  /**
+   * De forma similar a fetchStorylistDecks(), este método se encarga de obtener la
+   * lista de storylists referenciada en los objetos de configuración para mostrar
+   * la información de las mismas en cards en la landing page.
+   */
+  public fetchStorylistCards(): Observable<StorylistCardDeck[]> {
+    const cardConfigs = this.contentConfig.cards;
+
+    return combineLatest(
+      [...cardConfigs].map((config) =>
+        this.storylistService.getPreview(config.slug)
+      )
+    ).pipe(
+      map((storylists) =>
+        cardConfigs.map(
           (contentConfig): StorylistCardDeck => ({
             ...contentConfig,
             storylist: storylists
