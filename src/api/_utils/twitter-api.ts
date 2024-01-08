31f3aec23b26d21e3e8a9ee446e773af2904265e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-01-08 11:22:03 -0300

    [#517] - Agrega función de utilidad para obtener información de tweets

--- src/api/_utils/twitter-api.ts
+++ src/api/_utils/twitter-api.ts
@@ -1,9 +1,15 @@
-import { Rettiwt, Tweet } from 'rettiwt-api';
-import { environment } from '../_helpers/environment'
+import { Rettiwt } from 'rettiwt-api';
+import { environment } from '../_helpers/environment';
+import { Media } from '@models/media.model'
 
-export async function getTweetData(id: string): Promise<Tweet> {
+export async function getTweetData(
+	// TODO: Tipar parámetro de la función
+	spaceData: any,
+): Promise<Media> {
 	const rettiwt = new Rettiwt({
 		apiKey: environment.twitter.apiKey,
 	});
-	return await rettiwt.tweet.details(id);
+
+	const tweetData = await rettiwt.tweet.details(spaceData.spacesRecordingId);
+	return { title: spaceData.title, type: 'spaceRecording', data: tweetData, url: spaceData.spacesRecordingUrl };
 }
