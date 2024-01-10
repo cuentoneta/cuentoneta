9ce95873d00aa0f7f69f99fec68fb8cbe30bf20d
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-01-09 23:48:32 -0300

    [#517] - Agrega modelo Media y tipos derivados

---
+++ src/app/models/media.model.ts
@@ -0,0 +1,26 @@
+import { ITweet } from 'rettiwt-api';
+
+/**
+ * Modelos relacionados a los distintos tipos de contenido multimedia que se
+ * pueden encontrar vinculados a una Story o Storylist.
+ *
+ * La interfaz base Media define los atributos comunes a todos los tipos de
+ * contenido multimedia y debe ser usada para definir el tipo de la propiedad media en DTOs.
+ *
+ * El tipo MediaTypes define la unión de tipos de contenido multimedia que se pueden
+ * encontrar vinculados a una instancia definida de Story o Storylist.
+ */
+
+export interface Media {
+	title: string;
+	url: string;
+	type: MediaTypeKey;
+	data?: unknown;
+}
+
+export interface SpaceRecording extends Media {
+	data: ITweet & { duration: string };
+}
+
+export type MediaTypes = SpaceRecording;
+export type MediaTypeKey = 'spaceRecording';
