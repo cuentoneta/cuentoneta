768499f78ea4c442af50fe48cb0642a3a941d348
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-01-29 14:46:14 -0300

    [#519] - Agrega propiedad resources a modelo Story

--- src/app/models/story.model.ts
+++ src/app/models/story.model.ts
@@ -2,6 +2,7 @@ import { Author, AuthorDTO } from './author.model';
 import { Prologue, PrologueDTO } from './prologue.model';
 import { BlockContent } from '@models/block-content.model';
 import { Media, MediaTypes } from '@models/media.model'
+import { Resource } from '@models/resource.model'
 
 export interface StoryBase {
 	id: number;
@@ -12,6 +13,7 @@ export interface StoryBase {
 	videoUrl?: string;
 	badLanguage?: boolean;
 	language: string;
+	resources?: Resource[]
 }
 
 export interface Story extends StoryBase {
