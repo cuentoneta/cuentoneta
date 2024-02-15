640e782c9ef7542fe98f694d836e551abac0f212
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2024-02-15 01:43:53 +0000

    [#607] - Clases de tailwind agregadas al story.component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,28 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    screens: {
-      sm: '640px',
-      md: '1024px',
-      lg: '1280px',
-      xl: '1536px',
-    },
-    extend: {},
-  },
-  plugins: [],
+	content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'), ...createGlobPatternsForDependencies(__dirname)],
+	theme: {
+		colors: {
+			'primary-500': 'hsl(21, 57%, 44%)',
+			'gray-50': 'hsl(0, 0%, 98%)',
+			'gray-600': 'hsl(240, 5%, 34%)',
+		},
+		screens: {
+			sm: '640px',
+			md: '1024px',
+			lg: '1280px',
+			xl: '1536px',
+		},
+		extend: {
+			fontFamily: {
+				inter: ['Inter', 'sans-serif'],
+				'source-serift': ['Source Serif Pro', 'sans-serif'],
+			},
+			spacing: {
+				15: '60px',
+			},
+		},
+	},
+	plugins: [],
 };
