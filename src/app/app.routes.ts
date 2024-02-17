76fa7580b72d39bc768bfba5b899123520c78cea
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2024-02-17 23:52:05 +0000

    [#621] - queryParams cambiados por params

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,45 +1,40 @@
 import { Routes } from '@angular/router';
 
 export const APP_ROUTE_TREE: { [key: string]: string } = {
-    HOME: 'home',
-    STORY: 'story',
-    STORYLIST: 'storylist',
-    'STORY-LIST': 'story-list', // Ruta definida por cuestiones de retrocompatibilidad. Redirecciona a /storylist - RO | 2023-06-21
+	HOME: 'home',
+	STORY: 'story',
+	STORYLIST: 'storylist',
+	'STORY-LIST': 'story-list', // Ruta definida por cuestiones de retrocompatibilidad. Redirecciona a /storylist - RO | 2023-06-21
 };
 
 export const appRoutes: Routes = [
-    {
-        path: APP_ROUTE_TREE['HOME'],
-        loadComponent: () =>
-            import('./pages/home/home.component').then((m) => m.HomeComponent),
-    },
-    {
-        path: APP_ROUTE_TREE['STORY'],
-        loadComponent: () =>
-            import('./pages/story/story.component').then((m) => m.StoryComponent),
-    },
-    {
-        path: APP_ROUTE_TREE['STORYLIST'],
-        loadComponent: () =>
-            import('./pages/storylist/storylist.component').then((m) => m.StorylistComponent),
-    },
-    {
-        path: APP_ROUTE_TREE['STORY-LIST'],
-        redirectTo: APP_ROUTE_TREE['STORYLIST'],
-    },
-    {
-        path: 'about',
-        loadComponent: () =>
-            import('./pages/about/about.component').then((m) => m.AboutComponent),
-    },
-    {
-        path: 'dmca',
-        loadComponent: () =>
-            import('./pages/dmca/dmca.component').then((m) => m.DmcaComponent),
-    },
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full',
-    },
+	{
+		path: APP_ROUTE_TREE['HOME'],
+		loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+	},
+	{
+		path: `${APP_ROUTE_TREE['STORY']}/:list/:slug`,
+		loadComponent: () => import('./pages/story/story.component').then((m) => m.StoryComponent),
+	},
+	{
+		path: `${APP_ROUTE_TREE['STORYLIST']}/:slug`,
+		loadComponent: () => import('./pages/storylist/storylist.component').then((m) => m.StorylistComponent),
+	},
+	{
+		path: `${APP_ROUTE_TREE['STORY-LIST']}/:slug`,
+		redirectTo: `${APP_ROUTE_TREE['STORYLIST']}/:slug`,
+	},
+	{
+		path: 'about',
+		loadComponent: () => import('./pages/about/about.component').then((m) => m.AboutComponent),
+	},
+	{
+		path: 'dmca',
+		loadComponent: () => import('./pages/dmca/dmca.component').then((m) => m.DmcaComponent),
+	},
+	{
+		path: '',
+		redirectTo: 'home',
+		pathMatch: 'full',
+	},
 ];
