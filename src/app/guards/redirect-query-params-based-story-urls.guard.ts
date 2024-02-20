69a391534c762a8f2344576aea21b568f0579565
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-02-20 05:08:11 -0300

    [#621] - Variables inline en guard

--- src/app/guards/redirect-query-params-based-story-urls.guard.ts
+++ src/app/guards/redirect-query-params-based-story-urls.guard.ts
@@ -2,10 +2,8 @@ import type { ActivatedRouteSnapshot, UrlTree } from '@angular/router';
 import { createUrlTreeFromSnapshot } from '@angular/router';
 
 export const redirectQueryParamsBasedStoryUrlsGuard = (activatedRoute: ActivatedRouteSnapshot): UrlTree => {
-	const url = createUrlTreeFromSnapshot(activatedRoute, [
+	return createUrlTreeFromSnapshot(activatedRoute, [
 		activatedRoute.queryParams['slug'],
 		activatedRoute.queryParams['list'],
 	]);
-
-	return url;
 };
