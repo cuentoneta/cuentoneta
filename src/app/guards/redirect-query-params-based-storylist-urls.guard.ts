c2090ac2d953ca56d7078b83ea80b0b76ab38943
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-02-20 05:08:02 -0300

    [#621] - Agrega guard para storylists

---
+++ src/app/guards/redirect-query-params-based-storylist-urls.guard.ts
@@ -0,0 +1,6 @@
+import type { ActivatedRouteSnapshot, UrlTree } from '@angular/router';
+import { createUrlTreeFromSnapshot } from '@angular/router';
+
+export const redirectQueryParamsBasedStorylistUrlsGuard = (activatedRoute: ActivatedRouteSnapshot): UrlTree => {
+    return createUrlTreeFromSnapshot(activatedRoute, [activatedRoute.queryParams['slug']]);
+};
