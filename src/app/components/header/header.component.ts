8233da179c30c784a91a6d024897d031f8344506
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-03-18 18:06:43 -0300

    [#593] - Migra estilos de nav y título

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,31 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { Storylist } from '@models/storylist.model';
 import { ContentService } from '../../providers/content.service';
-import { APP_ROUTE_TREE } from '../../app.routes';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'cuentoneta-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    NgOptimizedImage
-  ]
+	selector: 'cuentoneta-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
+	standalone: true,
+	imports: [CommonModule, RouterModule, NgOptimizedImage],
 })
 export class HeaderComponent {
-  readonly appRouteTree = APP_ROUTE_TREE;
-  lists: Pick<Storylist, 'title' | 'slug'>[] = [];
-  displayMenu: boolean = false;
-  constructor() {
-    const contentService = inject(ContentService);
-    this.lists = contentService.getNavLists();
-  }
+	readonly navLinks: { title: string; route: string }[] = [
+		{ title: 'Inicio', route: '/home' },
+		{ title: 'Nosotros', route: '/about' },
+	];
+	lists: Pick<Storylist, 'title' | 'slug'>[] = [];
+	displayMenu: boolean = false;
+	constructor() {
+		const contentService = inject(ContentService);
+		this.lists = contentService.getNavLists();
+	}
 
-  onMenuTogglerClicked(event: MouseEvent) {
-    this.displayMenu = !this.displayMenu;
-  }
+	onMenuTogglerClicked(event: MouseEvent) {
+		this.displayMenu = !this.displayMenu;
+	}
 }
