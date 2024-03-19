8e486d45c47ba18dd1d9cc277791f9e39e762c97
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-03-19 16:44:05 -0300

    [#599] - Adapta componente a template inline

--- src/app/components/story-edition-date-label/story-edition-date-label.component.ts
+++ src/app/components/story-edition-date-label/story-edition-date-label.component.ts
@@ -3,7 +3,20 @@ import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
 	selector: 'cuentoneta-story-edition-date-label[label]',
-	templateUrl: './story-edition-date-label.component.html',
+	template: `
+		@if (!!label) {
+			<span class="flex items-center text-primary-500 max-sm:inter-body-xs-bold sm:inter-body-sm-bold">
+				<div class="bg-primary-500 mr-2 h-[12px] w-[2px]"></div>
+				{{ label }}
+			</span>
+		}
+		<!--ToDo: Extraer badge a nuevo componente-->
+		@if (markAsNew) {
+			<div class="flex items-center bg-primary-200 rounded">
+				<label class="inter-body-xs-bold my-1 mx-4">NUEVO</label>
+			</div>
+		}
+	`,
 	styles: `
         :host {
             display: flex;
