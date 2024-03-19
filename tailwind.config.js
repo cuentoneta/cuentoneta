3e7a5b1ceefe6993506d537be92b043e19e540d4
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-03-18 23:54:26 -0300

    [#604] - Elimina opción purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	purge: {
-		enabled: true,
-	},
 	content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'), ...createGlobPatternsForDependencies(__dirname)],
 	theme: {
 		colors: {
