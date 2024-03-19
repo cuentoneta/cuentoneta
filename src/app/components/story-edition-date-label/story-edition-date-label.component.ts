9718e4920b41329e08bc4c38177e17476c534b96
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-03-19 16:43:16 -0300

    [#599] - Adapta componente a estilos inline

--- src/app/components/story-edition-date-label/story-edition-date-label.component.ts
+++ src/app/components/story-edition-date-label/story-edition-date-label.component.ts
@@ -2,16 +2,18 @@ import { Component, Input } from '@angular/core';
 import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
-    selector: 'cuentoneta-story-edition-date-label[label]',
-    templateUrl: './story-edition-date-label.component.html',
-    styleUrls: ['./story-edition-date-label.component.scss'],
-    standalone: true,
-    imports: [
-        CommonModule, 
-        NgIf
-    ],
+	selector: 'cuentoneta-story-edition-date-label[label]',
+	templateUrl: './story-edition-date-label.component.html',
+	styles: `
+        :host {
+            display: flex;
+            justify-content: space-between;
+        }
+    `,
+	standalone: true,
+	imports: [CommonModule, NgIf],
 })
 export class StoryEditionDateLabelComponent {
-    @Input() label: string | undefined;
-    @Input() markAsNew: boolean = false;
+	@Input() label: string | undefined;
+	@Input() markAsNew: boolean = false;
 }
