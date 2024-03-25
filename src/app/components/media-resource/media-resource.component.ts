b25ba5ec88c781d526c13aedc9bf48e13e763557
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-03-25 11:16:15 -0300

    [#493] - Declara componentes para tratamiento de spaces de X y grabaciones de audio

--- src/app/components/media-resource/media-resource.component.ts
+++ src/app/components/media-resource/media-resource.component.ts
@@ -2,17 +2,28 @@ import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpaceRecordingWidgetComponent } from '../space-recording-widget/space-recording-widget.component';
 import { MediaTypes } from '@models/media.model';
+import { AudioRecordingWidgetComponent } from '../audio-recording-widget/audio-recording-widget.component';
 
 @Component({
 	selector: 'cuentoneta-media-resource',
 	standalone: true,
-	imports: [CommonModule, SpaceRecordingWidgetComponent],
+	imports: [CommonModule, SpaceRecordingWidgetComponent, AudioRecordingWidgetComponent],
 	template: ` @for (media of mediaResources(); track $index) {
 		@if (media.type === 'spaceRecording') {
-			<cuentoneta-space-recording-widget [media]="media" class="block mb-10"></cuentoneta-space-recording-widget>
+			@defer {
+				<cuentoneta-space-recording-widget [media]="media" class="block"></cuentoneta-space-recording-widget>
+			}
+		}
+		@if (media.type === 'audioRecording') {
+			@defer {
+				<cuentoneta-audio-recording-widget [media]="media"></cuentoneta-audio-recording-widget>
+			}
 		}
 	}`,
-	styles: ``,
+	styles: `
+	:host{
+		@apply block w-full mb-10;
+	}`,
 })
 export class MediaResourceComponent {
 	mediaResources = input.required<MediaTypes[]>();
