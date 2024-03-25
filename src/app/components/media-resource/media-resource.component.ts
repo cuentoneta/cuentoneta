57c050cf6dcf2c3ea8123675bccfff066951e70f
Ramiro Olivencia <ramiro@olivencia.com.ar>
2024-03-24 22:37:01 -0300

    [#493] - Agrega método para type assertion en recursos de tipo SpaceRecording

--- src/app/components/media-resource/media-resource.component.ts
+++ src/app/components/media-resource/media-resource.component.ts
@@ -1,7 +1,12 @@
+// Angular Core
 import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Componentes
 import { SpaceRecordingWidgetComponent } from '../space-recording-widget/space-recording-widget.component';
-import { MediaTypes } from '@models/media.model';
+
+// Modelos
+import { MediaTypes, SpaceRecording } from '@models/media.model';
 
 @Component({
 	selector: 'cuentoneta-media-resource',
@@ -9,11 +14,25 @@ import { MediaTypes } from '@models/media.model';
 	imports: [CommonModule, SpaceRecordingWidgetComponent],
 	template: ` @for (media of mediaResources(); track $index) {
 		@if (media.type === 'spaceRecording') {
-			<cuentoneta-space-recording-widget [media]="media" class="block mb-10"></cuentoneta-space-recording-widget>
+			<cuentoneta-space-recording-widget
+				[media]="asSpaceRecording(media)"
+				class="block"
+			></cuentoneta-space-recording-widget>
+		}
+		@if (media.type === 'audioRecording') {
+			<audio [src]="media.url" controls class="block mb-2 w-full"></audio>
+			<label class="inter-body-caption-bold">{{ media.title }}</label>
 		}
 	}`,
-	styles: ``,
+	styles: `
+	:host{
+		@apply block w-full mb-10;
+	}`,
 })
 export class MediaResourceComponent {
-	mediaResources = input.required<MediaTypes[]>();
+	readonly mediaResources = input.required<MediaTypes[]>();
+
+	asSpaceRecording(media: MediaTypes): SpaceRecording {
+		return media as SpaceRecording;
+	}
 }
