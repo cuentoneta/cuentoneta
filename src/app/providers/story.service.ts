9f20fd36f64c5a50790ed9c6b599cc7a33cfc5cf
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-04-08 15:22:59 -0300

    [#588] - Elimina métodos ya reemplazados por templating y control flow

--- src/app/providers/story.service.ts
+++ src/app/providers/story.service.ts
@@ -7,8 +7,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
 // Models
-import { Paragraph, Story, StoryCard, StoryDTO } from '@models/story.model';
-import { Block, BlockContent, MarkDef } from '@models/block-content.model';
+import { Story, StoryCard, StoryDTO } from '@models/story.model';
 import { AudioRecording, Media, MediaTypes, SpaceRecording } from '@models/media.model';
 
 @Injectable({ providedIn: 'root' })
@@ -31,7 +30,7 @@ export class StoryService {
 			},
 			prologues: story.prologues ?? [],
 			paragraphs: story?.paragraphs ?? [],
-			summary: story?.summary?.map((x: BlockContent) => this.parseParagraph(x)) ?? [],
+			summary: story?.summary ?? [],
 			media: story.media?.map((x) => this.mediaTypesAdapter(x)) ?? [],
 		};
 	}
@@ -41,11 +40,11 @@ export class StoryService {
 			...story,
 			prologues: story.prologues ?? [],
 			paragraphs: story?.paragraphs ?? [],
-			summary: story?.summary?.map((x: BlockContent) => this.parseParagraph(x)) ?? [],
+			summary: story?.summary ?? [],
 			author: {
 				...story.author,
 				imageUrl: this.parseAvatarImageUrl(story.author.imageUrl),
-				biography: story.author.biography?.map((x) => this.parseParagraph(x)) ?? [],
+				biography: story.author.biography ?? [],
 			},
 			media: story.media?.map((x) => this.mediaTypesAdapter(x)) ?? [],
 		};
@@ -53,91 +52,6 @@ export class StoryService {
 
 	private parseAvatarImageUrl(imageUrl: string | undefined): string {
 		return imageUrl ?? 'assets/img/default-avatar.jpg';
-	}
-
-	private parseParagraph(blockContent: BlockContent): Paragraph {
-		let paragraph = '';
-		let classes: string[] = [];
-
-		blockContent.markDefs;
-
-		blockContent.children.forEach((block: Block) => {
-			let part = block.text;
-
-			part = this.parseBlockTextStyleMarks(block, part);
-			part = this.parseBlockTextMarkDefs(part, block.marks ?? [], blockContent.markDefs ?? []);
-
-			// Transformación de salto de línea en texto dentro del mismo párrafo
-			part = part.replaceAll('\n', '<br/>');
-
-			// Asignación de clases para modificar estilos del texto
-			// TODO: Utilizar mark particular en BlockContent para centrar cualquier tipo de texto a futuro
-			if (block.text?.includes('***')) {
-				classes = classes.concat(['text-center']);
-			}
-
-			paragraph = paragraph.concat(part);
-		});
-		return { classes: classes.join(' '), text: paragraph };
-	}
-
-	/**
-	 * Método encargado de procesar los estilos estáticos de texto soportados por Sanity
-	 * Genera tags HTML para los estilos de texto en negrita e itálica
-	 * // TODO: Soportar los restantes estilos de texto del tipo BlockContent en Sanity
-	 * @param block
-	 * @param part
-	 * @private
-	 */
-	private parseBlockTextStyleMarks(block: Block, part: string = ''): string {
-		const marks = block.marks ?? [];
-
-		if (block.marks?.length === 0) return part;
-
-		marks.forEach((mark) => {
-			switch (mark) {
-				case 'em':
-					part = this.addItalics(part);
-					break;
-				case 'strong':
-					part = this.addBold(part);
-					break;
-				default:
-					break;
-			}
-		});
-
-		return part;
-	}
-
-	private parseBlockTextMarkDefs(text: string, marks: string[], markDefs: MarkDef[]): string {
-		if (markDefs.length === 0 || marks.length === 0) return text;
-
-		markDefs.forEach((markDef) => {
-			if (marks.includes(markDef._key)) {
-				switch (markDef._type) {
-					case 'link':
-						text = this.addUrlLink(text, markDef.href);
-						break;
-					default:
-						break;
-				}
-			}
-		});
-
-		return text;
-	}
-
-	private addItalics(text: string): string {
-		return `<i>${text}</i>`;
-	}
-
-	private addBold(text: string): string {
-		return `<b>${text}</b>`;
-	}
-
-	private addUrlLink(text: string, url: string): string {
-		return `<a href="${url}">${text}</a>`;
 	}
 
 	/**
