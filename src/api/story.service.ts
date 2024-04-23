6f69b304632ed51ccf6e7dd4ede640033a6afd0c
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-04-23 01:08:21 -0300

    [#702] - Agrega endpoint para obtener todas las stories de un author en base a su slug

--- src/api/story.service.ts
+++ src/api/story.service.ts
@@ -12,9 +12,44 @@ import { StoryDTO } from '@models/story.model';
 import { authorForStory } from './_queries/author.query';
 import { resourcesSubQuery } from './_queries/resources.query';
 
-async function fetchForRead(slug: string): Promise<StoryDTO> {
-	{
-		const query = groq`*[_type == 'story' && slug.current == '${slug}']
+export async function fetchByAuthorSlug(slug: string): Promise<StoryDTO[]> {
+	const query = groq`*[_type == 'story' && author->slug.current == '${slug}']
+						  {
+							  'slug': slug.current,
+							  title, 
+							  language,
+							  videoUrl,
+							  badLanguage,
+							  forewords, 
+							  categories, 
+							  body[0...2], 
+							  approximateReadingTime,
+							  mediaSources,
+							  ${resourcesSubQuery},
+						  }`;
+
+	const result = await client.fetch(query, {});
+	const stories = [];
+
+	// Toma las publicaciones que fueron traídas en la consulta a Sanity y las mapea a una colección de publicaciones
+	for (const story of result) {
+		const { body, review, author, forewords, mediaSources, ...properties } = story;
+
+		stories.push({
+			...properties,
+			media: await mapMediaSources(mediaSources),
+			prologues: mapPrologues(forewords),
+			resources: mapResources(properties.resources),
+			paragraphs: body,
+			summary: review,
+		});
+	}
+
+	return stories;
+}
+
+export async function fetchForRead(slug: string): Promise<StoryDTO> {
+	const query = groq`*[_type == 'story' && slug.current == '${slug}']
                           {
                               'slug': slug.current,
                               title, 
@@ -31,20 +66,17 @@ async function fetchForRead(slug: string): Promise<StoryDTO> {
                               ${resourcesSubQuery},
 							  ${authorForStory}
                           }[0]`;
-		const story = await client.fetch(query, {});
+	const story = await client.fetch(query, {});
 
-		const { body, review, author, forewords, mediaSources, ...properties } = story;
+	const { body, review, author, forewords, mediaSources, ...properties } = story;
 
-		return {
-			...properties,
-			media: await mapMediaSources(mediaSources),
-			author: mapAuthorForStory(author, properties.language),
-			prologues: mapPrologues(forewords),
-			resources: mapResources(properties.resources),
-			paragraphs: body,
-			summary: review,
-		};
-	}
+	return {
+		...properties,
+		media: await mapMediaSources(mediaSources),
+		author: mapAuthorForStory(author, properties.language),
+		prologues: mapPrologues(forewords),
+		resources: mapResources(properties.resources),
+		paragraphs: body,
+		summary: review,
+	};
 }
-
-export { fetchForRead };
