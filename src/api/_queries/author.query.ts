c951338dc2c2051ecc83790674d64dabc45203fc
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-04-23 00:30:43 -0300

    [#702] - Agrega constantes para reutilizar subqueries relativas a autores en consultas

--- src/api/_queries/author.query.ts
+++ src/api/_queries/author.query.ts
@@ -1,5 +1,7 @@
 import { resourcesSubQuery } from './resources.query';
 
-export const authorForStoryCard = "'author': author-> { name, slug, image, nationality-> }";
+export const authorThumbnailFields = ['slug', 'name', 'image', 'nationality->'];
+export const authorDetailedFields = [...authorThumbnailFields, 'biography', resourcesSubQuery]
 
-export const authorForStory = `'author': author-> { name, slug, image, biography, nationality->, ${resourcesSubQuery}}`;
+export const authorForStoryCard = `'author': author-> { ${authorThumbnailFields.join(',')} }`;
+export const authorForStory = `'author': author-> { ${authorDetailedFields.join(',')} }`;
