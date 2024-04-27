3e7cd1d433e99f5c7cbcf0436cd1f127f5e65cca
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-04-27 15:41:55 -0300

    [#376] - Acomoda estilos y remueve border radius

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -2,11 +2,7 @@
 import { Component, inject, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { combineLatest, switchMap } from 'rxjs';
-import {
-  CommonModule,
-  isPlatformBrowser,
-  NgOptimizedImage,
-} from '@angular/common';
+import { CommonModule, isPlatformBrowser, NgOptimizedImage } from '@angular/common';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { YouTubePlayer } from '@angular/youtube-player';
 
@@ -34,8 +30,8 @@ import { StoryNavigationBarComponent } from '../../components/story-navigation-b
 import { BioSummaryCardComponent } from '../../components/bio-summary-card/bio-summary-card.component';
 import { ShareContentComponent } from '../../components/share-content/share-content.component';
 import { EpigraphComponent } from '../../components/epigraph/epigraph.component';
-import { MediaResourceComponent } from '../../components/media-resource/media-resource.component'
-import { PortableTextParserComponent } from '../../components/portable-text-parser/portable-text-parser.component'
+import { MediaResourceComponent } from '../../components/media-resource/media-resource.component';
+import { PortableTextParserComponent } from '../../components/portable-text-parser/portable-text-parser.component';
 
 @Component({
 	selector: 'cuentoneta-story',
@@ -43,7 +39,27 @@ import { PortableTextParserComponent } from '../../components/portable-text-pars
 	styles: `
 		:host {
 			@apply md:grid md:mt-28 gap-x-8 md:grid-cols-[286px_1fr];
-		}`,
+		}
+
+		@keyframes scrollbar {
+			to {width: 100%;}
+		}
+
+		.progress-container {
+			@apply bg-primary-100 w-full h-2;
+			overflow: hidden;
+			position: sticky;
+			top: 0;
+		}
+
+		.progress-bar {
+			@apply bg-primary-400 h-full w-0;
+			transition-timing-function: ease-out;
+			transition: width .5s;
+			animation: scrollbar linear;
+			animation-timeline: scroll(root);
+		}
+	`,
 	standalone: true,
 	imports: [
 		CommonModule,
