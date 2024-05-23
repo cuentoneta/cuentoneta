71863efac4e297eb94522d3267f0388ac98eb316
Ramiro Olivencia <ramiro@olivencia.com.ar>
2024-05-23 10:15:35 -0300

    [#708] - Agrega nuevas funciones para mappear storylists y previews de storylists

--- src/api/_utils/functions.ts
+++ src/api/_utils/functions.ts
@@ -11,6 +11,7 @@ import { baseLanguage } from '../../../cms/utils/localization';
 
 // Modelos
 import { AuthorDTO } from '@models/author.model';
+import { mapMediaSources } from './media-sources.functions';
 
 export function mapAuthor(rawAuthorData: any, language?: string): AuthorDTO {
 	return {
@@ -57,4 +58,81 @@ export function mapResources(resources: any[]) {
 			},
 		})) ?? []
 	);
+}
+
+export async function mapStorylist(result: any) {
+	const storylistImages = result.gridConfig.cardsPlacement?.filter((config: any) => !!config.imageSlug) ?? [];
+
+	const rawPublications = result.gridConfig.cardsPlacement
+		.filter((cardPlacement: any) => !!cardPlacement.publication && !!cardPlacement.publication.story)
+		.map((cardPlacement: any) => cardPlacement.publication);
+	const publications = [];
+
+	// Toma las publicaciones que fueron traídas en la consulta a Sanity y las mapea a una colección de publicaciones
+	for (const publication of rawPublications) {
+		const { review, body, author, mediaSources, ...story } = publication.story;
+		publications.push({
+			...publication,
+			story: {
+				...story,
+				media: mediaSources ? await mapMediaSources(mediaSources) : undefined,
+				summary: review,
+				paragraphs: body,
+				author: mapAuthorForStory(author),
+			},
+		});
+	}
+
+	const storylist = {
+		...result,
+		featuredImage: !result.featuredImage ? undefined : urlFor(result.featuredImage).url(),
+		images:
+			storylistImages.length === 0
+				? []
+				: storylistImages.map((card: any) => ({
+						slug: card.imageSlug,
+						url: urlFor(card.image).url(),
+					})),
+		publications: publications,
+	};
+}
+
+export function mapStorylistPreview(result: any) {
+	const previewImages = result.gridConfig?.cardsPlacement?.filter((config: any) => !!config.imageSlug) ?? [];
+	return {
+		...result,
+		// Elimina elementos publication traídos en la consulta a Sanity del objeto grid config
+		gridConfig: {
+			...result.gridConfig,
+			cardsPlacement: result.gridConfig.cardsPlacement?.map((placement: any) => {
+				const { publication, image, ...other } = placement;
+				return other;
+			}),
+		},
+		featuredImage: !result.featuredImage ? undefined : urlFor(result.featuredImage).url(),
+		images:
+			previewImages.length === 0
+				? []
+				: previewImages.map((card: any) => ({
+						slug: card.imageSlug,
+						url: urlFor(card.image).url(),
+					})),
+
+		// Toma las publicaciones que fueron traídas en la consulta a Sanity y las mapea a una colección de publicaciones
+		publications: result.gridConfig.cardsPlacement
+			.filter((cardPlacement: any) => !!cardPlacement.publication && !!cardPlacement.publication.story)
+			.map((cardPlacement: any) => cardPlacement.publication)
+			.map((publication: any) => {
+				const { review, body, author, ...story } = publication.story;
+				return {
+					...publication,
+					story: {
+						...story,
+						summary: review,
+						paragraphs: body,
+						author: mapAuthorForStory(author),
+					},
+				};
+			}),
+	};
 }
