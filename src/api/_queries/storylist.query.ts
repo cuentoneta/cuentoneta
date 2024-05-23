5f0916f2c18ff7242abf7b9f03663cdb7c66f204
Ramiro Olivencia <ramiro@olivencia.com.ar>
2024-05-23 10:14:18 -0300

    [#708] - Abstrae queries de storylist en nuevos const

---
+++ src/api/_queries/storylist.query.ts
@@ -0,0 +1,106 @@
+import { authorForStoryCard } from './author.query';
+
+export const storylistPreviewQuery = `
+{ 
+                        _id,
+                        'slug': slug.current,
+                        title,
+                        description,
+                        language,
+                        displayDates,
+                        editionPrefix,
+                        comingNextLabel,
+                        'tags': tags[] -> {
+                            title, 
+                            'slug': slug.current, 
+                            description, 
+                            'icon': {'name': icon.name, 'provider': icon.provider, 'svg': icon.svg}
+                        },
+                        featuredImage,
+                        'gridConfig': { 
+                            'gridTemplateColumns': previewGridConfig.gridTemplateColumns,
+                            'titlePlacement': previewGridConfig.titlePlacement,
+                            'cardsPlacement': previewGridConfig.cardsPlacement[]
+                                {
+                                    'order': order,
+                                    'slug': publication.story->slug.current,
+                                    'startCol': startCol,
+                                    'image': image,
+                                    'imageSlug': imageSlug.current,
+                                    'endCol': endCol,
+                                    'startRow': startRow,
+                                    'endRow': endRow,
+                                    'publication': {
+                                        'publishingOrder': publication.publishingOrder,
+                                        'publishingDate': publication.publishingDate,
+                                        'published': publication.published,
+                                        'story': publication.story->{
+                                            _id,
+                                            'slug': slug.current,
+                                            title,
+                                            badLanguage,
+                                            categories,
+                                            body[0...3],
+                                            review,
+                                            approximateReadingTime,
+                                            language,
+                                            mediaSources,
+                                        	${authorForStoryCard}
+                                        }
+                                    }
+                                }
+                        },
+                        'count': count(*[ _type == 'publication' && storylist._ref == ^._id ])
+                    }
+`;
+
+export const storylistQuery = `{ 
+                        _id,
+                        'slug': slug.current,
+                        title,
+                        description,
+                        language,
+                        displayDates,
+                        editionPrefix,
+                        comingNextLabel,
+                        featuredImage,
+                        'tags': tags[] -> {
+                            title, 
+                            'slug': slug.current, 
+                            description, 
+                            'icon': {'name': icon.name, 'provider': icon.provider, 'svg': icon.svg}
+                        },
+                        'gridConfig': {
+                            'gridTemplateColumns': gridConfig.gridTemplateColumns,
+                            'titlePlacement': gridConfig.titlePlacement,
+                            'cardsPlacement': gridConfig.cardsPlacement[]
+                            {
+                                'order': order,
+                                'slug': publication.story->slug.current,
+                                'startCol': startCol,
+                                'image': image,
+                                'imageSlug': imageSlug.current,
+                                'endCol': endCol,
+                                'startRow': startRow,
+                                'endRow': endRow,
+                                'publication': {
+                                     'publishingOrder': publication.publishingOrder,
+                                     'publishingDate': publication.publishingDate,
+                                     'published': publication.published,
+                                     'story': publication.story->{
+                                        _id,
+                                        'slug': slug.current,
+                                        title,
+                                        categories,
+                                        body[0...3],
+                                        review,
+                                        approximateReadingTime,
+                                        language,
+                                        mediaSources,
+                                        ${authorForStoryCard}
+                                    }
+                                }
+                          	}
+                        },
+                        'count': count(*[ _type == 'publication' && storylist._ref == ^._id ])
+`;
