f0f935c20f91b4dd2a4e31a9d9e7349e65bf3a15
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-05-28 01:40:25 -0300

    [#232] - Agrega validaciones para los parámetros de pickColor

--- src/app/providers/theme.service.ts
+++ src/app/providers/theme.service.ts
@@ -9,6 +9,16 @@ import { DefaultColors } from 'tailwindcss/types/generated/colors';
 })
 export class ThemeService {
 	pickColor(color: keyof DefaultColors, scale: number = 50) {
-		return colors[color][200];
+		if (!colors[color]) {
+			throw new Error(`Color ${color} not found in Tailwind CSS config!`);
+		}
+
+		// @ts-expect-error - Este guard chequea la existencia de la escala en el color
+		if (!colors[color][scale.toString()]) {
+			throw new Error(`Scale ${scale} not found in color ${color}!`);
+		}
+
+		// @ts-expect-error - En este punto tanto el color como la escala han sido validados
+		return colors[color][scale.toString()].toUpperCase();
 	}
 }
