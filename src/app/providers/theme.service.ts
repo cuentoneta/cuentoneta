15307fd10e2596e0374e15d2109d823e0626a584
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-05-28 02:08:10 -0300

    [#232] - Extrae definiciones de colores extendidos a un nuevo archivo

--- src/app/providers/theme.service.ts
+++ src/app/providers/theme.service.ts
@@ -1,32 +1,36 @@
 import { inject, Injectable, PLATFORM_ID } from '@angular/core';
 
 // Tailwind
-import colors from 'tailwindcss/colors';
+import * as defaultColors from 'tailwindcss/colors';
 import { DefaultColors } from 'tailwindcss/types/generated/colors';
 import { Meta } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
+import { extendedColors } from '../../../theme.config';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class ThemeService {
 	private meta = inject(Meta);
-	private platform = inject(PLATFORM_ID);
+	private platformId = inject(PLATFORM_ID);
 
 	pickColor(color: keyof DefaultColors, scale: number = 50) {
-		if (!colors[color]) {
+		if (!defaultColors[color]) {
 			throw new Error(`Color ${color} not found in Tailwind CSS config!`);
 		}
 
 		// @ts-expect-error - Este guard chequea la existencia de la escala en el color
-		if (!colors[color][scale.toString()]) {
+		if (!defaultColors[color][scale.toString()]) {
 			throw new Error(`Scale ${scale} not found in color ${color}!`);
 		}
 
 		// @ts-expect-error - En este punto tanto el color como la escala han sido validados
-		return colors[color][scale.toString()].toUpperCase();
+		return defaultColors[color][scale.toString()].toUpperCase();
 	}
 
 	addThemeColorTag() {
-		this.meta.addTag({ name: 'theme-color', content: 'hsl(21, 57%, 44%)' });
+		if (isPlatformBrowser(this.platformId)) {
+			this.meta.addTag({ name: 'theme-color', content: extendedColors['primary-500'] });
+		}
 	}
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,12 @@
 import { createGlobPatternsForDependencies } from '@nx/angular/tailwind';
 import { join } from 'path';
 import { Config } from 'tailwindcss/types/config';
+import { extendedColors } from './theme.config';
 
 export default {
 	content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'), ...createGlobPatternsForDependencies(__dirname)],
 	theme: {
-		colors: {
-			white: '#fff',
-			black: '#000',
-			'primary-100': 'hsl(9, 70%, 96%)',
-			'primary-200': 'hsl(11, 78%, 93%)',
-			'primary-300': 'hsl(24, 45%, 76%)',
-			'primary-400': 'hsl(25, 45%, 62%)',
-			'primary-500': 'hsl(21, 57%, 44%)',
-			'gray-50': 'hsl(0, 0%, 98%)',
-			'gray-100': 'hsl(240, 5%, 96%)',
-			'gray-200': 'hsl(240, 6%, 90%)',
-			'gray-300': 'hsl(240, 5%, 84%)',
-			'gray-400': 'hsl(240, 5%, 65%)',
-			'gray-500': 'hsl(240, 4%, 46%)',
-			'gray-600': 'hsl(240, 5%, 34%)',
-			'gray-700': 'hsl(240, 5%, 26%)',
-			'gray-800': 'hsl(240, 5%, 20%)',
-			'gray-900': 'hsl(240, 5%, 14%)',
-			'interactive-500': 'hsl(212, 70%, 45%)',
-			'interactive-600': 'hsl(212, 70%, 35%)',
-			'zinc-300': '#d4d4d8', // Utilizado formato hexadecimal por limitaciones de ngx-skeleton-loader
-		},
+		colors: extendedColors,
 		content: {
 			blank: '""',
 		},
