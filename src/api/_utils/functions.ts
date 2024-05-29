6ddca7a3b0338be824f00e842e1d35272a1aedb0
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-05-29 00:00:47 -0300

    [#755] - Cambia valores undefined en objetos mappeados

--- src/api/_utils/functions.ts
+++ src/api/_utils/functions.ts
@@ -22,9 +22,9 @@ export function mapAuthor(rawAuthorData: any, language?: string): AuthorDTO {
 			flag: urlFor(rawAuthorData.nationality?.flag)?.url(),
 		},
 		resources: mapResources(rawAuthorData.resources),
-		imageUrl: rawAuthorData.image ? urlFor(rawAuthorData.image).url() : undefined,
+		imageUrl: rawAuthorData.image ? urlFor(rawAuthorData.image).url() : '',
 		name: rawAuthorData.name,
-		biography: rawAuthorData.biography,
+		biography: rawAuthorData.biography ? rawAuthorData.biography[language || baseLanguage!.id] : undefined,
 	};
 }
 
@@ -36,7 +36,7 @@ export function mapAuthorForStory(rawAuthorData: any, language?: string): Author
 			flag: urlFor(rawAuthorData.nationality?.flag)?.url(),
 		},
 		resources: mapResources(rawAuthorData.resources),
-		imageUrl: rawAuthorData.image ? urlFor(rawAuthorData.image).url() : undefined,
+		imageUrl: rawAuthorData.image ? urlFor(rawAuthorData.image).url() : '',
 		name: rawAuthorData.name,
 		biography: rawAuthorData.biography ? rawAuthorData.biography[language || baseLanguage!.id] : undefined,
 	};
