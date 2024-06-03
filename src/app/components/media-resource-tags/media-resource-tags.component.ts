dbbad5bfe15f97500dc3d5d1c8fe5a4bccf7b5d7
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-06-03 14:55:40 -0300

    [#742] - Cambio en leyendas de tags

--- src/app/components/media-resource-tags/media-resource-tags.component.ts
+++ src/app/components/media-resource-tags/media-resource-tags.component.ts
@@ -9,18 +9,18 @@ import { Media } from '@models/media.model';
 	template: ` @for (mediaResource of resources(); track $index) {
 		@switch (mediaResource.type) {
 			@case ('audioRecording') {
-				<div [title]="'Contiene material en formato audio'" class="h-6 w-6">
-					<img [alt]="'Contiene audio'" src="assets/svg/waveform.svg" />
+				<div [title]="'Contiene narraciones en audio'" class="h-6 w-6">
+					<img [alt]="'Contiene narraciones en audio'" src="assets/svg/waveform.svg" />
 				</div>
 			}
 			@case ('spaceRecording') {
-				<div [title]="'Contiene material en formato Spaces de X'" class="h-6 w-6">
-					<img [alt]="'Contiene link a Spaces'" src="assets/svg/twitter.svg" />
+				<div [title]="'Contiene grabaciones de Spaces de X'" class="h-6 w-6">
+					<img [alt]="'Contiene grabaciones de Spaces de X'" src="assets/svg/twitter.svg" />
 				</div>
 			}
 			@case ('youTubeVideo') {
-				<div [title]="'Contiene material en formato video de YouTube'" class="h-6 w-6">
-					<img [alt]="'Contiene video de YouTube'" class="video" src="assets/svg/video.svg" />
+				<div [title]="'Contiene videos de YouTube'" class="h-6 w-6">
+					<img [alt]="'Contiene videos de YouTube'" class="video" src="assets/svg/video.svg" />
 				</div>
 			}
 		}
