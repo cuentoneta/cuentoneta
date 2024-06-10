3e7432ad2cc0db93e00e5cc06e22ae05a3af29e6
Ramiro Olivencia <ramiro@olivencia.com.ar>
2024-06-10 00:00:03 -0300

    [#773] - Agrega método getByAuthorSlug. Asigna campo originalPublication en parseStoryCardContent

--- src/app/providers/story.service.ts
+++ src/app/providers/story.service.ts
@@ -1,5 +1,5 @@
 // Core
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
@@ -13,23 +13,36 @@ import { ApiUrl, Endpoints } from './endpoints';
 @Injectable({ providedIn: 'root' })
 export class StoryService {
 	private readonly url: ApiUrl = `${environment.apiUrl}${Endpoints.Story}`;
-	constructor(private http: HttpClient) {}
+	private http = inject(HttpClient);
+
 	public getBySlug(slug: string): Observable<Story> {
 		const params = new HttpParams().set('slug', slug);
 
 		return this.http.get<StoryDTO>(this.url, { params }).pipe(map((story) => this.parseStoryContent(story)));
 	}
 
+	public getByAuthorSlug(slug: string, offset: number = 0, limit: number = 20): Observable<StoryCard[]> {
+		const params = new HttpParams().set('offset', offset).append('limit', limit);
+		return this.http
+			.get<StoryDTO[]>(`${this.url}/author/${slug}`, { params })
+			.pipe(map((stories) => stories.map((story) => this.parseStoryCardContent(story))));
+	}
+
 	public parseStoryCardContent(story: StoryDTO): StoryCard {
-		return {
+		const card = {
 			...story,
-			author: {
-				...story.author,
-				imageUrl: this.parseAvatarImageUrl(story.author.imageUrl),
-			},
 			paragraphs: story?.paragraphs ?? [],
 			media: story.media ?? [],
+			originalPublication: story.originalPublication ?? '',
 		};
+
+		if (story.author) {
+			card.author = {
+				...story.author,
+				imageUrl: this.parseAvatarImageUrl(story.author.imageUrl),
+			};
+		}
+		return card;
 	}
 
 	private parseStoryContent(story: StoryDTO): Story {
