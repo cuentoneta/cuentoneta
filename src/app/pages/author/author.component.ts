2c459d903144b658c0f2cc760d8621b205c4cdb1
Ramiro Olivencia <ramiro@olivencia.com.ar>
2024-06-10 00:12:39 -0300

    [#772] - Agrega stub de componente AuthorComponent

---
+++ src/app/pages/author/author.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+	selector: 'cuentoneta-author',
+	standalone: true,
+	imports: [CommonModule],
+	template: `<p>author works!</p>`,
+	styles: ``,
+})
+export class AuthorComponent {}
