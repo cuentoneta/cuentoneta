cbe2e08c0745f1979681281ae5965c07dce99145
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-06-12 02:44:31 -0300

    [#780] - Visualiza tarjetas en frame

--- src/app/components/author-navigation-frame/author-navigation-frame.component.ts
+++ src/app/components/author-navigation-frame/author-navigation-frame.component.ts
@@ -1,19 +1,33 @@
+// Core
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavigationFrameComponent } from '@models/navigation-frame.component';
-import { AppRoutes } from '../../app.routes';
-import { StoryCard } from '@models/story.model';
-import { StoryService } from '../../providers/story.service';
 import { switchMap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
+
+// Routing
+import { AppRoutes } from '../../app.routes';
+
+// Models
+import { StoryCard } from '@models/story.model';
+
+// Providers
+import { StoryService } from '../../providers/story.service';
+
+// Componentes
+import { NavigationFrameComponent } from '@models/navigation-frame.component';
+import { NavigableStoryTeaserComponent } from '../navigable-story-teaser/navigable-story-teaser.component';
 
 @Component({
 	selector: 'cuentoneta-author-navigation-frame',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, NavigableStoryTeaserComponent],
 	template: `@if (stories) {
 		@for (story of stories; track $index) {
-			{{ story | json }}
+			<cuentoneta-navigable-story-teaser
+				[story]="story"
+				[selected]="selectedStorySlug() === story.slug"
+				[authorSlug]="authorSlug"
+			></cuentoneta-navigable-story-teaser>
 		}
 	}`,
 	styles: `
@@ -24,6 +38,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 })
 export class AuthorNavigationFrameComponent extends NavigationFrameComponent {
 	stories: StoryCard[] = [];
+	authorSlug: string = '';
 
 	readonly appRoutes = AppRoutes;
 
@@ -31,17 +46,20 @@ export class AuthorNavigationFrameComponent extends NavigationFrameComponent {
 		super();
 
 		const storyService = inject(StoryService);
-		this.fetchContentDirective
-			.fetchContent$<StoryCard[]>(
-				this.activatedRoute.queryParams.pipe(switchMap(({ slug }) => storyService.getByAuthorSlug(slug))),
+
+		this.activatedRoute.queryParams
+			.pipe(
+				takeUntilDestroyed(),
+				switchMap(({ slug }) =>
+					this.fetchContentDirective.fetchContent$<StoryCard[]>(storyService.getByAuthorSlug(slug)),
+				),
 			)
-			.pipe(takeUntilDestroyed())
 			.subscribe((stories) => {
 				this.stories = stories;
 				this.config.set({
 					headerTitle: 'Más del autor',
 					footerTitle: 'Ver más...',
-					navigationRoute: this.router.createUrlTree([this.appRoutes.Author, 'abacab']),
+					navigationRoute: this.router.createUrlTree([this.appRoutes.Author, this.authorSlug]),
 					showFooter: true,
 				});
 			});
