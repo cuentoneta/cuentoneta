b9375151b8a069cc1080916a71d916234681d01a
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-06-12 01:55:56 -0300

    [#780] - Agrega servicio NavigationFrameService

--- src/app/components/author-navigation-frame/author-navigation-frame.component.ts
+++ src/app/components/author-navigation-frame/author-navigation-frame.component.ts
@@ -1,13 +1,50 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationFrameComponent } from '@models/navigation-frame.component';
+import { AppRoutes } from '../../app.routes';
+import { StoryCard } from '@models/story.model';
+import { StoryService } from '../../providers/story.service';
+import { switchMap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NavigationBarConfig } from '../storylist-navigation-frame/storylist-navigation-frame.component';
 
 @Component({
 	selector: 'cuentoneta-author-navigation-frame',
 	standalone: true,
 	imports: [CommonModule],
-	template: `<p>author-navigation-frame works!</p>`,
-	styles: ``,
+	template: `@if (stories) {
+		@for (story of stories; track $index) {
+			{{ story | json }}
+		}
+	}`,
+	styles: `
+		:host {
+			@apply grid grid-cols-1 gap-y-0.5 rounded-xl bg-gray-200 shadow-lg;
+		}
+	`,
 })
-export class AuthorNavigationFrameComponent {
-	selectedStorySlug = input<string>();
+export class AuthorNavigationFrameComponent extends NavigationFrameComponent {
+	stories: StoryCard[] = [];
+
+	readonly appRoutes = AppRoutes;
+
+	constructor() {
+		super();
+
+		const storyService = inject(StoryService);
+		this.fetchContentDirective
+			.fetchContent$<StoryCard[]>(
+				this.activatedRoute.queryParams.pipe(switchMap(({ slug }) => storyService.getByAuthorSlug(slug))),
+			)
+			.pipe(takeUntilDestroyed())
+			.subscribe((stories) => {
+				this.stories = stories;
+				this.config.set({
+					headerTitle: 'Más del autor',
+					footerTitle: 'Ver más...',
+					navigationRoute: this.router.createUrlTree([this.appRoutes.Author, 'abacab']),
+					showFooter: true,
+				});
+			});
+	}
 }
---
+++ src/app/providers/navigation-frame.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, signal } from '@angular/core';
+import { NavigationBarConfig } from '../components/storylist-navigation-frame/storylist-navigation-frame.component';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class NavigationFrameService {
+	navigationBarConfig = signal<NavigationBarConfig>({
+		headerTitle: '',
+		footerTitle: '',
+		navigationRoute: '',
+		showFooter: false,
+	});
+
+	setNavigationBarConfig(config: NavigationBarConfig) {
+		this.navigationBarConfig.set(config);
+	}
+}
