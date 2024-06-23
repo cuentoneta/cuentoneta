fe8edbb01375de3e06d42821014ffc36c7ec064d
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-06-23 16:58:41 -0300

    [#788] - Agrega skeletons para template de autor

--- src/app/pages/author/author.component.ts
+++ src/app/pages/author/author.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ActivatedRoute, Router, RouterLink, UrlTree } from '@angular/router';
 import { StoryService } from '../../providers/story.service';
-import { combineLatest, map, of, switchMap, tap } from 'rxjs';
+import { combineLatest, delay, map, of, switchMap, tap } from 'rxjs';
 import { StoryCardComponent } from '../../components/story-card/story-card.component';
 import { AuthorService } from '../../providers/author.service';
 import { PortableTextParserComponent } from '../../components/portable-text-parser/portable-text-parser.component';
@@ -10,7 +10,14 @@ import { ResourceComponent } from '../../components/resource/resource.component'
 import { StoryCard } from '@models/story.model';
 import { AppRoutes } from '../../app.routes';
 import { Author } from '@models/author.model';
+
+// Directives
 import { MetaTagsDirective } from '../../directives/meta-tags.directive';
+import { FetchContentDirective } from '../../directives/fetch-content.directive';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { StoryCardSkeletonComponent } from '../../components/story-card-skeleton/story-card-skeleton.component';
+import { ThemeService } from '../../providers/theme.service';
+import { RepeatPipe } from '../../pipes/repeat.pipe';
 
 @Component({
 	selector: 'cuentoneta-author',
@@ -22,41 +29,111 @@ import { MetaTagsDirective } from '../../directives/meta-tags.directive';
 		PortableTextParserComponent,
 		ResourceComponent,
 		RouterLink,
+		NgxSkeletonLoaderModule,
+		StoryCardSkeletonComponent,
+		RepeatPipe,
 	],
-	hostDirectives: [MetaTagsDirective],
-	template: ` <main>
-		<article class="grid grid-cols-1 gap-8">
-			<section class="flex flex-col items-center gap-4">
-				@if (author$ | async; as author) {
-					@if (author) {
-						<img [src]="author.imageUrl" class="h-[192px] rounded-xl" />
+	hostDirectives: [FetchContentDirective, MetaTagsDirective],
+	template: `
+		<main>
+			<article class="grid grid-cols-1 gap-8">
+				@if (content$ | async; as content) {
+					<section class="flex flex-col items-center gap-4">
+						<img [ngSrc]="content.author.imageUrl" class="h-[192px] rounded-xl" width="192" height="192" />
 						<div class="flex items-center gap-4">
-							<h1 class="h1">{{ author.name }}</h1>
-							<img [ngSrc]="author.nationality.flag" width="30" height="20" class="h-6 w-8" />
+							<h1 class="h1">{{ content.author.name }}</h1>
+							<img [ngSrc]="content.author.nationality.flag" width="30" height="20" class="h-6 w-8" />
 						</div>
-						@if (author.resources && author.resources.length > 0) {
+						@if (content.author.resources && content.author.resources.length > 0) {
 							<div class="flex justify-start gap-4 sm:justify-end">
-								@for (resource of author.resources; track $index) {
+								@for (resource of content.author.resources; track $index) {
 									<cuentoneta-resource [resource]="resource"></cuentoneta-resource>
 								}
 							</div>
 						}
 						<cuentoneta-portable-text-parser
-							[paragraphs]="author.biography!"
-							[classes]="'source-serif-pro-body-xl mb-8 leading-8'"
+							[paragraphs]="content.author.biography!"
+							[classes]="'source-serif-pro-body-xl mb-8 leading-8 max-w-[960px]'"
 						></cuentoneta-portable-text-parser>
-					}
+					</section>
+					<section class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8">
+						@for (story of content.stories; track $index) {
+							<cuentoneta-story-card [story]="story" [navigationRoute]="story.navigationRoute"></cuentoneta-story-card>
+						}
+					</section>
+				} @else {
+					<ng-container *ngTemplateOutlet="authorSkeleton"></ng-container>
+					<ng-container *ngTemplateOutlet="cardsSkeleton"></ng-container>
 				}
+			</article>
+		</main>
+
+		<ng-template #authorSkeleton>
+			<section class="flex flex-col items-center justify-center gap-4">
+				<ngx-skeleton-loader
+					[theme]="{
+						'height.px': 192,
+						'width.px': 192,
+						'background-color': skeletonColor
+					}"
+					count="1"
+					appearance="line"
+				></ngx-skeleton-loader>
+				<div class="flex items-center gap-4">
+					<ngx-skeleton-loader
+						[theme]="{
+							'height.px': 40,
+							'width.px': 320,
+							'margin.px': 0,
+							'background-color': skeletonColor
+						}"
+						count="1"
+						appearance="line"
+					></ngx-skeleton-loader>
+					<ngx-skeleton-loader
+						[theme]="{
+							'height.px': 40,
+							'width.px': 40,
+							'margin.px': 0,
+							'background-color': skeletonColor
+						}"
+						count="1"
+						appearance="line"
+					></ngx-skeleton-loader>
+				</div>
+				<div class="flex justify-start gap-4 sm:justify-end">
+					@for (item of 3 | repeat; track $index) {
+						<ngx-skeleton-loader
+							[theme]="{
+								'height.px': 48,
+								'width.px': 48,
+								'margin.px': 0,
+								'background-color': skeletonColor
+							}"
+							count="1"
+							appearance="circle"
+						></ngx-skeleton-loader>
+					}
+				</div>
+				<ngx-skeleton-loader
+					[theme]="{
+						'height.px': 20,
+						'width.px': 768,
+						'background-color': skeletonColor
+					}"
+					class="max-width-[960px] flex flex-col"
+					count="6"
+				></ngx-skeleton-loader>
 			</section>
+		</ng-template>
+		<ng-template #cardsSkeleton>
 			<section class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8">
-				@if (stories$ | async; as stories) {
-					@for (story of stories; track $index) {
-						<cuentoneta-story-card [story]="story" [navigationRoute]="story.navigationRoute"></cuentoneta-story-card>
-					}
+				@for (item of 6 | repeat; track $index) {
+					<cuentoneta-story-card-skeleton [animation]="'progress'" [displayFooter]="false" />
 				}
 			</section>
-		</article>
-	</main>`,
+		</ng-template>
+	`,
 })
 export class AuthorComponent {
 	private readonly appRoutes = AppRoutes;
@@ -64,9 +141,15 @@ export class AuthorComponent {
 	// Providers
 	private activatedRoute = inject(ActivatedRoute);
 	private authorService = inject(AuthorService);
-	private metaTagsDirective = inject(MetaTagsDirective);
 	private storyService = inject(StoryService);
 	private router = inject(Router);
+	private themeService = inject(ThemeService);
+
+	// Directives
+	private metaTagsDirective = inject(MetaTagsDirective);
+	public fetchContentDirective = inject(FetchContentDirective);
+
+	skeletonColor = this.themeService.pickColor('zinc', 300);
 
 	author$ = this.activatedRoute.params.pipe(
 		switchMap(({ slug }) => this.authorService.getBySlug(slug)),
@@ -86,6 +169,8 @@ export class AuthorComponent {
 			})) as (StoryCard & { navigationRoute: UrlTree })[];
 		}),
 	);
+
+	content$ = this.fetchContentDirective.fetchContent$(combineLatest({ author: this.author$, stories: this.stories$ }));
 
 	private updateMetaTags(author: Author) {
 		this.metaTagsDirective.setTitle(`${author.name}`);
