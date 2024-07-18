b1713b7dfccc7085870bc573d075a76c45b67038
Luz <luzojeda@proton.me>
2024-07-18 15:12:41 -0300

    [#833] Definir consulta literal usando groq para author by slug

--- src/api/author/author.service.ts
+++ src/api/author/author.service.ts
@@ -1,8 +1,6 @@
 // Sanity
 import { client } from '../_helpers/sanity-connector';
-
-// Subqueries
-import { resourcesSubQuery } from '../_queries/resources.query';
+import { authorBySlugQuery } from '../_queries/author.query';
 
 // Funciones
 import { mapAuthor } from '../_utils/functions';
@@ -11,9 +9,6 @@ import { mapAuthor } from '../_utils/functions';
 import { AuthorDTO } from '@models/author.model';
 
 export async function getBySlug(slug: string): Promise<AuthorDTO> {
-	const filter = `*[_type == 'author' && slug.current == '${slug}'][0]`;
-	const fields = ['slug', 'name', 'image', 'nationality->', 'biography', resourcesSubQuery];
-	const query = filter + `{ ${fields.join(',')} }`;
-	const result = await client.fetch(query, {});
+	const result = await client.fetch(authorBySlugQuery, { slug });
 	return mapAuthor(result);
 }
