b809860af6b58b04437014f9b369f318d6c84b07
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2024-07-27 23:40:50 +0000

    [#822] - Input cambiado a Signal Input en storylist-card.component.ts

--- src/app/components/storylist-card-component/storylist-card.component.ts
+++ src/app/components/storylist-card-component/storylist-card.component.ts
@@ -26,10 +26,10 @@ import { ThemeService } from '../../providers/theme.service';
 	template: `
 		<article class="shadow-lg hover:shadow-lg-hover">
 			@if (!!storylist()) {
-				<div [routerLink]="['/' + appRoutes.StoryList, storylist().slug]" class="navigation-link">
+				<div [routerLink]="['/' + appRoutes.StoryList, storylist()!.slug]" class="navigation-link">
 					<header class="h-[240px] max-w-[602px] cursor-pointer">
 						<img
-							[ngSrc]="storylist().featuredImage ?? ''"
+							[ngSrc]="storylist()!.featuredImage ?? ''"
 							width="602"
 							height="240"
 							class="h-[240px] rounded-t-lg object-cover"
@@ -38,17 +38,17 @@ import { ThemeService } from '../../providers/theme.service';
 					</header>
 					<section class="flex flex-col gap-4 border-1 border-y-0 border-solid border-primary-300 px-4 pt-5">
 						<h1 class="h1 cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap hover:text-interactive-500">
-							{{ storylist().title }}
+							{{ storylist()!.title }}
 						</h1>
-						<p class="min-h-9">{{ storylist().description }}</p>
+						<p class="min-h-9">{{ storylist()!.description }}</p>
 						<hr class="text-gray-300" />
 					</section>
 				</div>
 				<footer
 					class="flex justify-end rounded-b-lg border-1 border-t-0 border-solid border-primary-300 px-5 pb-5 pt-4"
 				>
-					@if (!!storylist().tags && storylist().tags.length > 0) {
-						@for (tag of storylist().tags; track tag.slug) {
+					@if (!!storylist()!.tags && storylist()!.tags.length > 0) {
+						@for (tag of storylist()!.tags; track tag.slug) {
 							<cuentoneta-badge [tag]="tag" [showIcon]="true" class="ml-3" />
 						}
 					}
