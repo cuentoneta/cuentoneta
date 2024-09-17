10080186f01c1d7ef297b68ca89bafa9006ce433
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-17 12:31:36 -0300

    [#1006] - Simplifica definición de ContentService

--- src/app/providers/content.service.ts
+++ src/app/providers/content.service.ts
@@ -21,36 +21,9 @@ export class ContentService {
 
 	public getLandingPageContent(): Observable<LandingPageContent> {
 		return this.http.get<LandingPageContent>(`${this.prefix}/landing-page`).pipe(
-			map((content) => ({
-				cards: content.cards,
-				previews: content.cards,
+			map(({ cards }) => ({
+				cards: cards,
 			})),
-		);
-	}
-
-	// ToDo: Obtener listas de navs desde API
-	public getNavLists(): Pick<Storylist, 'slug' | 'title'>[] {
-		return [
-			{ slug: 'otono-2023', title: 'Cuentos de Otoño' },
-			{ slug: 'fec-english-sessions', title: 'FEC English Sessions' },
-			{ slug: 'verano-2022', title: 'La Cuentoneta 1.0' },
-		];
-	}
-
-	/**
-	 * Hace fetch de la configuración de landing page desde el origen de datos y genera
-	 * una tupla de arrays de objetos compuestos de tipo StorylistCardDeck, los cuales
-	 * contienen la configuración y la correspondiente información para renderizar
-	 * los decks de previews y cards de cada storylist.
-	 */
-	public fetchStorylistDecks(): Observable<LandingPageContent> {
-		return this.getLandingPageContent().pipe(
-			map(({ previews, cards }) => {
-				return {
-					previews: previews,
-					cards: cards,
-				};
-			}),
 		);
 	}
 }
