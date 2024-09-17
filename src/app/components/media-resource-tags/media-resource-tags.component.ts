127e63bbc26ad536c99f4468ca8a41ef78c65151
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-17 07:31:42 -0300

    [#995] - Alinea horizontal y verticalmente los íconos de MediaResourceTagsComponent

--- src/app/components/media-resource-tags/media-resource-tags.component.ts
+++ src/app/components/media-resource-tags/media-resource-tags.component.ts
@@ -9,17 +9,17 @@ import { Media } from '@models/media.model';
 	template: ` @for (mediaResource of resources(); track $index) {
 		@switch (mediaResource.type) {
 			@case ('audioRecording') {
-				<div [title]="'Contiene narraciones en audio'" [class]="size()">
+				<div [title]="'Contiene narraciones en audio'" [class]="size()" class="flex items-center justify-center">
 					<img [alt]="'Contiene narraciones en audio'" src="assets/svg/waveform.svg" />
 				</div>
 			}
 			@case ('spaceRecording') {
-				<div [title]="'Contiene grabaciones de Spaces de X'" [class]="size()">
+				<div [title]="'Contiene grabaciones de Spaces de X'" [class]="size()" class="flex items-center justify-center">
 					<img [alt]="'Contiene grabaciones de Spaces de X'" src="assets/svg/twitter.svg" />
 				</div>
 			}
 			@case ('youTubeVideo') {
-				<div [title]="'Contiene videos de YouTube'" [class]="size()">
+				<div [title]="'Contiene videos de YouTube'" [class]="size()" class="flex items-center justify-center">
 					<img [alt]="'Contiene videos de YouTube'" src="assets/svg/video.svg" />
 				</div>
 			}
