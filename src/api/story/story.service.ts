7fa84693f4c47dc5a48f0639c83bdab232509253
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-18 09:45:20 -0300

    [#993] - Abstrae procesamiento de resultados de query en fetchByAuthorSlug y fetchStoryBySlug en funciones. Agrega tipos a queries

--- src/api/story/story.service.ts
+++ src/api/story/story.service.ts
@@ -2,54 +2,34 @@
 import { client } from '../_helpers/sanity-connector';
 
 // Utilidades
-import { mapAuthor, mapResources, mapStoryContent } from '../_utils/functions';
+import { mapStoryContent, mapStoryTeaser } from '../_utils/functions';
 
 // Modelos
-import { Epigraph, Story, StoryTeaser } from '@models/story.model';
-import { mapMediaSources, mapMediaSourcesForStorylist } from '../_utils/media-sources.functions';
+import { Story, StoryTeaser } from '@models/story.model';
 
 // Subqueries
 import { storiesByAuthorSlugQuery, storyBySlugQuery } from '../_queries/story.query';
 
 // Interfaces
 import { StoriesByAuthorSlugArgs } from '../interfaces/queryArgs';
-import { BlockContent } from '../sanity/generated-schema-types';
+import { StoriesByAuthorSlugQueryResult, StoryBySlugQueryResult } from '../sanity/generated-query-types';
 
 export async function fetchByAuthorSlug(args: StoriesByAuthorSlugArgs): Promise<StoryTeaser[]> {
-	const result = await client.fetch(storiesByAuthorSlugQuery, {
+	const result: StoriesByAuthorSlugQueryResult = await client.fetch(storiesByAuthorSlugQuery, {
 		slug: args.slug,
 		start: args.offset * args.limit,
 		end: (args.offset + 1) * args.limit,
 	});
-	const stories = [];
 
-	// Toma las publicaciones que fueron traídas en la consulta a Sanity y las mapea a una colección de publicaciones
-	for (const story of result) {
-		const { body, mediaSources, resources, ...properties } = story;
-
-		stories.push({
-			...properties,
-			media: mapMediaSourcesForStorylist(mediaSources),
-			resources: mapResources(resources),
-			paragraphs: body,
-		});
-	}
-
-	return stories;
+	return mapStoryTeaser(result);
 }
 
 export async function fetchStoryBySlug(slug: string): Promise<Story> {
-	const result = await client.fetch(storyBySlugQuery, { slug });
+	const result: StoryBySlugQueryResult = await client.fetch(storyBySlugQuery, { slug });
 
-	const { body, review, author, mediaSources, epigraphs, ...properties } = result;
+	if (!result) {
+		throw new Error(`Story with slug ${slug} not found`);
+	}
 
-	return mapStoryContent({
-		...properties,
-		media: await mapMediaSources(mediaSources),
-		author: mapAuthor(author, properties.language),
-		resources: mapResources(properties.resources),
-		paragraphs: body as BlockContent,
-		summary: review as BlockContent,
-		epigraphs: epigraphs as Epigraph[],
-	});
+	return await mapStoryContent(result);
 }
