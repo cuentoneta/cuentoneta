3ea70da9d4c3fcdf66549ccc3265469ff3272d8f
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-18 13:32:40 -0300

    [#993] - Remueve import redundante

--- src/app/components/badge/badge.component.ts
+++ src/app/components/badge/badge.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Tag } from '@models/tag.model';
 import { BypassHtmlSanitizerPipe } from '../../pipes/bypass-html-sanitizer.pipe';
