65ce06a3a38ba880b21f318399c5e4ba4b4a4273
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-19 00:13:48 -0300

    [#1014] - Simplifica consts y tipos utilizados para localización

--- cms/utils/localization.ts
+++ cms/utils/localization.ts
@@ -5,9 +5,4 @@ export interface Language {
 }
 
 export const defaultLanguage: Language = { id: 'es', title: 'Español', isDefault: true };
-
 export const supportedLanguages: Language[] = [defaultLanguage, { id: 'en', title: 'English' }];
-
-export const baseLanguage: Language = supportedLanguages.find((l) => l.isDefault) ?? defaultLanguage;
-
-export const languageCodes = supportedLanguages.map((lang) => lang.id);
