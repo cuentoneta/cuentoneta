e017352bd3e58033f1b5b6f6d5893921de7f903d
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-23 18:03:59 -0300

    [#1024] - Agrega propiedad opcional campaigns en modelo LandingPageContent

--- src/app/models/landing-page-content.model.ts
+++ src/app/models/landing-page-content.model.ts
@@ -1,5 +1,8 @@
 import { StorylistTeaser } from '@models/storylist.model';
+import { ContentCampaign } from '@models/content-campaign.model';
 
 export interface LandingPageContent {
 	cards: StorylistTeaser[];
+	// TODO: Transformar propiedad en obligatoria al actualizar método de request de frontend
+	campaigns?: ContentCampaign[];
 }
