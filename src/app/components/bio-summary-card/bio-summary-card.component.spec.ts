a61c78ebe5ebc84e80292c45eae9a5daa6973136
Dylanson25 <dylanson25@users.noreply.github.com>
2024-09-25 12:05:44 -0700

    [#946] - test country image in bioSummaryCard

--- src/app/components/bio-summary-card/bio-summary-card.component.spec.ts
+++ src/app/components/bio-summary-card/bio-summary-card.component.spec.ts
@@ -2,6 +2,9 @@ import { render, screen } from '@testing-library/angular';
 import { BioSummaryCardComponent } from './bio-summary-card.component';
 import { storyMock } from 'src/app/mocks/story.mock';
 
+const country = storyMock.author.nationality.country;
+const countryRegex = new RegExp(`\\b${country}\\b`, 'i');
+
 describe('BioSummaryCardComponent', () => {
 	it('should render the component', async () => {
 		const { container } = await render(BioSummaryCardComponent, {
@@ -19,5 +22,19 @@ describe('BioSummaryCardComponent', () => {
 		});
 
 		expect(screen.getByText('(Chateauroux, 1948 - París, 1994) fue un escritor', { exact: false })).toBeInTheDocument();
+	});
+
+	it('should display the country image text', async () => {
+		await render(BioSummaryCardComponent, {
+			inputs: {
+				story: storyMock,
+			},
+		});
+
+		const countryImage = screen.getByRole('img', {
+			name: countryRegex,
+		});
+
+		expect(countryImage).toBeInTheDocument();
 	});
 });
