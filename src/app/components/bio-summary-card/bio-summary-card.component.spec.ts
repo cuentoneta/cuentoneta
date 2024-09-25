ab7620900951940c1c293264ba6877a308ee41ad
dylanson25 <dylanson25@users.noreply.github.com>
2024-09-24 19:06:59 -0700

    [#946] - Migracion de test bio summary card component

--- src/app/components/bio-summary-card/bio-summary-card.component.spec.ts
+++ src/app/components/bio-summary-card/bio-summary-card.component.spec.ts
@@ -1,50 +1,23 @@
-// import { render } from '@testing-library/angular';
-//
-// import { BioSummaryCardComponent } from './bio-summary-card.component';
-// import { Author } from 'src/app/models/author.model';
-// import { Story } from '@models/story.model';
+import { render, screen } from '@testing-library/angular';
+import { BioSummaryCardComponent } from './bio-summary-card.component';
+import { storyMock } from 'src/app/mocks/story.mock';
 
-// const mockAuthor: Author = {
-// 	id: '0',
-// 	name: 'Estudios Gativideo',
-// 	imageUrl: 'https://gativideo.com/image.png',
-// 	nationality: {
-// 		country: 'Argentina',
-// 		flag: 'AR',
-// 	},
-// };
-//
-// const mockStory: Story = {
-// 	id: 0,
-// 	title: 'El Marajá de San Telmo',
-// 	slug: 'el-maraja-de-san-telmo',
-// 	approximateReadingTime: 5,
-// 	author: mockAuthor,
-// 	language: 'es',
-// 	badLanguage: false,
-// 	epigraphs: [],
-// 	media: [],
-// 	summary: [
-// 		{ classes: '', text: 'Arranca con "Fanfare for the Common Man" y la próxima canción es "Silhouette" de Kenny G.' },
-// 	],
-// 	paragraphs: [
-// 		{
-// 			classes: '',
-// 			text: 'El presente Video-Cassette se vende para uso personal o doméstico exclusivamente, todos los demás derechos quedan reservados',
-// 		},
-// 		{
-// 			classes: '',
-// 			text: 'Cualquier divulgación total o parcial de la obra, sea a través de copias, edición, adición, exhibición, difusión y/o emisión de difusión queda expresamente prohibida.',
-// 		},
-// 	],
-// };
+describe('BioSummaryCardComponent', () => {
+	it('should render the component', async () => {
+		const { container } = await render(BioSummaryCardComponent, {
+			inputs: { story: storyMock },
+		});
 
-xdescribe('BioSummaryCardComponent', () => {
-	it('should create', async () => {
-		// const component = await render(BioSummaryCardComponent, {
-		// 	componentProperties: { story: mockStory },
-		// });
-		//
-		// expect(component).toBeTruthy();
+		expect(container).toBeInTheDocument();
+	});
+
+	it('should display the Biography', async () => {
+		await render(BioSummaryCardComponent, {
+			inputs: {
+				story: storyMock,
+			},
+		});
+
+		expect(screen.getByText('(Chateauroux, 1948 - París, 1994) fue un escritor', { exact: false })).toBeInTheDocument();
 	});
 });
