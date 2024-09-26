3b635936616a1fa9860de7626d0b93219788742b
rolivencia <ramiro.olivencia@latch.com>
2024-09-25 23:39:15 -0300

    [#1030] - Solucionar problema de compilación de Storybook

--- src/app/components/space-recording-widget/space-recording-widget.stories.ts
+++ src/app/components/space-recording-widget/space-recording-widget.stories.ts
@@ -14,12 +14,45 @@ export default {
 } as Meta<SpaceRecordingWidgetComponent>;
 
 const media: Media = {
-	title: 'En Algún Lugar de Tlön: Encuentro #1',
+	title: 'Desde el sótano de la casa de la calle Garay: Encuentro #2',
 	type: 'spaceRecording',
+	description: [
+		{
+			_key: '41d2361c078b',
+			markDefs: [
+				{
+					_type: 'link',
+					href: 'https://x.com/ladrondesabado',
+					_key: '0f3107ec26f8',
+				},
+			],
+			children: [
+				{
+					_type: 'span',
+					marks: [],
+					text: 'Space de X organizado y dirigido por ',
+					_key: '61bda96789450',
+				},
+				{
+					_type: 'span',
+					marks: ['0f3107ec26f8'],
+					text: '@ladrondesabado',
+					_key: '5250d3f6b741',
+				},
+				{
+					_type: 'span',
+					marks: [],
+					text: ' que incluye la lectura, análisis y discusión del cuento.',
+					_key: '78789bc376f3',
+				},
+			],
+			_type: 'block',
+			style: 'normal',
+		},
+	],
 	data: {
-		id: '1736920785162240151',
-		createdAt: 'Tue Dec 19 01:26:00 +0000 2023',
-		duration: '3:24:03',
+		id: '1738650310468399373',
+		createdAt: 'Sat Dec 23 19:58:31 +0000 2023',
 		tweetBy: {
 			id: '736315211389472769',
 			userName: 'ladrondesabado',
@@ -27,28 +60,29 @@ const media: Media = {
 			createdAt: 'Fri May 27 21:56:41 +0000 2016',
 			description: 'Sombra terrible de Facundo\n\n«οὐ μανθάνεις ὅτι ζητῶ τὸ ἐπὶ πᾶσιν τούτοις ταὐτόν;»',
 			isVerified: false,
-			favouritesCount: 134855,
-			followersCount: 5920,
-			followingsCount: 1343,
-			statusesCount: 43688,
-			location: '',
+			likeCount: 139246,
+			followersCount: 6810,
+			followingsCount: 1381,
+			statusesCount: 46555,
 			pinnedTweet: '1366391960026689543',
 			profileBanner: 'https://pbs.twimg.com/profile_banners/736315211389472769/1674041435',
 			profileImage: 'https://pbs.twimg.com/profile_images/1610082924069588992/xCKlsPnA_normal.jpg',
 		},
 		entities: {
 			hashtags: [],
-			urls: ['https://x.com/i/spaces/1OdKrjnmmQVKX'],
 			mentionedUsers: [],
+			urls: ['https://x.com/i/spaces/1eaKbgZvmOBGX'],
 		},
-		fullText: 'https://t.co/hTvEMOZzRE.',
-		lang: 'zxx',
-		quoteCount: 1,
-		replyCount: 28,
-		retweetCount: 5,
-		likeCount: 24,
-		viewCount: 3760,
-		bookmarkCount: 12,
+		fullText:
+			'En un rato arranca la segunda parte. Esta vez leeremos "El Fin" para luego analizarlo entre todos, así que están invitadísimos\n https://t.co/e8dI374Sco',
+		lang: 'es',
+		quoteCount: 0,
+		replyCount: 1,
+		retweetCount: 2,
+		likeCount: 11,
+		viewCount: 1839,
+		bookmarkCount: 5,
+		duration: '3:04:21',
 	},
 };
 
