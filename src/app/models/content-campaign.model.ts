95ad95e760774e6dea01acda233db990cabe6e17
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-30 12:44:18 -0300

    [#1040] - Agrega tipos y constantes para definir parámetros de los viewports para campañas de contenido

--- src/app/models/content-campaign.model.ts
+++ src/app/models/content-campaign.model.ts
@@ -1,11 +1,39 @@
 import { TextBlockContent } from '@models/block-content.model';
 
-type Viewport = 'xs' | 'md';
+/**
+ * Tipo que representa los diferentes viewports soportados por la aplicación para las campañas de contenido, a fin de
+ * proveer una experiencia responsive y adaptable para su visualización.
+ */
+export type ContentCampaignViewport = 'xs' | 'md';
 
+/**
+ * Constante que indica, para los viewports soportados, las longitudes máximas asignables a los títulos y subtítulos
+ * de las campañas de contenido y las dimensiones de las imágenes asociadas a cada una de ellas.
+ */
+export const imageViewportSizes = Object.freeze({
+	xs: {
+		title: 32,
+		subtitle: 36,
+		imageWidth: 540,
+		imageHeight: 220,
+	},
+	md: {
+		title: 40,
+		subtitle: 60,
+		imageWidth: 960,
+		imageHeight: 280,
+	},
+});
+
+/**
+ * Interface que define al objeto de dominio que representa una campaña de contenido en la plataforma.
+ */
 export interface ContentCampaign {
 	title: string;
 	slug: string;
 	description: TextBlockContent[];
 	url: string;
-	contents: { [key in Viewport]: { title: TextBlockContent[]; subtitle: TextBlockContent[]; imageUrl: string } };
+	contents: {
+		[key in ContentCampaignViewport]: { title: TextBlockContent[]; subtitle: TextBlockContent[]; imageUrl: string };
+	};
 }
