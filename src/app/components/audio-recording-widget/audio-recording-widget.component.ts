37d1e216414c7ca2fce0a57c6721c78b803998aa
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-30 07:30:13 -0300

    [#919] - Agrega atributo para dirigir testing

--- src/app/components/audio-recording-widget/audio-recording-widget.component.ts
+++ src/app/components/audio-recording-widget/audio-recording-widget.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Models
 import { AudioRecording } from '@models/media.model';
+
+// Components
 import { PortableTextParserComponent } from '../portable-text-parser/portable-text-parser.component';
 
 @Component({
@@ -8,10 +12,9 @@ import { PortableTextParserComponent } from '../portable-text-parser/portable-te
 	standalone: true,
 	imports: [CommonModule, PortableTextParserComponent],
 	template: `
-		<audio [src]="media().data.url" controls class="mb-2 block w-full"></audio>
+		<audio [src]="media().data.url" data-testid="audio-recording" controls class="mb-2 block w-full"></audio>
 		<cuentoneta-portable-text-parser [paragraphs]="media().description" class="inter-body-xs-medium text-primary-500" />
 	`,
-	styles: ``,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AudioRecordingWidgetComponent {
