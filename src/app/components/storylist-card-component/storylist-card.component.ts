65fa2e59c5452840f719a9105cbaf9abbb517459
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-10-21 14:28:30 -0300

    [#1051] - Elimina imagen destacada de tarjeta y ajusta estilos

--- src/app/components/storylist-card-component/storylist-card.component.ts
+++ src/app/components/storylist-card-component/storylist-card.component.ts
@@ -32,32 +32,35 @@ import { PortableTextParserComponent } from '../portable-text-parser/portable-te
 		<article class="shadow-lg hover:shadow-lg-hover">
 			@if (storylist(); as storylist) {
 				<div [routerLink]="['/' + appRoutes.StoryList, storylist.slug]" class="navigation-link">
-					<header class="h-[240px] max-w-[602px] cursor-pointer">
-						<img
-							[ngSrc]="storylist.featuredImage"
-							width="602"
-							height="240"
-							class="h-[240px] rounded-t-lg object-cover"
-							alt=""
-						/>
-					</header>
-					<section class="flex flex-col gap-4 border-1 border-y-0 border-solid border-primary-300 px-4 pt-5">
-						<h1 class="h1 cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap hover:text-interactive-500">
+					<section
+						class="flex max-w-[480px] flex-col gap-4 rounded-t-2xl border-1 border-b-0 border-solid border-primary-300 px-4 pt-5"
+					>
+						<h1
+							class="h3 cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap font-source-serif italic hover:text-interactive-500"
+						>
 							{{ storylist.title }}
 						</h1>
-						<p class="min-h-9">
-							<cuentoneta-portable-text-parser [paragraphs]="storylist.description"></cuentoneta-portable-text-parser>
+						<p class="inter-body-base-regular h-24 text-gray-600">
+							<cuentoneta-portable-text-parser
+								[paragraphs]="storylist.description"
+								class="line-clamp-4 min-h-24 text-ellipsis"
+							></cuentoneta-portable-text-parser>
 						</p>
 						<hr class="text-gray-300" />
 					</section>
 				</div>
 				<footer
-					class="flex justify-end rounded-b-lg border-1 border-t-0 border-solid border-primary-300 px-5 pb-5 pt-4"
+					class="flex justify-between rounded-b-2xl border-1 border-t-0 border-solid border-primary-300 px-5 pb-5 pt-4"
 				>
+					<div class="flex rounded bg-gray-200 px-4.5 py-0.5 uppercase hover:cursor-default">
+						<span class="inter-body-xs-bold flex items-center gap-1">{{ storylist.count }} historias</span>
+					</div>
 					@if (!!storylist.tags && storylist.tags.length > 0) {
-						@for (tag of storylist.tags; track tag.slug) {
-							<cuentoneta-badge [tag]="tag" [showIcon]="true" class="ml-3" />
-						}
+						<div class="flex">
+							@for (tag of storylist.tags; track tag.slug) {
+								<cuentoneta-badge [tag]="tag" [showIcon]="true" class="ml-3" />
+							}
+						</div>
 					}
 				</footer>
 			}
@@ -65,7 +68,7 @@ import { PortableTextParserComponent } from '../portable-text-parser/portable-te
 	`,
 	styles: `
 		:host {
-			@apply block max-w-[602px];
+			@apply block rounded-2xl;
 		}
 	`,
 })
