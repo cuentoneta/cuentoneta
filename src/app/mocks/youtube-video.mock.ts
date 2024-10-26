4dce42494008df163064229db26f4b2207e08d2e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-10-26 18:27:38 -0300

    [#920] - Agrega mock para video de YouTube

---
+++ src/app/mocks/youtube-video.mock.ts
@@ -0,0 +1,40 @@
+import { YouTubeVideo } from '@models/media.model';
+
+const youtubeVideoMock: YouTubeVideo = {
+	title: 'Video alusivo a la narración de "El espejo del tiempo"',
+	description: [
+		{
+			markDefs: [
+				{
+					_type: 'link',
+					href: 'https://www.youtube.com/@gativideo',
+					_key: 'de7b21f8e43f',
+				},
+			],
+			children: [
+				{
+					marks: [],
+					text: 'Video alusivo a la narración de "El espejo del tiempo" en el canal de',
+					_key: '7d7e212de407',
+					_type: 'span',
+				},
+				{
+					_type: 'span',
+					marks: ['de7b21f8e43f'],
+					text: 'Gativideo',
+					_key: '374fb3581549',
+				},
+				{
+					text: '.',
+					_key: '83baeb531387',
+					_type: 'span',
+					marks: [],
+				},
+			],
+			_type: 'block',
+			style: 'normal',
+			_key: '321178cc04c5',
+		},
+	],
+	data: { videoId: 'cMFHs_vWPrw' },
+};
