4168f2fc6309490805a1aab1011b244553c0da60
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-10-28 01:16:09 -0300

    [#913] - Agrega método getter para obtener viewport actual

--- src/app/providers/theme.service.ts
+++ src/app/providers/theme.service.ts
@@ -1,11 +1,16 @@
+// Core
 import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 // Tailwind
 import * as defaultColors from 'tailwindcss/colors';
 import { DefaultColors } from 'tailwindcss/types/generated/colors';
-import { Meta } from '@angular/platform-browser';
-import { isPlatformBrowser } from '@angular/common';
 import { extendedColors } from '../../../theme.config';
+import { screens } from '../../../tailwind.screens';
+
+// Models
+import { ContentCampaignViewport } from '@models/content-campaign.model';
 
 @Injectable({
 	providedIn: 'root',
@@ -13,6 +18,16 @@ import { extendedColors } from '../../../theme.config';
 export class ThemeService {
 	private meta = inject(Meta);
 	private platformId = inject(PLATFORM_ID);
+
+	get viewport(): ContentCampaignViewport {
+		// Para SSR, siempre devolver md dado que no se puede acceder a window
+		if (isPlatformServer(this.platformId)) {
+			return 'md';
+		}
+
+		const matcher = window.matchMedia(`(max-width: ${screens.md})`).matches;
+		return matcher ? 'xs' : 'md';
+	}
 
 	pickColor(color: keyof DefaultColors, scale: number = 50) {
 		if (!defaultColors[color]) {
