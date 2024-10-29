aba56f8db19adfd881e5df9c3a27cd0a82b1b04d
dylanson25 <dylanson25@users.noreply.github.com>
2024-10-28 19:01:43 -0700

    [#949] - Migracion de PublicationCardComponent

--- src/app/components/publication-card/publication-card.component.spec.ts
+++ src/app/components/publication-card/publication-card.component.spec.ts
@@ -1,10 +1,28 @@
 import { render } from '@testing-library/angular';
-
+import { publicationMock } from '../../mocks/story.mock';
 import { PublicationCardComponent } from './publication-card.component';
+import { DefaultUrlSerializer, UrlTree } from '@angular/router';
 
-xdescribe('PublicationCardComponent', () => {
-	it('should create', async () => {
-		const view = await render(PublicationCardComponent);
-		expect(view).toBeTruthy();
+describe('PublicationCardComponent', () => {
+	let urlTree: UrlTree;
+
+	const setup = async () => {
+		const urlSerializer = new DefaultUrlSerializer();
+		urlTree = urlSerializer.parse(
+			'/story/la-gallina-degollada?navigation=storylist&navigationSlug=cuentos-de-terror-de-alberto-laiseca',
+		);
+
+		return await render(PublicationCardComponent, {
+			inputs: {
+				editionLabel: ' Episodio 1 ',
+				publication: publicationMock,
+				navigationRoute: urlTree,
+			},
+		});
+	};
+
+	it('should render the component', async () => {
+		const { container } = await setup();
+		expect(container).toBeInTheDocument();
 	});
 });
