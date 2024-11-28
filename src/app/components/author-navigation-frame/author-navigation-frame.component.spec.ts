fe003745c3f23e52c1875035044e1b548dab7de5
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-11-28 02:24:02 -0300

    [#1112] - Agrega tests de renderizado de elementos en navbar para navigation=author

--- src/app/components/author-navigation-frame/author-navigation-frame.component.spec.ts
+++ src/app/components/author-navigation-frame/author-navigation-frame.component.spec.ts
@@ -1,16 +1,33 @@
 import { AuthorNavigationFrameComponent } from './author-navigation-frame.component';
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
 import { FetchContentDirective } from '../../directives/fetch-content.directive';
 import { CommonModule } from '@angular/common';
 import { StoryService } from '../../providers/story.service';
 import { Observable, of } from 'rxjs';
 import { storyTeaserMock } from '../../mocks/story.mock';
 import { StoryTeaser } from '@models/story.model';
+import { RouterTestingModule } from '@angular/router/testing';
+import * as ngExtension from 'ngxtension/inject-query-params';
+import { signal } from '@angular/core';
+import { NavigableStoryTeaserComponent } from '../navigable-story-teaser/navigable-story-teaser.component';
+
+jest.mock('ngxtension/inject-query-params', () => ({
+	__esModule: true,
+	...jest.requireActual('ngxtension/inject-query-params'),
+}));
+
+// Add a jest mock for the injectQueryParams function
+jest.spyOn(ngExtension, 'injectQueryParams').mockReturnValue(
+	signal({
+		navigation: 'author',
+		navigationSlug: 'francois-onoff',
+	}),
+);
 
 describe('AuthorNavigationFrameComponent', () => {
 	const setup = async () => {
 		return await render(AuthorNavigationFrameComponent, {
-			componentImports: [CommonModule],
+			componentImports: [CommonModule, RouterTestingModule, NavigableStoryTeaserComponent],
 			inputs: {},
 			providers: [
 				{
@@ -36,5 +53,13 @@ describe('AuthorNavigationFrameComponent', () => {
 	test('should render AuthorNavigationFrameComponent', async () => {
 		const view = await setup();
 		expect(view).toBeTruthy();
+	});
+
+	test('should render AuthorNavigationFrameComponent with stories', async () => {
+		const view = await setup();
+		view.detectChanges();
+		expect(screen.getByText(storyTeaserMock.title)).toBeInTheDocument();
+		expect(screen.getByText(storyTeaserMock.originalPublication)).toBeInTheDocument();
+		expect(screen.getByText(`${storyTeaserMock.approximateReadingTime} minutos de lectura`)).toBeInTheDocument();
 	});
 });
