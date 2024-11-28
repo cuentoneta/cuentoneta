77ff0ca942e3dc1aaabca76cc7c7eb1a9d765dfc
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-11-28 17:57:40 -0300

    [#976] - Elimina non-null assertions

--- src/api/_utils/clarity.utils.ts
+++ src/api/_utils/clarity.utils.ts
@@ -140,66 +140,66 @@ type MetricData =
 export type ClarityApiResponse = MetricData[];
 
 // Funciones de utilidad para obtener métricas específicas desde la respuesta de la API
-export const getDeadClickMetric = (response: ClarityApiResponse): DeadClickMetric => {
-	return response.find((metric): metric is DeadClickMetric => metric.metricName === 'DeadClickCount')!;
+export const getDeadClickMetric = (response: ClarityApiResponse): DeadClickMetric | undefined => {
+	return response.find((metric): metric is DeadClickMetric => metric.metricName === 'DeadClickCount');
 };
 
-export const getExcessiveScrollMetric = (response: ClarityApiResponse): ExcessiveScrollMetric => {
-	return response.find((metric): metric is ExcessiveScrollMetric => metric.metricName === 'ExcessiveScroll')!;
+export const getExcessiveScrollMetric = (response: ClarityApiResponse): ExcessiveScrollMetric | undefined => {
+	return response.find((metric): metric is ExcessiveScrollMetric => metric.metricName === 'ExcessiveScroll');
 };
 
-export const getRageClickMetric = (response: ClarityApiResponse): RageClickMetric => {
-	return response.find((metric): metric is RageClickMetric => metric.metricName === 'RageClickCount')!;
+export const getRageClickMetric = (response: ClarityApiResponse): RageClickMetric | undefined => {
+	return response.find((metric): metric is RageClickMetric => metric.metricName === 'RageClickCount');
 };
 
-export const getQuickbackClickMetric = (response: ClarityApiResponse): QuickbackClickMetric => {
-	return response.find((metric): metric is QuickbackClickMetric => metric.metricName === 'QuickbackClick')!;
+export const getQuickbackClickMetric = (response: ClarityApiResponse): QuickbackClickMetric | undefined => {
+	return response.find((metric): metric is QuickbackClickMetric => metric.metricName === 'QuickbackClick');
 };
 
-export const getScriptErrorMetric = (response: ClarityApiResponse): ScriptErrorMetric => {
-	return response.find((metric): metric is ScriptErrorMetric => metric.metricName === 'ScriptErrorCount')!;
+export const getScriptErrorMetric = (response: ClarityApiResponse): ScriptErrorMetric | undefined => {
+	return response.find((metric): metric is ScriptErrorMetric => metric.metricName === 'ScriptErrorCount');
 };
 
-export const getErrorClickMetric = (response: ClarityApiResponse): ErrorClickMetric => {
-	return response.find((metric): metric is ErrorClickMetric => metric.metricName === 'ErrorClickCount')!;
+export const getErrorClickMetric = (response: ClarityApiResponse): ErrorClickMetric | undefined => {
+	return response.find((metric): metric is ErrorClickMetric => metric.metricName === 'ErrorClickCount');
 };
 
-export const getScrollDepthMetric = (response: ClarityApiResponse): ScrollDepthMetric => {
-	return response.find((metric): metric is ScrollDepthMetric => metric.metricName === 'ScrollDepth')!;
+export const getScrollDepthMetric = (response: ClarityApiResponse): ScrollDepthMetric | undefined => {
+	return response.find((metric): metric is ScrollDepthMetric => metric.metricName === 'ScrollDepth');
 };
 
-export const getTrafficMetric = (response: ClarityApiResponse): TrafficMetric => {
-	return response.find((metric): metric is TrafficMetric => metric.metricName === 'Traffic')!;
+export const getTrafficMetric = (response: ClarityApiResponse): TrafficMetric | undefined => {
+	return response.find((metric): metric is TrafficMetric => metric.metricName === 'Traffic');
 };
 
-export const getEngagementTimeMetric = (response: ClarityApiResponse): EngagementTimeMetric => {
-	return response.find((metric): metric is EngagementTimeMetric => metric.metricName === 'EngagementTime')!;
+export const getEngagementTimeMetric = (response: ClarityApiResponse): EngagementTimeMetric | undefined => {
+	return response.find((metric): metric is EngagementTimeMetric => metric.metricName === 'EngagementTime');
 };
 
-export const getBrowserMetric = (response: ClarityApiResponse): BrowserMetric => {
-	return response.find((metric): metric is BrowserMetric => metric.metricName === 'Browser')!;
+export const getBrowserMetric = (response: ClarityApiResponse): BrowserMetric | undefined => {
+	return response.find((metric): metric is BrowserMetric => metric.metricName === 'Browser');
 };
 
-export const getDeviceMetric = (response: ClarityApiResponse): DeviceMetric => {
-	return response.find((metric): metric is DeviceMetric => metric.metricName === 'Device')!;
+export const getDeviceMetric = (response: ClarityApiResponse): DeviceMetric | undefined => {
+	return response.find((metric): metric is DeviceMetric => metric.metricName === 'Device');
 };
 
-export const getOSMetric = (response: ClarityApiResponse): OSMetric => {
-	return response.find((metric): metric is OSMetric => metric.metricName === 'OS')!;
+export const getOSMetric = (response: ClarityApiResponse): OSMetric | undefined => {
+	return response.find((metric): metric is OSMetric => metric.metricName === 'OS');
 };
 
-export const getCountryMetric = (response: ClarityApiResponse): CountryMetric => {
-	return response.find((metric): metric is CountryMetric => metric.metricName === 'Country')!;
+export const getCountryMetric = (response: ClarityApiResponse): CountryMetric | undefined => {
+	return response.find((metric): metric is CountryMetric => metric.metricName === 'Country');
 };
 
-export const getPageTitleMetric = (response: ClarityApiResponse): PageTitleMetric => {
-	return response.find((metric): metric is PageTitleMetric => metric.metricName === 'PageTitle')!;
+export const getPageTitleMetric = (response: ClarityApiResponse): PageTitleMetric | undefined => {
+	return response.find((metric): metric is PageTitleMetric => metric.metricName === 'PageTitle');
 };
 
-export const getReferrerUrlMetric = (response: ClarityApiResponse): ReferrerUrlMetric => {
-	return response.find((metric): metric is ReferrerUrlMetric => metric.metricName === 'ReferrerUrl')!;
+export const getReferrerUrlMetric = (response: ClarityApiResponse): ReferrerUrlMetric | undefined => {
+	return response.find((metric): metric is ReferrerUrlMetric => metric.metricName === 'ReferrerUrl');
 };
 
-export const getPopularPagesMetric = (response: ClarityApiResponse): PopularPagesMetric => {
-	return response.find((metric): metric is PopularPagesMetric => metric.metricName === 'PopularPages')!;
+export const getPopularPagesMetric = (response: ClarityApiResponse): PopularPagesMetric | undefined => {
+	return response.find((metric): metric is PopularPagesMetric => metric.metricName === 'PopularPages');
 };
