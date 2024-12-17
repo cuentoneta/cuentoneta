b272833c4d2bd06d7ef7279d102ed77f4d4fbc10
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-12-17 12:20:50 -0300

    [#1108] - Mueve server routes a nuevo archivo

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,50 +1,8 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { provideServerRoutesConfig, RenderMode, ServerRoute } from '@angular/ssr';
-
-export const serverRoutes: Array<ServerRoute> = [
-	{
-		path: '',
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: 'home',
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: 'about',
-		renderMode: RenderMode.Prerender,
-	},
-	{
-		path: 'dmca',
-		renderMode: RenderMode.Prerender,
-	},
-	{
-		path: 'author/:slug',
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: 'story',
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: 'story/:slug',
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: 'story/:slug/:list',
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: 'storylist',
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: 'storylist/:slug',
-		renderMode: RenderMode.Server,
-	},
-];
+import { provideServerRoutesConfig } from '@angular/ssr';
+import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
 	providers: [provideServerRendering(), provideServerRoutesConfig(serverRoutes)],
