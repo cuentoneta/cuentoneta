071f91532458ddbb5bc1a96cd17783e3068a83d2
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2025-02-06 05:11:56 -0300

    [#891] - Agrega test para ShareButtonComponent

--- src/app/components/share-button/share-button.component.spec.ts
+++ src/app/components/share-button/share-button.component.spec.ts
@@ -1,21 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ShareButtonComponent } from './share-button.component';
+import { render, screen } from '@testing-library/angular';
+import { SharingPlatform } from '@models/sharing-platform';
+import { storyMock } from '../../mocks/story.mock';
+
+class MockSharingPlatform implements SharingPlatform {
+	name = 'MySpace';
+	logo = 'myspace';
+	platformApiUrl = `https://www.facebook.com/share.php`;
+
+	generateSharingUrl(appRoute: string, urlParams: string): string {
+		const url = encodeURIComponent(`http://localhost:4200/${appRoute}?${urlParams}`);
+		return `${this.platformApiUrl}?u=${url}`;
+	}
+}
 
 describe('ShareButtonComponent', () => {
-	let component: ShareButtonComponent;
-	let fixture: ComponentFixture<ShareButtonComponent>;
+	const setup = async () => {
+		return await render(ShareButtonComponent, {
+			inputs: {
+				platform: new MockSharingPlatform(),
+				params: { slug: storyMock.slug },
+				message: 'Leí esta historia y me pareció interesante',
+				route: `/stories/${storyMock.slug}`,
+			},
+		});
+	};
 
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			imports: [ShareButtonComponent],
-		}).compileComponents();
-
-		fixture = TestBed.createComponent(ShareButtonComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
+	it('should render the component', async () => {
+		const { container } = await setup();
+		expect(container).toBeInTheDocument();
 	});
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
+	it('should render the platform logo', async () => {
+		await setup();
+		const logo = screen.getByRole('img');
+		expect(logo).toHaveAttribute('src', 'assets/svg/myspace.svg');
 	});
 });
