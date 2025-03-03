286f47c60a982950b39a5b8c31741f9db0551413
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2025-03-03 19:53:06 -0300

    [#391] - Configura modo zoneless para Jest

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,4 +1,4 @@
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+import { setupZonelessTestEnv } from 'jest-preset-angular/setup-env/zoneless';
 import '@testing-library/jest-dom';
 
-setupZoneTestEnv();
+setupZonelessTestEnv();
