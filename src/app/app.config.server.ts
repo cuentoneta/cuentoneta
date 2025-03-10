fb68ce9d8c663b1ee8485cc7c82d5e2b3293465f
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-03-10 00:59:54 -0300

    [#1168] - Corrige error de linting

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { provideServerRoutesConfig, provideServerRouting, withAppShell } from '@angular/ssr';
+import { provideServerRouting, withAppShell } from '@angular/ssr';
 import { serverRoutes } from './app.routes.server';
 import { AppComponent } from './app.component';
 
