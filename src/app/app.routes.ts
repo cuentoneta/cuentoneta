0ad7e909cc3d827a5309953b3b9060327ad879e6
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2025-03-25 07:47:07 -0300

    [HF] - Revierte cambios en guards de redirección

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 
+// Guards
+import { redirectQueryParamsBasedStoryUrlsGuard } from './guards/redirect-query-params-based-story-urls.guard';
+import { redirectQueryParamsBasedStorylistUrlsGuard } from './guards/redirect-query-params-based-storylist-urls.guard';
+import { redirectParamsForStorylistInStoryRouteGuard } from './guards/redirect-params-for-storylist-in-story-route.guard';
+
 export enum AppRoutes {
 	Home = 'home',
 	Story = 'story',
@@ -28,8 +33,23 @@ export const appRoutes: Routes = [
 		loadComponent: () => import('./pages/story/story.component'),
 	},
 	{
+		path: `${AppRoutes.Story}/:slug/:list`,
+		loadComponent: () => import('./pages/story/story.component'),
+		canActivate: [redirectParamsForStorylistInStoryRouteGuard],
+	},
+	{
+		path: `${AppRoutes.Story}`, // Ruta definida por cuestiones de retrocompatibilidad. Redirecciona de '/story' con queryParams a params
+		loadComponent: () => import('./pages/story/story.component'),
+		canActivate: [redirectQueryParamsBasedStoryUrlsGuard],
+	},
+	{
 		path: `${AppRoutes.StoryList}/:slug`,
 		loadComponent: () => import('./pages/storylist/storylist.component'),
+	},
+	{
+		path: `${AppRoutes.StoryList}`,
+		loadComponent: () => import('./pages/storylist/storylist.component'),
+		canActivate: [redirectQueryParamsBasedStorylistUrlsGuard],
 	},
 	{
 		path: AppRoutes.About,
