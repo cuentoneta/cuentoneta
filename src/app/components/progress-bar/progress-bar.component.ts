167ce2bf72a7dcb6c67a7fe953db85546f46c29c
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2025-05-03 02:54:20 -0300

    [#714] - Agrega componente para abstraer barra de progreso

---
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// TODO: Generar barra de progresso utilizando TypeScript y lectura del DOM, para mejorar feature
+@Component({
+	selector: 'cuentoneta-progress-bar',
+	imports: [CommonModule],
+	template: ` <div class="progress-bar h-full w-0 bg-primary-400" data-testid="progress-bar"></div>`,
+	styles: `
+		:host {
+			@apply sticky z-10 col-span-full h-2 w-full overflow-hidden bg-primary-100;
+			transition: top 200ms ease-in-out;
+		}
+
+		.progress-bar {
+			transition-timing-function: ease-out;
+			transition: width 0.5s;
+			animation: scrollbar linear;
+			animation-timeline: scroll(root);
+		}
+
+		@keyframes scrollbar {
+			to {
+				width: 100%;
+			}
+		}
+	`,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProgressBarComponent {}
