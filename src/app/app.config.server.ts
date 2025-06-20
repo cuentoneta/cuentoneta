8f2da4fa7080bbfb215edb1e015e19dc8ec762fb
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-06-19 21:49:55 -0300

    [#1240] - Actualiza import de provideServerRouting

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { appConfig } from './app.config';
-import { provideServerRouting, withAppShell, provideServerRendering } from '@angular/ssr';
+import { withAppShell, provideServerRendering, withRoutes } from '@angular/ssr';
 import { serverRoutes } from './app.routes.server';
 import { AppComponent } from './app.component';
 
 const serverConfig: ApplicationConfig = {
-	providers: [provideServerRendering(), provideServerRouting(serverRoutes, withAppShell(AppComponent))],
+	providers: [provideServerRendering(withRoutes(serverRoutes), withAppShell(AppComponent))],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
