37f35bb98323c9834ac619afc4c21a7e9cf9382a
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-09-15 16:16:47 -0300

    sasas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,7 +10,7 @@ import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angu
 import { appRoutes } from './app.routes';
 
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withIncrementalHydration } from '@angular/platform-browser';
 import localeEs from '@angular/common/locales/es-419';
 import { DatePipe, registerLocaleData } from '@angular/common';
 
@@ -33,7 +33,7 @@ export const appConfig: ApplicationConfig = {
 			return initializerFn();
 		}),
 		{ provide: LOCALE_ID, useValue: 'es-419' },
-		provideClientHydration(),
+		provideClientHydration(withIncrementalHydration()),
 		provideAnimations(),
 		provideAnimationsAsync(),
 		provideRouter(appRoutes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }), withViewTransitions()),
