4a3226982d62bd858716856e4f91afc7b42e18b9
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-09-27 03:33:29 -0300

    [#1260] - Rollback en el uso de path

--- api/index.mjs
+++ api/index.mjs
@@ -1,2 +1,3 @@
-const serverDistPath = '../dist/cuentoneta/server/server.mjs';
+const path = require('path');
+const serverDistPath = path.join(process.cwd(), 'dist/cuentoneta/server/server.mjs');
 export default import(serverDistPath).then((module) => module.app);
