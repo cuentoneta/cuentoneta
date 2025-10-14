3831c7071dddc61281502230578ce7301d2e15cc
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-10-14 11:46:16 -0300

    [#1328] - Configura documentos de schema story para visualización en tabla

--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -5,15 +5,14 @@ import schemas from './schemas/schema';
 import { LaunchIcon, RobotIcon } from '@sanity/icons';
 
 // Plugins
-import { deskTool } from 'sanity/desk';
+import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
 import { sanityComputedField } from 'sanity-plugin-computed-field';
-import { crossDatasetDuplicator } from '@sanity/cross-dataset-duplicator';
 import { iconPicker } from 'sanity-plugin-icon-picker';
 
 export default defineConfig([
 	{
-		name: 'production-workspace',
+		name: 'production',
 		title: 'PROD - La Cuentoneta',
 		basePath: '/production',
 		icon: LaunchIcon,
@@ -21,16 +20,13 @@ export default defineConfig([
 		token: process.env.SANITY_STUDIO_API_TOKEN,
 		dataset: 'production',
 		plugins: [
-			deskTool({
-				structure: deskStructure,
+			structureTool({
+				structure: (S, context) => {
+					return deskStructure(S, context);
+				},
 			}),
 			sanityComputedField(),
 			visionTool(),
-			crossDatasetDuplicator({
-				types: ['story', 'storylist', 'author', 'nationality'],
-				tool: true,
-				follow: [],
-			}),
 			iconPicker(),
 		],
 		schema: {
@@ -38,23 +34,20 @@ export default defineConfig([
 		},
 	},
 	{
-		name: 'dev-workspace',
+		name: 'development',
 		title: 'DEV - La Cuentoneta',
 		basePath: '/development',
 		icon: RobotIcon,
 		projectId: process.env.SANITY_STUDIO_PROJECT_ID,
 		dataset: 'development',
 		plugins: [
-			deskTool({
-				structure: deskStructure,
+			structureTool({
+				structure: (S, context) => {
+					return deskStructure(S, context);
+				},
 			}),
 			sanityComputedField(),
 			visionTool(),
-			crossDatasetDuplicator({
-				types: ['story', 'storylist', 'author', 'nationality'],
-				tool: true,
-				follow: [],
-			}),
 			iconPicker(),
 		],
 		schema: {
