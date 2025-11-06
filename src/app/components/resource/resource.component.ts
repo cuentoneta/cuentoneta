8a491eef1308da371ca30e62226b9aa1bc0b5fbf
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-11-06 13:10:54 -0300

    [#1304] - Actualiza componentes con inyector para provisión de íconos vía lazy loading

--- src/app/components/resource/resource.component.ts
+++ src/app/components/resource/resource.component.ts
@@ -1,5 +1,13 @@
 // Core
-import { Component, computed, inject, input, OnInit } from '@angular/core';
+import {
+	Component,
+	computed,
+	createEnvironmentInjector,
+	EnvironmentInjector,
+	inject,
+	input,
+	OnInit,
+} from '@angular/core';
 
 // Models
 import { Resource } from '@models/resource.model';
@@ -11,48 +19,20 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { TooltipDirective } from '../../directives/tooltip.directive';
 import { iconMappers } from '@models/icon.model';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { faSolidAddressBook, faSolidEnvelope, faSolidGlobe, faSolidMedal } from '@ng-icons/font-awesome/solid';
-import {
-	simpleBlogger,
-	simpleDiscord,
-	simpleSubstack,
-	simpleWattpad,
-	simpleWikisource,
-	simpleX,
-	simpleYoutube,
-} from '@ng-icons/simple-icons';
-import { faBrandAmazon, faBrandInstagram, faBrandWikipediaW } from '@ng-icons/font-awesome/brands';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
 	selector: 'cuentoneta-resource',
 	hostDirectives: [TooltipDirective],
-	imports: [NgIcon, NgxSkeletonLoaderModule, NgIcon],
-	providers: [
-		provideIcons({
-			faBrandAmazon,
-			faBrandInstagram,
-			faBrandWikipediaW,
-			faSolidAddressBook,
-			faSolidEnvelope,
-			faSolidGlobe,
-			faSolidMedal,
-			simpleBlogger,
-			simpleDiscord,
-			simpleSubstack,
-			simpleWattpad,
-			simpleWikisource,
-			simpleYoutube,
-			simpleX,
-		}),
-	],
+	imports: [NgxSkeletonLoaderModule, NgComponentOutlet],
 	template: `
 		<a
 			[href]="resource().url"
 			target="_blank"
 			class="flex h-12 w-12 items-center justify-center rounded-full border-1 border-solid border-gray-200 bg-gray-100 hover:bg-gray-200"
 		>
-			@if (iconName(); as iconName) {
-				<ng-icon [name]="iconName" [attr.alt]="resource().title" size="22px" />
+			@if (icon(); as icon) {
+				<ng-container *ngComponentOutlet="NgIcon; inputs: { name: icon.name, size: '22px' }; injector: icon.injector" />
 			}
 		</a>
 	`,
@@ -64,13 +44,24 @@ import { faBrandAmazon, faBrandInstagram, faBrandWikipediaW } from '@ng-icons/fo
 })
 export class ResourceComponent implements OnInit {
 	readonly resource = input.required<Resource>();
-	readonly iconName = computed(() => {
-		if (!this.resource().resourceType.slug) {
-			return '';
+	readonly icon = computed(() => {
+		if (!this.resource()?.resourceType?.slug) {
+			return null;
 		}
-		return iconMappers.find((resource) => resource.name === this.resource().resourceType.slug)?.ngIconsName ?? '';
+		const foundIcon = iconMappers.find((iconMap) => iconMap.name === this.resource().resourceType.slug)?.ngIconsName;
+		if (!foundIcon) {
+			return null;
+		}
+
+		return {
+			name: Object.keys(foundIcon)[0],
+			injector: createEnvironmentInjector([provideIcons(foundIcon)], this.injector),
+		};
 	});
 
+	readonly NgIcon = NgIcon;
+
+	private injector = inject(EnvironmentInjector);
 	private tooltipDirective = inject(TooltipDirective);
 
 	ngOnInit() {
