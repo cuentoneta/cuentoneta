b146c8e5c2d38349a269c46ae1b3960ebda249da
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-11-14 01:39:27 -0300

    [#914] - Configura plugin de singletons para Sanity. Agrega categoría "Configuración" para almacenar objetos singleton en Sanity.

--- cms/deskStructure.ts
+++ cms/deskStructure.ts
@@ -1,20 +1,38 @@
 // deskStructure.js
 import { createBulkActionsTable } from 'sanity-plugin-bulk-actions-table';
+import { filteredDocumentListItems, singletonDocumentListItems } from 'sanity-plugin-singleton-management';
 
 export default (S, context) =>
 	S.list()
 		.title('Content')
 		.items([
-			...S.documentTypeListItems().map((collection) => {
-				if (['story'].includes(collection.getId())) {
-					return createBulkActionsTable({
-						type: 'story',
-						S,
-						context,
-						title: 'Cuentos',
-					});
-				} else {
-					return collection;
-				}
-			}),
+			S.listItem()
+				.title('Configuración')
+				.id('configuration')
+				.child(
+					S.list()
+						.title('Configuración')
+						.items([...singletonDocumentListItems({ S, context })]),
+				),
+			S.listItem()
+				.title('Contenido')
+				.id('content')
+				.child(
+					S.list()
+						.title('Contenido')
+						.items([
+							...filteredDocumentListItems({ S, context }).map((collection) => {
+								if (['story'].includes(collection.getId())) {
+									return createBulkActionsTable({
+										type: 'story',
+										S,
+										context,
+										title: 'Cuentos',
+									});
+								} else {
+									return collection;
+								}
+							}),
+						]),
+				),
 		]);
