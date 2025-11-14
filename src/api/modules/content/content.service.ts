3781242c43037ec8f16eaa6fa9d957369ce28ca4
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-11-14 01:56:38 -0300

    [#914] - Separa responsabilidad de obtener contenido desde objeto singleton y prepara landing page para actualización semanal

--- src/api/modules/content/content.service.ts
+++ src/api/modules/content/content.service.ts
@@ -2,20 +2,38 @@
 import { client } from '../../_helpers/sanity-connector';
 
 // Modelos
-import { LandingPageContent } from '@models/landing-page-content.model';
+import { LandingPageContent, RotatingContent } from '@models/landing-page-content.model';
 
 // Queries
-import { landingPageContentQuery } from '../../_queries/content.query';
+import { landingPageContentQuery, rotatingContentQuery } from '../../_queries/content.query';
 
 // Utils
-import { mapLandingPageContent } from '../../_utils/functions';
+import { getWeek, getYear } from 'date-fns';
+import { mapLandingPageContent, mapStoryNavigationTeaserWithAuthor } from '../../_utils/functions';
 
 export async function fetchLandingPageContent(): Promise<LandingPageContent> {
-	const result = await client.fetch(landingPageContentQuery);
+	const weekOfYear = getWeek(new Date());
+	const year = getYear(new Date());
 
-	if (!result) {
+	const title = `${weekOfYear.toString().padStart(2, '0')}-${year}`;
+	const landingPageResult = await client.fetch(landingPageContentQuery, { title });
+	const rotatingContentResult = await client.fetch(rotatingContentQuery);
+
+	if (!landingPageResult || !rotatingContentResult) {
 		throw new Error('Landing page content not found');
 	}
 
-	return mapLandingPageContent(result);
+	return {
+		...mapLandingPageContent({ ...landingPageResult, ...rotatingContentResult }),
+	};
+}
+
+export async function fetchRotatingContent(): Promise<RotatingContent> {
+	const result = await client.fetch(rotatingContentQuery);
+
+	if (!result) {
+		throw new Error('Rotating content not found');
+	}
+
+	return { ...result, mostRead: mapStoryNavigationTeaserWithAuthor(result.mostRead) };
 }
