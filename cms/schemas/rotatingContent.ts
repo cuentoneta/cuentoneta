06eb054ebeb767f8635df5e63ae75a617213954c
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-11-14 01:54:09 -0300

    [#914] - Configura schema rotatingContent como singleton

--- cms/schemas/rotatingContent.ts
+++ cms/schemas/rotatingContent.ts
@@ -4,6 +4,9 @@ export default defineType({
 	name: 'rotatingContent',
 	title: 'Contenido rotativo',
 	type: 'document',
+	options: {
+		singleton: true, // Identify this document as a singleton
+	},
 	fields: [
 		defineField({
 			name: 'name',
