e11a2a4bb595cef624df0c60f9a8130d9e86e0ea
fsystemweb <facu.cappella@gmail.com>
2025-11-17 23:05:27 +0100

    progress

--- src/app/components/storylist-card-component/storylist-card.component.spec.ts
+++ src/app/components/storylist-card-component/storylist-card.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StorylistCardComponent } from './storylist-card.component';
+import { NgModule } from '@angular/core';
+@NgModule({
+	declarations: [],
+	exports: [],
+})
+class MockA11yTooltipModule {}
 
 describe('StorylistCardComponent', () => {
 	let component: StorylistCardComponent;
 	let fixture: ComponentFixture<StorylistCardComponent>;
 
 	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			imports: [StorylistCardComponent],
-		}).compileComponents();
+		await TestBed.configureTestingModule({})
+			.overrideComponent(StorylistCardComponent, {
+				set: {
+					imports: [MockA11yTooltipModule],
+				},
+			})
+			.compileComponents();
 
 		fixture = TestBed.createComponent(StorylistCardComponent);
 		component = fixture.componentInstance;
