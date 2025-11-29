73f94a5df507faee7ebb5e139b639871834ad2eb
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-11-29 15:55:22 -0300

    [#1358] - Actualiza orden de rutas

--- src/api/modules/story/story.controller.ts
+++ src/api/modules/story/story.controller.ts
@@ -24,6 +24,19 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
 		.catch((err) => next(err));
 });
 
+router.get('/most-read', (req: Request, res: Response, next: NextFunction) => {
+	const { limit, offset } = req.query;
+	getMostReadStoryNavigationTeasers(parseInt((limit ?? '6') as string), parseInt((offset ?? '0') as string))
+		.then((result) => res.json(result))
+		.catch((err) => next(err));
+});
+
+router.get('/update-most-read', (req: Request, res: Response, next: NextFunction) => {
+	updateMostReadStories()
+		.then((result) => res.json(result))
+		.catch((err) => next(err));
+});
+
 router.get('/:slug', (req: Request, res: Response, next: NextFunction) => {
 	const { slug } = req.params;
 	getStoryBySlug(slug as string)
@@ -52,19 +65,6 @@ router.get('/author/:slug/navigation', (req: Request, res: Response, next: NextF
 		limit: parseInt((limit ?? '100') as string),
 		offset: parseInt((offset ?? '0') as string),
 	})
-		.then((result) => res.json(result))
-		.catch((err) => next(err));
-});
-
-router.get('/most-read', (req: Request, res: Response, next: NextFunction) => {
-	const { limit, offset } = req.query;
-	getMostReadStoryNavigationTeasers(parseInt((limit ?? '6') as string), parseInt((offset ?? '0') as string))
-		.then((result) => res.json(result))
-		.catch((err) => next(err));
-});
-
-router.get('/update-most-read', (req: Request, res: Response, next: NextFunction) => {
-	updateMostReadStories()
 		.then((result) => res.json(result))
 		.catch((err) => next(err));
 });
