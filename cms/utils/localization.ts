8f72c2e168c9cc6a30084e6de55f7c0218ebcdfa
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-12-10 10:06:59 -0300

    [#1430] - Elimina referencias a idiomas en schemas

--- cms/utils/localization.ts
+++
@@ -1,8 +0,0 @@
-export interface Language {
-	id: string;
-	title: string;
-	isDefault?: boolean;
-}
-
-export const defaultLanguage: Language = { id: 'es', title: 'Español', isDefault: true };
-export const supportedLanguages: Language[] = [defaultLanguage, { id: 'en', title: 'English' }];
