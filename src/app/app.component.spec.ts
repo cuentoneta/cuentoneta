5f246e2a26561ed71985b0afaea8d4e2b0381ed6
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-12-18 14:52:16 -0300

    [#1371] - Agrega provider faltante en tests de AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,16 +2,21 @@ import { AppComponent } from './app.component';
 import { render } from '@testing-library/angular';
 import { NgOptimizedImage } from '@angular/common';
 import { provideRouter, RouterModule } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from '@components/header/header.component';
+import { FooterComponent } from '@components/footer/footer.component';
 import { AnalyticsService } from './providers/analytics/analytics.service';
 import { AnalyticsMockService } from './providers/analytics/analytics.mock.service';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
 	const setup = async () => {
 		return await render(AppComponent, {
 			componentImports: [HeaderComponent, FooterComponent, NgOptimizedImage, RouterModule],
-			providers: [provideRouter([]), { provide: AnalyticsService, useClass: AnalyticsMockService }],
+			providers: [
+				provideRouter([]),
+				{ provide: AnalyticsService, useClass: AnalyticsMockService },
+				provideNoopAnimations(),
+			],
 		});
 	};
 
