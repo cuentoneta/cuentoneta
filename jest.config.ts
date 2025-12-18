a7e97a2b084baf3fe6fbc65ed65ce25b31e4dc59
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-12-18 15:33:39 -0300

    [#1371] - Corrige problema de configuración de Jest

--- jest.config.ts
+++ jest.config.ts
@@ -14,7 +14,7 @@ export default {
 			},
 		],
 	},
-	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+	transformIgnorePatterns: ['node_modules/(?!.*(@sanity|\.mjs$))'],
 	snapshotSerializers: [
 		'jest-preset-angular/build/serializers/no-ng-attributes',
 		'jest-preset-angular/build/serializers/ng-snapshot',
