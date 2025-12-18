1152306cea32e33e509d09760631722a3e77adbc
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-12-18 09:56:01 -0300

    [#1371] - Ejecuta migraciones de Nx v22.3.0

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,19 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 export default {
 	stories: ['../src/app/**/*.stories.mdx', '../src/app/**/*.stories.@(js|jsx|ts|tsx)'],
 	styles: [],
 	addons: [
-		'@storybook/addon-docs',
+		getAbsolutePath('@storybook/addon-docs'),
 		{
-			name: '@storybook/addon-styling-webpack',
+			name: getAbsolutePath('@storybook/addon-styling-webpack'),
 			options: {
 				postCss: true,
 			},
 		},
 	],
 	framework: {
-		name: '@storybook/angular',
+		name: getAbsolutePath('@storybook/angular'),
 		options: {},
 	},
 	docs: {},
@@ -21,3 +23,7 @@ export default {
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
 // and https://nx.dev/packages/storybook/documents/custom-builder-configs
+
+function getAbsolutePath(value) {
+	return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
