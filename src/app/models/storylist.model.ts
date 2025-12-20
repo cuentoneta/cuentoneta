03e381155a9ab9e7c9902612244de29de4f41290
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-12-20 14:52:25 -0300

    [#1777] - Agrega definición de propiedades tabs y media en aggregates de modelo Storylist

--- src/app/models/storylist.model.ts
+++ src/app/models/storylist.model.ts
@@ -1,6 +1,14 @@
 import { StoryNavigationTeaserWithAuthor, StoryTeaserWithAuthor } from './story.model';
 import { Tag } from '@models/tag.model';
 import { TextBlockContent } from '@models/block-content.model';
+import { Media } from '@models/media.model';
+
+export interface StorylistTab {
+	title: string;
+	slug: string;
+	content: TextBlockContent[];
+	icon?: string;
+}
 
 interface StorylistBase<T> {
 	_id: string;
@@ -14,14 +22,19 @@ interface StorylistBase<T> {
 	config: {
 		showAuthors: boolean;
 	};
+	tabs: StorylistTab[];
+	media: Media[];
 }
 
 export interface StorylistTeaser extends StorylistBase<never> {
 	stories: Array<never>;
+	tabs: Array<never>;
 }
 
 export interface StorylistStoriesNavigationTeasers extends StorylistBase<StoryNavigationTeaserWithAuthor> {
 	stories: StoryNavigationTeaserWithAuthor[];
+	tabs: Array<never>;
+	media: Array<never>;
 }
 
 export interface Storylist extends StorylistBase<StoryTeaserWithAuthor> {
