3d432d71aeb209549f810c3edbe306b04a36d92f
Ramiro Olivencia <ramiro@olivencia.com.ar>
2026-01-09 03:15:49 -0300

    [#1465] - Simplifica documentación de CollectionTeaser en Storybook

--- src/app/components/collection-teaser/collection-teaser.stories.ts
+++ src/app/components/collection-teaser/collection-teaser.stories.ts
@@ -7,7 +7,7 @@ import { storylistMock } from '@mocks/storylist.mock';
 
 const meta: Meta<CollectionTeaser> = {
 	component: CollectionTeaser,
-	title: 'CollectionTeaser',
+	title: 'Componentes/CollectionTeaser',
 	decorators: [
 		applicationConfig({
 			providers: [provideRouter([])],
@@ -24,40 +24,12 @@ export const Primary = {
 		props: args,
 		template: `
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
-          <cuentoneta-storylist-card class="card" [collection]="storylist"/>
-          <cuentoneta-storylist-card-skeleton class="card w-full"/>
+          <cuentoneta-collection-teaser class="card" [collection]="storylist"/>
+          <cuentoneta-collection-teaser-skeleton class="card w-full"/>
     </div>
 `,
 	}),
 	args: {
 		storylist: storylistMock,
 	},
-};
-
-export const Loaded = {
-	render: (args: CollectionTeaser) => ({
-		props: args,
-		template: `
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 bg-neutral-100 p-4">
-          <cuentoneta-storylist-card class="card" [collection]="storylist1"/>
-          <cuentoneta-storylist-card class="card" [collection]="storylist2"/>
-    </div>
-`,
-	}),
-	args: {
-		storylist1: storylistMock,
-		storylist2: storylistMock,
-	},
-};
-
-export const Loading = {
-	render: (args: CollectionTeaser) => ({
-		props: args,
-		template: `
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 bg-neutral-100 p-4">
-          <cuentoneta-storylist-card-skeleton class="card w-full"/>
-          <cuentoneta-storylist-card-skeleton class="card w-full"/>
-    </div>
-`,
-	}),
 };
