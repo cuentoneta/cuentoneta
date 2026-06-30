238187d7a686dd07d72a75a7e3152e775aa4dbeb
Ramiro Olivencia <ramiro@olivencia.com.ar>
2026-06-29 23:14:37 -0300

    [#1638] - Corrige comentarios en CollectionTeaser

--- src/app/components/collection-teaser/collection-teaser.ts
+++ src/app/components/collection-teaser/collection-teaser.ts
@@ -21,7 +21,7 @@ import { CoverImageComponent } from '../cover-image/cover-image.component';
 			@if (collection(); as storylist) {
 				<a [routerLink]="['/' + appRoutes.StoryList, storylist.slug]" class="navigation-link flex items-start gap-5">
 					<section
-						class="relative flex h-[192px] items-end justify-center overflow-hidden rounded-xl bg-neutral-100 px-3 sm:flex-1"
+						class="relative flex h-48 items-end justify-center overflow-hidden rounded-xl bg-neutral-100 px-3 sm:flex-1"
 					>
 						@if (storylist.imagery.kind === 'representative') {
 							<cuentoneta-cover-image [src]="storylist.imagery.image" class="-mb-2" />
@@ -59,8 +59,9 @@ export class CollectionTeaser {
 	public readonly collection = input<StorylistTeaser>();
 	protected readonly appRoutes = AppRoutes;
 
-	// Posiciones de las 3 portadas del sample (nodo Figma 5047-3446): [0] central al frente con bottom-bleed,
-	// [1] lateral izquierda y [2] derecha desplazadas, con borde neutral-100. En el TS para iterar con @for.
+	// Posiciones de las portadas en visualización múltiple a partir de las imágenes alusivas de stories
+	// [0] central al frente con bottom-bleed, [1] lateral izquierda y [2] derecha desplazadas, con borde neutral-100.
+	// Se expresan las clases CSS en sampleImageClasses para hacer más sencilla la notación al iterar con @for
 	protected readonly sampleImageClasses = [
 		'absolute bottom-[-8px] left-1/2 z-20 -translate-x-1/2 border-[3px] border-neutral-100',
 		'absolute top-[calc(50%_+_39.35px)] left-[calc(50%_-_82.75px)] z-10 -translate-x-1/2 -translate-y-1/2 border-[3px] border-neutral-100',
